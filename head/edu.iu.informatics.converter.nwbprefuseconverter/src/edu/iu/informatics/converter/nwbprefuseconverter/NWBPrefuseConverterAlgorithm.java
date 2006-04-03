48eb01bae881c58130eec8c32ded3a1e520fa2c2
Bonnie Huang <huangb@indiana.edu>
2006-04-03 17:32:01 +0000

    the initial version

---
+++ head/edu.iu.informatics.converter.nwbprefuseconverter/src/edu/iu/informatics/converter/nwbprefuseconverter/NWBPrefuseConverterAlgorithm.java
@@ -0,0 +1,139 @@
+/* 
+ * InfoVis CyberInfrastructure: A Data-Code-Compute Resource for Research
+ * and Education in Information Visualization (http://iv.slis.indiana.edu)
+ */
+package edu.iu.informatics.converter.nwbprefuseconverter;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+
+import edu.iu.iv.common.property.PropertyMap;
+import edu.iu.iv.core.IVC;
+import edu.iu.iv.core.UnsupportedModelException;
+import edu.iu.iv.core.algorithm.AbstractAlgorithm;
+import edu.iu.iv.core.algorithm.AlgorithmProperty;
+import edu.iu.iv.core.datamodels.BasicDataModel;
+import edu.iu.iv.core.datamodels.DataModel;
+import edu.iu.iv.core.datamodels.DataModelProperty;
+import edu.iu.iv.core.datamodels.DataModelType;
+import edu.iu.iv.core.persistence.BasicFileResourceDescriptor;
+import edu.iu.iv.core.persistence.PersistenceException;
+import edu.iu.iv.persisters.standard.PrefusexGMMLGraphPersister;
+import edu.iu.nwb.core.model.Edge;
+import edu.iu.nwb.core.model.NWBModel;
+import edu.iu.nwb.core.model.Node;
+
+/**
+ * Class to implement the Algorithm for this IVC Plug-in.
+ *
+ * @author
+ */
+public class NWBPrefuseConverterAlgorithm extends AbstractAlgorithm {    
+    private static final String ALGORITHM_NAME = "NWB -> Prefuse Converter";
+    private DataModel dm;
+    
+    /**
+     * Creates a new NWBPrefuseConverterAlgorithm.
+     */
+	public NWBPrefuseConverterAlgorithm(DataModel dm) {
+	    propertyMap.put(AlgorithmProperty.LABEL, ALGORITHM_NAME);
+	    
+	    this.dm = dm;
+	}
+	
+	/**
+	 * Executes this NWBPrefuseConverterAlgorithm.
+	 * 
+	 * @return true if the Algorithm was successful, false if not
+	 */
+	public boolean execute() {
+		NWBModel nwbModel = (NWBModel) dm.getData();
+
+		String temp = IVC.getInstance().getTemporaryFilesFolder();
+		File tempFile;
+		try {
+			tempFile = File.createTempFile("graph-ml-", ".xml", new File(temp));
+			writeGraphMl(nwbModel, tempFile);
+
+//			PersistenceRegistry perReg = IVC.getInstance()
+//					.getPersistenceRegistry();
+			try {
+//				XMLGraphReader xgr = new XMLGraphReader() ;
+//		        Graph graph = xgr.loadGraph(tempFile) ;
+				
+				PrefusexGMMLGraphPersister persister = new PrefusexGMMLGraphPersister();
+		        
+				Object model = persister.restore(new BasicFileResourceDescriptor(tempFile));
+//				Object model = perReg.load(new BasicFileResourceDescriptor(tempFile));
+//				System.out.println(model.getClass().getName());
+				String label = tempFile.getName();
+				DataModelType type = DataModelType.NETWORK;
+
+				DataModel dataModel = new BasicDataModel(model);
+				PropertyMap propMap = dataModel.getProperties();
+				propMap.put(DataModelProperty.LABEL, label);
+				propMap.put(DataModelProperty.TYPE, type);
+				try {
+					IVC.getInstance().addModel(dataModel);
+				} catch (UnsupportedModelException e) {
+					e.printStackTrace();
+				}
+
+			} catch (OutOfMemoryError e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+//			}
+			} catch (PersistenceException e) {
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	    return true;
+	}
+	
+	private void writeGraphMl(NWBModel nwbModel, File tempFile) {
+		try {
+//			System.out.println("file: " + tempFile);
+			PrintWriter out
+			   = new PrintWriter(new BufferedWriter(new FileWriter(tempFile)));
+			
+			out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+			out.println("<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\">");
+			out.println("<graph edgedefault=\"undirected\">");
+			
+			Iterator nodeIter = nwbModel.getNodes();
+			
+			//System.out.println(nwbModel.getClass());
+			while (nodeIter.hasNext()) {
+				Node bnc = (Node)nodeIter.next();
+				Object numAttr = bnc.getPropertyValue(Node.ID);
+				
+				out.println("<node id=\"" + numAttr.toString() + "\" label=\""+bnc.toString()+"\"> </node>");
+//				out.println("<node id=\"" + numAttr.toString() + "\"></node>");
+			}
+			
+			Iterator edgeIter = nwbModel.getUndirectedEdges();
+			while (edgeIter.hasNext()) {
+				Edge bec = (Edge)edgeIter.next();
+				
+				
+				out.println("<edge source=\"" + bec.getPropertyValue(Edge.ORIGIN)  + 
+						    "\" target=\"" + bec.getPropertyValue(Edge.DEST) + "\">" +
+						    "</edge>");
+			}
+			
+			out.println("</graph>");
+			out.println("</graphml>");
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
