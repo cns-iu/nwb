92a47e8dbdde2f98bdcce8a7d92febda0872cbbf
Bonnie Huang <huangb@indiana.edu>
2006-04-25 18:19:41 +0000

    specify the parent file name as network --- the data model

--- head/plugins/modeling/edu.iu.informatics.modeling.smallworld/src/edu/iu/informatics/modeling/smallworld/SmallWorldAlgorithm.java
+++ head/plugins/modeling/edu.iu.informatics.modeling.smallworld/src/edu/iu/informatics/modeling/smallworld/SmallWorldAlgorithm.java
@@ -52,7 +52,8 @@ public class SmallWorldAlgorithm extends FortranAlgorithm {
 
 		runner.registerFile("network", "network file", DataModelType.NETWORK,
 				new BasicEdgePersister(parameterMap.getIntValue("nodes")));
-
+		runner.setParentFilename("network");
+		
 		makeInputFile(runner, "inputfile");
 		runner.execute(SmallWorldPlugin.ID_PLUGIN);
 
