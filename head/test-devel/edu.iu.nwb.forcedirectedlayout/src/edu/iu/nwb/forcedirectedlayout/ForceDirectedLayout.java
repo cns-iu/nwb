4bcac6fe664b0494c91b46bdedc8d35326d2e2f7
Bonnie Huang <huangb@indiana.edu>
2006-08-17 20:31:30 +0000

    initial version

---
+++ head/test-devel/edu.iu.nwb.forcedirectedlayout/src/edu/iu/nwb/forcedirectedlayout/ForceDirectedLayout.java
@@ -0,0 +1,75 @@
+package edu.iu.nwb.forcedirectedlayout;
+
+
+import java.util.Dictionary;
+
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.metatype.MetaTypeProvider;
+import org.osgi.service.metatype.MetaTypeService;
+
+import org.cishell.framework.CIShellContext;
+import org.cishell.framework.algorithm.Algorithm;
+import org.cishell.framework.algorithm.AlgorithmFactory;
+import org.cishell.framework.data.Data;
+
+import edu.berkeley.guir.prefuse.graph.Graph;
+import edu.berkeley.guir.prefusex.force.DragForce;
+import edu.berkeley.guir.prefusex.force.ForceSimulator;
+import edu.berkeley.guir.prefusex.force.NBodyForce;
+import edu.berkeley.guir.prefusex.force.SpringForce;
+
+/**
+ * @author Weixia(Bonnie) Huang 
+ */
+public class ForceDirectedLayout implements AlgorithmFactory {
+	   private MetaTypeProvider provider;
+
+	   protected void activate(ComponentContext ctxt) {
+	    	MetaTypeService mts = (MetaTypeService)ctxt.locateService("MTS");
+	        provider = mts.getMetaTypeInformation(ctxt.getBundleContext().getBundle());  
+	    }
+	    protected void deactivate(ComponentContext ctxt) {
+	        provider = null;
+	    }
+
+	    /**
+	     * @see org.cishell.framework.algorithm.AlgorithmFactory#createParameters(org.cishell.framework.data.Data[])
+	     */
+	    public MetaTypeProvider createParameters(Data[] dm) {
+	        return provider;
+	    }
+	    
+	    /**
+	     * @see org.cishell.framework.algorithm.AlgorithmFactory#createAlgorithm(org.cishell.framework.data.Data[], java.util.Dictionary, org.cishell.framework.CIShellContext)
+	     */
+	    public Algorithm createAlgorithm(Data[] dm, Dictionary parameters,
+	            CIShellContext context) {
+	        return new ForceDirectedLayoutAlg(dm, parameters, context);
+	    }
+	    
+	    private class ForceDirectedLayoutAlg implements Algorithm {
+	        Data[] dm;
+	        Dictionary parameters;
+	        CIShellContext ciContext;
+	        
+	        public ForceDirectedLayoutAlg(Data[] dm, Dictionary parameters,
+	        		CIShellContext ciContext) {
+	        	this.dm = dm;
+	        	this.parameters = parameters;
+	            this.ciContext = ciContext;
+	        }
+
+	        public Data[] execute() {
+	            Graph graph = (Graph) dm[0].getData();
+	            ForceSimulator fsim = new ForceSimulator();
+	            fsim.addForce(new NBodyForce(-0.4f, -1f, 0.9f));
+	            fsim.addForce(new SpringForce(4E-5f, 75f));
+	            fsim.addForce(new DragForce(-0.005f));
+
+	            ForceDirectedLayoutVisualization viz = 
+	            	new ForceDirectedLayoutVisualization(graph, fsim);
+	            viz.show();
+	            return null;
+	        }
+	    }
+}
