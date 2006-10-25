1096e053a20f676ff6d89c3a9245823ccef7b09e
Bonnie Huang <huangb@indiana.edu>
2006-10-25 04:35:23 +0000

    add validateGraphMLHeader()method to prevent load a GraphML file(.xml) as an XGMML file(.xml)

--- trunk/plugins/converter/edu.iu.nwb.converter.prefusexgmml/src/edu/iu/nwb/converter/prefusexgmml/reader/PrefuseXGMMLValidation.java
+++ trunk/plugins/converter/edu.iu.nwb.converter.prefusexgmml/src/edu/iu/nwb/converter/prefusexgmml/reader/PrefuseXGMMLValidation.java
@@ -1,6 +1,9 @@
 package edu.iu.nwb.converter.prefusexgmml.reader;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Dictionary;
 
@@ -11,7 +14,7 @@ import org.cishell.framework.data.BasicData;
 import org.cishell.framework.data.Data;
 import org.cishell.framework.data.DataProperty;
 import org.osgi.service.component.ComponentContext;
-import org.osgi.service.log.LogService;
+//import org.osgi.service.log.LogService;
 import org.osgi.service.metatype.MetaTypeProvider;
 
 import edu.berkeley.guir.prefuse.graph.io.XMLGraphReader;
@@ -45,16 +48,21 @@ public class PrefuseXGMMLValidation implements AlgorithmFactory {
         }
 
         public Data[] execute() {
-	    	LogService logger = (LogService)context.getService(LogService.class.getName());
+//	    	LogService logger = (LogService)context.getService(LogService.class.getName());
 
 			String fileHandler = (String) data[0].getData();
 			File inData = new File(fileHandler);
         	try{
-        		(new XMLGraphReader()).loadGraph(fileHandler);
-        		Data[] dm = new Data[] {new BasicData(inData, "file:text/xgmml+xml")};
-        		dm[0].getMetaData().put(DataProperty.LABEL, "Prefuse XGMML .xml file: " + fileHandler);
-                dm[0].getMetaData().put(DataProperty.TYPE, DataProperty.NETWORK_TYPE);
-        		return dm;
+        		if (!validateGraphMLHeader(inData)){
+	        		(new XMLGraphReader()).loadGraph(fileHandler);
+	        		Data[] dm = new Data[] {new BasicData(inData, "file:text/xgmml+xml")};
+	        		dm[0].getMetaData().put(DataProperty.LABEL, "Prefuse XGMML .xml file: " + fileHandler);
+	                dm[0].getMetaData().put(DataProperty.TYPE, DataProperty.NETWORK_TYPE);
+	        		return dm;
+        		}else
+        			return null;   
+        	}catch (FileNotFoundException e){
+        		return null;
         	}catch (IOException ioe){
 //				logger.log(LogService.LOG_ERROR, "Might not be a XGMML file. Got the following exception");
 //        		logger.log(LogService.LOG_ERROR, "IOException", ioe);
@@ -62,5 +70,33 @@ public class PrefuseXGMMLValidation implements AlgorithmFactory {
         	}
 
         }
+        
+        //TODO Bonnie
+        //This is a temporary fix. The problem is after we get rid of LoadDataChooser
+        //It seems that prefuse library can load xgmml file(.xml) in as a graphml.
+        //but all visualization algs didn't work since it is not a real graphml file.
+        //It also possible that sometimes prefuse can load gramphml file in as an xgmml.
+        //although the reverse path never happened in my testing env. 
+        //Here I try to detect if there is "<graphml" header in the file, if not, it 
+        //is not a gramphml file
+        private boolean validateGraphMLHeader(File inData)
+        		throws FileNotFoundException, IOException{
+        	boolean hasGraphMLHeader = false;
+    		
+        	BufferedReader reader = 
+    			new BufferedReader(new FileReader(inData));
+    		
+    		String line = reader.readLine();
+    		while(line != null){
+    			if(line.startsWith("<graphml")){
+    				hasGraphMLHeader = true;
+    				break;
+    			}    			
+    			line = reader.readLine();	
+    		}
+    		return hasGraphMLHeader;
+    		
+        }
+
     }
 }
