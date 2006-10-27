a1ca25af119fb5e26146ce2fdf342325896d5720
Bonnie Huang <huangb@indiana.edu>
2006-10-27 18:23:21 +0000

    initial version

---
+++ trunk/plugins/deployment/edu.iu.nwb.gui.brand/src/edu/iu/nwb/gui/brand/DataManagerView.java
@@ -0,0 +1,9 @@
+package edu.iu.nwb.gui.brand;
+
+import org.cishell.reference.gui.datamanager.AbstractDataManagerView;
+
+public class DataManagerView extends AbstractDataManagerView {
+	public DataManagerView() {
+		super("edu.iu.nwb.gui.brand");
+	}
+}
