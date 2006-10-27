b4e9779dfa92d38a12c67598ae91d454e69fe31b
Bonnie Huang <huangb@indiana.edu>
2006-10-27 20:07:27 +0000

    Temporarily fix a bug in GraphMLFile.save in Jung lib. It can generate a bad graphml file that won't be able to read in. The fix is to read in the file and manually clean the bad entry based on our observation and save it back. Note: This makes this converter to become a lossy one

--- trunk/plugins/converter/edu.iu.nwb.converter.junggraphml/src/edu/iu/nwb/converter/junggraphml/writer/JungGraphMLWriter.java
+++ trunk/plugins/converter/edu.iu.nwb.converter.junggraphml/src/edu/iu/nwb/converter/junggraphml/writer/JungGraphMLWriter.java
@@ -1,14 +1,23 @@
 package edu.iu.nwb.converter.junggraphml.writer;
 
 //Java
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Dictionary;
+import java.util.StringTokenizer;
+
 //CIShell
 import org.cishell.framework.CIShellContext;
 import org.cishell.framework.algorithm.Algorithm;
 import org.cishell.framework.data.BasicData;
 import org.cishell.framework.data.Data;
+import org.cishell.service.guibuilder.GUIBuilderService;
 import org.osgi.service.log.LogService;
 //Jung
 import edu.uci.ics.jung.graph.Graph;
@@ -22,33 +31,98 @@ public class JungGraphMLWriter implements Algorithm {
     Dictionary parameters;
     CIShellContext context;
     LogService logger;
+    GUIBuilderService guiBuilder;
     
     public JungGraphMLWriter(Data[] data, Dictionary parameters, CIShellContext context) {
         this.data = data;
         this.parameters = parameters;
         this.context = context;
         logger = (LogService)context.getService("LogService.class.getName()");
+    	guiBuilder = (GUIBuilderService) context.getService(GUIBuilderService.class.getName());
+
     }
 
     public Data[] execute() {
-       File tempFile;
+    	File tempFile = getTempFile();
+    	File goodGraphML = getTempFile();
         
-    	String tempPath = System.getProperty("java.io.tmpdir");
-    	File tempDir = new File(tempPath+File.separator+"temp");
-    	if(!tempDir.exists())
-    		tempDir.mkdir();
-    	try{
-    		tempFile = File.createTempFile("NWB-Session-", ".xml", tempDir);
-    		
-    	}catch (IOException e){
-    		logger.log(LogService.LOG_ERROR, e.toString());
-    		tempFile = new File (tempPath+File.separator+"nwbTemp"+File.separator+"temp.nwb");
-
-    	}
     	if (tempFile != null){
     		(new GraphMLFile()).save((Graph)(data[0].getData()), tempFile.getPath()) ;
     	}
-        return new Data[]{new BasicData(tempFile, "file:text/graphml+xml") };
+    	
+    	//TODO: This is a temporary fix. We found a bug in GraphMLFile.save. 
+    	//It can generate a bad graphml file that won't be able to read in.
+    	//Here I read in the file and clean the bad entry and save it back.
+    	try{
+    		BufferedReader reader = new BufferedReader(new FileReader(tempFile));
+			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(goodGraphML)));
+
+			String line = reader.readLine();
+			while(line != null){
+				if(line.startsWith("<graph edgedefault=")){
+					StringBuffer goodline = new StringBuffer();
+					StringTokenizer st = new StringTokenizer(line) ;
+				     while (st.hasMoreTokens()) {
+				    	 String token = st.nextToken();
+				         if (token.equals("<graph")){
+				         	goodline.append(token);	
+				         }else if (token.startsWith("edgedefault=")){
+				        	goodline.append(" "+token+">");
+				        	break;
+				         }			         
+				     }
+				     out.println(goodline.toString());
+
+				}
+				else if(line.startsWith ("<node" )){
+					StringBuffer goodline = new StringBuffer();
+					StringTokenizer st = new StringTokenizer(line) ;
+				     while (st.hasMoreTokens()) {
+				    	 String token = st.nextToken();
+				         if (token.equals("<node")){
+				         	goodline.append(token);	
+				         }else if (token.startsWith("id=")){
+				        	goodline.append(" "+token+"/>");
+				        	break;
+				         }			         
+				     }
+				     out.println(goodline.toString());
+				}
+				else
+					out.println(line);
+				line = reader.readLine();
+			}
+	    	reader.close();
+			out.close();
+    	}catch (FileNotFoundException e){
+			guiBuilder.showError("File Not Found Exception", 
+					"Got an File Not Found Exception",e);	
+			return null;
+		}catch (IOException ioe){
+			guiBuilder.showError("IOException", 
+					"Got an IOException",ioe);
+			return null;
+		}    	
+        return new Data[]{new BasicData(goodGraphML, "file:text/graphml+xml") };
 
     }
+    
+	private File getTempFile(){
+		File tempFile;
+    
+		String tempPath = System.getProperty("java.io.tmpdir");
+		File tempDir = new File(tempPath+File.separator+"temp");
+		if(!tempDir.exists())
+			tempDir.mkdir();
+		try{
+			tempFile = File.createTempFile("NWB-Session-", ".xml", tempDir);
+		
+		}catch (IOException e){
+			logger.log(LogService.LOG_ERROR, e.toString());
+			tempFile = new File (tempPath+File.separator+"nwbTemp"+File.separator+"temp.nwb");
+
+		}
+		return tempFile;
+	}
+
 }
