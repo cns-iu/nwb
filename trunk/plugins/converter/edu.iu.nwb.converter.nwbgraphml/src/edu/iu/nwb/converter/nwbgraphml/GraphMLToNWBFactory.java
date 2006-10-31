de183cdf714f5b20519f4044b1ec31b080fc331e
Benjamin Markines <bmarkine@indiana.edu>
2006-10-31 18:01:38 +0000

    Added javadoc to the GraphML to NWB Factory converter

--- trunk/plugins/converter/edu.iu.nwb.converter.nwbgraphml/src/edu/iu/nwb/converter/nwbgraphml/GraphMLToNWBFactory.java
+++ trunk/plugins/converter/edu.iu.nwb.converter.nwbgraphml/src/edu/iu/nwb/converter/nwbgraphml/GraphMLToNWBFactory.java
@@ -9,15 +9,30 @@ import org.cishell.framework.data.Data;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.metatype.MetaTypeProvider;
 
-
+/**
+ * Algorithm factory for GraphML to NWB converter
+ * 
+ * @author bmarkine
+ */
 public class GraphMLToNWBFactory implements AlgorithmFactory {
 
     protected void activate(ComponentContext ctxt) { }
     protected void deactivate(ComponentContext ctxt) { }
 
+    /**
+     * Create an converter
+     * 
+     * @param data The data to convert
+     * @param parameters Parameters passed to the algorithm
+     * @param context Access to the CIShell environment
+     */
     public Algorithm createAlgorithm(Data[] data, Dictionary parameters, CIShellContext context) {
         return new GraphMLToNWB(data, parameters, context);
     }
+    
+    /**
+     * This converter accepts no parameters
+     */
     public MetaTypeProvider createParameters(Data[] data) {
         return null;
     }
