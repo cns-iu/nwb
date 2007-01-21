8ce9367b6e853f1df4a8f0881e673aad38ef8428
Santo Fortunato <santo@indiana.edu>
2007-01-21 02:03:23 +0000

    node label problem solved

--- trunk/plugins/analysis/edu.iu.nwb.analysis.totaldegreedistribution/src/total_degree_distr.f90
+++ trunk/plugins/analysis/edu.iu.nwb.analysis.totaldegreedistribution/src/total_degree_distr.f90
@@ -11,6 +11,7 @@
        integer i,j,k,maxdeg,mindeg,n_vert,n_edges,n_bins,i1,i2,maxind,minind
        integer, allocatable, dimension (:) :: degree,degdis,npoints,np
        real*8, allocatable, dimension (:) :: interv,avdegbin
+       logical, allocatable, dimension(:):: nodelist
        real*8 binsize
        character*256 filename,fileout2,fileout3,str1,str2,sn_bins
        
@@ -25,14 +26,22 @@
 
        n_edges=0
        maxind=1
-       minind=1000000
+       minind=10000000
 
        open(20,file=filename,status='unknown')
        do 
           read(20,106,err=8103,end=8103)str1
-          if(str1(1:1)=='*'.AND.str1(2:2)=='U')then
+          if(str1(1:1)=='*'.AND.str1(2:2)=='N')then
+             n_vert=0
              do 
-                read(20,*,err=8103,end=8103)i1,i2
+                read(20,*,err=8103,end=8103)i1
+                if(minind>i1)minind=i1
+                if(maxind<i1)maxind=i1
+                n_vert=n_vert+1  
+             enddo
+          else if(str1(1:1)=='*'.AND.str1(2:2)=='U')then 
+             do 
+                read(20,*,err=9103,end=9103)i1,i2
                 if(minind>i1)minind=i1
                 if(minind>i2)minind=i2
                 if(maxind<i2)maxind=i2
@@ -42,35 +51,81 @@
           endif
        enddo
 8103   continue
+       backspace(20)
+       do
+          read(20,106,err=9103,end=9103)str1
+          if(str1(1:1)=='*'.AND.str1(2:2)=='U')then
+             do 
+                read(20,*,err=9103,end=9103)i1,i2
+                if(minind>i1)minind=i1
+                if(minind>i2)minind=i2
+                if(maxind<i2)maxind=i2
+                if(maxind<i1)maxind=i1
+                n_edges=n_edges+1   
+             enddo
+          endif
+       enddo
+9103   continue
+       close(20)
+       allocate(nodelist(minind:maxind))
+       allocate(degree(minind:maxind))
+       degree=0
+       nodelist=.false.
+       open(20,file=filename,status='unknown')
+       do 
+          read(20,106,err=9203,end=9203)str1
+          if(str1(1:1)=='*'.AND.str1(2:2)=='N')then
+             n_vert=0
+             do 
+                read(20,*,err=9203,end=9203)i1
+                nodelist(i1)=.true.
+                n_vert=n_vert+1
+             enddo
+          else if(str1(1:1)=='*'.AND.str1(2:2)=='U')then
+             n_vert=0
+             do i=1,n_edges
+                read(20,*)i1,i2
+                if(nodelist(i1).eqv..false.)then
+                   nodelist(i1)=.true.
+                   n_vert=n_vert+1
+                endif
+                if(nodelist(i2).eqv..false.)then
+                   nodelist(i2)=.true.
+                   n_vert=n_vert+1
+                endif
+                degree(i1)=degree(i1)+1
+                degree(i2)=degree(i2)+1
+             enddo
+             goto 9303
+          endif
+       enddo
+9203   continue
+       backspace(20)
+       do
+          read(20,106,err=9303,end=9303)str1
+          if(str1(1:1)=='*'.AND.str1(2:2)=='U')then
+             do i=1,n_edges
+                read(20,*)i1,i2
+                if(nodelist(i1).eqv..false.)then
+                   nodelist(i1)=.true.
+                   n_vert=n_vert+1
+                endif
+                if(nodelist(i2).eqv..false.)then
+                   nodelist(i2)=.true.
+                   n_vert=n_vert+1
+                endif
+                degree(i1)=degree(i1)+1
+                degree(i2)=degree(i2)+1
+             enddo
+          endif
+       enddo
+9303   continue
        close(20)
 
        if(n_edges==0)then
           write(*,*)'Error! The program should be applied on undirected networks'
           stop
        endif
-       if(minind/=1)then
-          write(*,*)'Error! The minimal node index is not 1'
-          stop
-       endif
-
-       n_vert=maxind
-
-       allocate(degree(1:n_vert))
-       degree=0
-
-       open(20,file=filename,status='unknown')
-       do 
-          read(20,106)str1
-          if(str1(1:1)=='*'.AND.str1(2:2)=='U')then
-             do j=1,n_edges
-                read(20,*)i1,i2
-                degree(i1)=degree(i1)+1
-                degree(i2)=degree(i2)+1
-             enddo
-             exit
-          endif
-       enddo
-       close(20)
 
 !      Here the arrays are allocated
 
@@ -88,8 +143,15 @@
 !      Here we calculate the minimum and maximum degree, allocate and 
 !      initialize the histogram of the degree distribution (array degdis)
 
-       mindeg=MINVAL(degree)
-       maxdeg=MAXVAL(degree)
+       mindeg=1000000
+       maxdeg=0
+
+       do i=minind,maxind
+          if(nodelist(i).eqv..true.)then
+             if(degree(i)>maxdeg)maxdeg=degree(i)
+             if(degree(i)<mindeg)mindeg=degree(i)
+          endif
+       enddo
        
        allocate(degdis(mindeg:maxdeg))
 
@@ -98,8 +160,10 @@
 !      Here we evaluate the number of nodes having the same degree
 !      to get the probability we divide by the total number of edges n_edges
 
-       do k=1,n_vert
-          degdis(degree(k))=degdis(degree(k))+1
+       do k=minind,maxind
+          if(nodelist(k).eqv..true.)then
+             degdis(degree(k))=degdis(degree(k))+1
+          endif
        enddo
 
        open(20,file=fileout2,status='unknown')
@@ -142,14 +206,16 @@
        avdegbin=0.0d0
        np=0
 
-       do i=1,n_vert
-          do j=1,n_bins
-             if(real(degree(i))<interv(j))then
-                np(j)=np(j)+1
-                avdegbin(j)=avdegbin(j)+real(degree(i))
-                exit
-             endif
-          enddo
+       do i=minind,maxind
+          if(nodelist(i).eqv..true.)then
+             do j=1,n_bins
+                if(real(degree(i))<interv(j))then
+                   np(j)=np(j)+1
+                   avdegbin(j)=avdegbin(j)+real(degree(i))
+                   exit
+                endif
+             enddo
+          endif
        enddo
        
        do i=1,n_bins
