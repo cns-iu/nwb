187ecdcaf3d0b9ee12b96bd0af60a1e1b235490e
Santo Fortunato <santo@indiana.edu>
2007-01-21 21:54:24 +0000

    node label problem solved

--- trunk/plugins/analysis/edu.iu.nwb.analysis.clustering_vs_k/src/clustering_vs_k.f90
+++ trunk/plugins/analysis/edu.iu.nwb.analysis.clustering_vs_k/src/clustering_vs_k.f90
@@ -16,8 +16,9 @@
        integer, allocatable, dimension (:) :: degree,intdegree,clus_coef,np
        integer, allocatable, dimension (:) :: check_neigh,listlink
        real*8, allocatable, dimension (:) :: clus,interv,avclusbin,avdegbin
+       logical, allocatable, dimension(:):: nodelist
        real*8 norm,Avcluscoeff,minclus,maxclus,abin,binsize,zero
-       character*256 filename,fileout,fileout5,sn_bins,str1,str2
+       character*256 filename,fileout5,sn_bins,str1,str2
        
 !      Here the program reads the input parameters
        
@@ -31,15 +32,23 @@
 
        n_edges=0
        maxind=1
-       minind=1000000
+       minind=10000000
        zero=0.0d0
 
        open(20,file=filename,status='unknown')
        do 
           read(20,106,err=8103,end=8103)str1
-          if(str1(1:1)=='*'.AND.str1(2:2)=='U')then
+          if(str1(1:1)=='*'.AND.str1(2:2)=='N')then
+             n_vert=0
              do 
-                read(20,*,err=8103,end=8103)i1,i2
+                read(20,*,err=8103,end=8103)i1
+                if(minind>i1)minind=i1
+                if(maxind<i1)maxind=i1
+                n_vert=n_vert+1  
+             enddo
+          else if(str1(1:1)=='*'.AND.str1(2:2)=='U')then 
+             do 
+                read(20,*,err=9103,end=9103)i1,i2
                 if(minind>i1)minind=i1
                 if(minind>i2)minind=i2
                 if(maxind<i2)maxind=i2
@@ -49,52 +58,98 @@
           endif
        enddo
 8103   continue
+       backspace(20)
+       do
+          read(20,106,err=9103,end=9103)str1
+          if(str1(1:1)=='*'.AND.str1(2:2)=='U')then
+             do 
+                read(20,*,err=9103,end=9103)i1,i2
+                if(minind>i1)minind=i1
+                if(minind>i2)minind=i2
+                if(maxind<i2)maxind=i2
+                if(maxind<i1)maxind=i1
+                n_edges=n_edges+1   
+             enddo
+          endif
+       enddo
+9103   continue
+       close(20)
+       allocate(nodelist(minind:maxind))
+       allocate(degree(minind:maxind))
+       degree=0
+       nodelist=.false.
+       open(20,file=filename,status='unknown')
+       do 
+          read(20,106,err=9203,end=9203)str1
+          if(str1(1:1)=='*'.AND.str1(2:2)=='N')then
+             n_vert=0
+             do 
+                read(20,*,err=9203,end=9203)i1
+                nodelist(i1)=.true.
+                n_vert=n_vert+1
+             enddo
+          else if(str1(1:1)=='*'.AND.str1(2:2)=='U')then
+             n_vert=0
+             do i=1,n_edges
+                read(20,*)i1,i2
+                if(nodelist(i1).eqv..false.)then
+                   nodelist(i1)=.true.
+                   n_vert=n_vert+1
+                endif
+                if(nodelist(i2).eqv..false.)then
+                   nodelist(i2)=.true.
+                   n_vert=n_vert+1
+                endif
+                degree(i1)=degree(i1)+1
+                degree(i2)=degree(i2)+1
+             enddo
+             goto 9303
+          endif
+       enddo
+9203   continue
+       backspace(20)
+       do
+          read(20,106,err=9303,end=9303)str1
+          if(str1(1:1)=='*'.AND.str1(2:2)=='U')then
+             do i=1,n_edges
+                read(20,*)i1,i2
+                if(nodelist(i1).eqv..false.)then
+                   nodelist(i1)=.true.
+                   n_vert=n_vert+1
+                endif
+                if(nodelist(i2).eqv..false.)then
+                   nodelist(i2)=.true.
+                   n_vert=n_vert+1
+                endif
+                degree(i1)=degree(i1)+1
+                degree(i2)=degree(i2)+1
+             enddo
+          endif
+       enddo
+9303   continue
        close(20)
 
        if(n_edges==0)then
           write(*,*)'Error! The program should be applied on undirected networks'
           stop
        endif
-       if(minind/=1)then
-          write(*,*)'Error! The minimal node index is not 1'
-          stop
-       endif
 
-       n_vert=maxind
-
-       allocate(degree(1:n_vert))
-       degree=0
-
-       open(20,file=filename,status='unknown')
-       do 
-          read(20,106)str1
-          if(str1(1:1)=='*'.AND.str1(2:2)=='U')then
-             do j=1,n_edges
-                read(20,*)i1,i2
-                degree(i1)=degree(i1)+1
-                degree(i2)=degree(i2)+1
-             enddo
-             exit
-          endif
-       enddo
-       close(20)
-
-       allocate(intdegree(1:n_vert))
-       allocate(check_neigh(1:n_vert))
-       allocate(clus_coef(1:n_vert))
-       allocate(clus(1:n_vert))
+       allocate(intdegree(minind:maxind))
+       allocate(check_neigh(minind:maxind))
+       allocate(clus_coef(minind:maxind))
+       allocate(clus(minind:maxind))
        allocate(listlink(1:2*n_edges))
        allocate(np(1:n_bins))
-       allocate(avclusbin(1:n_bins))
        allocate(avdegbin(1:n_bins))
+       allocate(avclusbin(1:n_bins))
        allocate(interv(0:n_bins))
 
 !      Here we determine for each node the position of the first link
 !      in the list of all links (array intdegree)
 
-       intdegree(1)=0
+       intdegree(minind)=0
 
-       do k=1,n_vert-1
+       do k=minind,maxind-1
           intdegree(k+1)=intdegree(k)+degree(k)
        enddo
 
@@ -132,7 +187,7 @@
        ntot_triangles=0
        ncluszero=0
 
-       do k=1,n_vert
+       do k=minind,maxind
           if(degree(k)>1)then
              do j=1,degree(k)
                 check_neigh(listlink(intdegree(k)+j))=1
@@ -159,8 +214,15 @@
           endif
        enddo
 
-       mindeg=MINVAL(degree)
-       maxdeg=MAXVAL(degree)
+       mindeg=1000000
+       maxdeg=0
+
+       do i=minind,maxind
+          if(nodelist(i).eqv..true.)then
+             if(degree(i)>maxdeg)maxdeg=degree(i)
+             if(degree(i)<mindeg)mindeg=degree(i)
+          endif
+       enddo
 
        if(mindeg==0)then
           interv(0)=real(mindeg+1)
@@ -177,17 +239,19 @@
        avclusbin=0.0d0
        np=0
        
-       do i=1,n_vert
-          do j=1,n_bins
-             if(real(degree(i))<interv(j))then
-                np(j)=np(j)+1
-                avdegbin(j)=avdegbin(j)+real(degree(i))
-                avclusbin(j)=avclusbin(j)+clus(i)
-                exit
-             endif
-          enddo
+       do i=minind,maxind
+          if(nodelist(i).eqv..true.)then
+             do j=1,n_bins
+                if(real(degree(i))<interv(j))then
+                   np(j)=np(j)+1
+                   avdegbin(j)=avdegbin(j)+real(degree(i))
+                   avclusbin(j)=avclusbin(j)+clus(i)
+                   exit
+                endif
+             enddo
+          endif
        enddo
-       
+
        do i=1,n_bins
           if(np(i)>0)then
              avclusbin(i)=avclusbin(i)/np(i)
