58ae6b085cc45e002ade4b6039b186c00b8e0a6f
Russell Duhon <rduhon@indiana.edu>
2007-02-22 20:39:10 +0000

    The prefuse beta visualizations presenting the list of options needing properties to be set now present the available properties in dropdown boxes. No effort is made to narrow in on candidate properties, but that might be work for the future (or might best be reserved for the vizlayers effort).

--- trunk/plugins/visualization/edu.iu.nwb.visualization.prefuse.beta/src/edu/iu/nwb/visualization/prefuse/beta/common/PrefuseBetaAlgorithmFactory.java
+++ trunk/plugins/visualization/edu.iu.nwb.visualization.prefuse.beta/src/edu/iu/nwb/visualization/prefuse/beta/common/PrefuseBetaAlgorithmFactory.java
@@ -1,17 +1,29 @@
 package edu.iu.nwb.visualization.prefuse.beta.common;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Dictionary;
+import java.util.List;
 
 import org.cishell.framework.CIShellContext;
 import org.cishell.framework.algorithm.Algorithm;
 import org.cishell.framework.algorithm.AlgorithmFactory;
 import org.cishell.framework.data.BasicData;
 import org.cishell.framework.data.Data;
+import org.cishell.reference.service.metatype.BasicAttributeDefinition;
+import org.cishell.reference.service.metatype.BasicMetaTypeProvider;
+import org.cishell.reference.service.metatype.BasicObjectClassDefinition;
 import org.osgi.service.component.ComponentContext;
+import org.osgi.service.metatype.AttributeDefinition;
+import org.osgi.service.metatype.MetaTypeInformation;
 import org.osgi.service.metatype.MetaTypeProvider;
 import org.osgi.service.metatype.MetaTypeService;
+import org.osgi.service.metatype.ObjectClassDefinition;
+
 
 import prefuse.data.Graph;
+import prefuse.data.Schema;
+import prefuse.data.Table;
 
 
 
@@ -19,24 +31,71 @@ import prefuse.data.Graph;
  * @author Weixia(Bonnie) Huang 
  */
 public abstract class PrefuseBetaAlgorithmFactory implements AlgorithmFactory {
-
-	private MetaTypeProvider provider;
+	
+	private MetaTypeInformation originalProvider;
 
 	protected void activate(ComponentContext ctxt) {
 		//You may delete all references to metatype service if 
 		//your algorithm does not require parameters and return
 		//null in the createParameters() method
 		MetaTypeService mts = (MetaTypeService)ctxt.locateService("MTS");
-		provider = mts.getMetaTypeInformation(ctxt.getBundleContext().getBundle());       
+		originalProvider = mts.getMetaTypeInformation(ctxt.getBundleContext().getBundle());
 	}
 	protected void deactivate(ComponentContext ctxt) {
-		provider = null;
+		originalProvider = null;
+	}
+	
+	private String[] createKeyArray(Schema schema) {
+		String[] keys = new String[schema.getColumnCount() + 1];
+		keys[0] = "";
+		
+		for(int ii = 1; ii <= schema.getColumnCount(); ii++) {
+			keys[ii] = schema.getColumnName(ii - 1);
+		}
+		
+		return keys;
 	}
 
 	/**
 	 * @see org.cishell.framework.algorithm.AlgorithmFactory#createParameters(org.cishell.framework.data.Data[])
 	 */
 	public MetaTypeProvider createParameters(Data[] dm) {
+		
+		Graph graph = (Graph) dm[0].getData();
+		
+		ObjectClassDefinition oldDefinition = originalProvider.getObjectClassDefinition(originalProvider.getPids()[0], null);
+		
+		BasicObjectClassDefinition definition;
+		try {
+			definition = new BasicObjectClassDefinition(oldDefinition.getID(), oldDefinition.getName(), oldDefinition.getDescription(), oldDefinition.getIcon(16));
+		} catch (IOException e) {
+			definition = new BasicObjectClassDefinition(oldDefinition.getID(), oldDefinition.getName(), oldDefinition.getDescription(), null);
+		}
+		
+		String[] nodeAttributesArray = createKeyArray(graph.getNodeTable().getSchema());
+		String[] edgeAttributesArray = createKeyArray(graph.getEdgeTable().getSchema());
+		
+		definition.addAttributeDefinition(ObjectClassDefinition.REQUIRED,
+				new BasicAttributeDefinition("nodeSize", "Node Size", "The label for the node size property", AttributeDefinition.STRING, nodeAttributesArray, nodeAttributesArray));
+		
+		definition.addAttributeDefinition(ObjectClassDefinition.REQUIRED,
+				new BasicAttributeDefinition("edgeSize", "Edge Size", "The label for the edge size property", AttributeDefinition.STRING, edgeAttributesArray, edgeAttributesArray));
+
+		definition.addAttributeDefinition(ObjectClassDefinition.REQUIRED,
+				new BasicAttributeDefinition("nodeColor", "Node Color", "The label for the node color property", AttributeDefinition.STRING, nodeAttributesArray, nodeAttributesArray));
+		
+		definition.addAttributeDefinition(ObjectClassDefinition.REQUIRED,
+				new BasicAttributeDefinition("edgeColor", "Edge Color", "The label for the edge color property", AttributeDefinition.STRING, edgeAttributesArray, edgeAttributesArray));
+		
+		definition.addAttributeDefinition(ObjectClassDefinition.REQUIRED,
+				new BasicAttributeDefinition("ringColor", "Ring Color", "The label for the ring color property", AttributeDefinition.STRING, nodeAttributesArray, nodeAttributesArray));
+
+		definition.addAttributeDefinition(ObjectClassDefinition.REQUIRED,
+				new BasicAttributeDefinition("nodeShape", "Node Shape", "The label for the node shape property", AttributeDefinition.STRING, nodeAttributesArray, nodeAttributesArray));
+
+		
+		
+		MetaTypeProvider provider = new BasicMetaTypeProvider(definition);
 		return provider;
 	}
 
