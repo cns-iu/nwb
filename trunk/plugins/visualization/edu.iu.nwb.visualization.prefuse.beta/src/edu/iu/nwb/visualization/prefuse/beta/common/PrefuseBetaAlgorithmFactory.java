8c14f6ea97f06c9d868b5f6ae871e3069d1aebcc
Russell Duhon <rduhon@indiana.edu>
2007-05-17 15:30:58 +0000

    Now checks if the current pid is one the metatypeprovider 'knows about' before attempting to enrich it, making Tree Map and Tree View work again.

--- trunk/plugins/visualization/edu.iu.nwb.visualization.prefuse.beta/src/edu/iu/nwb/visualization/prefuse/beta/common/PrefuseBetaAlgorithmFactory.java
+++ trunk/plugins/visualization/edu.iu.nwb.visualization.prefuse.beta/src/edu/iu/nwb/visualization/prefuse/beta/common/PrefuseBetaAlgorithmFactory.java
@@ -2,6 +2,7 @@ package edu.iu.nwb.visualization.prefuse.beta.common;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Dictionary;
 import java.util.List;
 
@@ -29,7 +30,7 @@ import prefuse.data.Table;
 
 
 public abstract class PrefuseBetaAlgorithmFactory implements AlgorithmFactory {
-	
+
 	private MetaTypeInformation originalProvider;
 	private String pid;
 
@@ -44,15 +45,15 @@ public abstract class PrefuseBetaAlgorithmFactory implements AlgorithmFactory {
 	protected void deactivate(ComponentContext ctxt) {
 		originalProvider = null;
 	}
-	
+
 	private String[] createKeyArray(Schema schema) {
 		String[] keys = new String[schema.getColumnCount() + 1];
 		keys[0] = "";
-		
+
 		for(int ii = 1; ii <= schema.getColumnCount(); ii++) {
 			keys[ii] = schema.getColumnName(ii - 1);
 		}
-		
+
 		return keys;
 	}
 
@@ -60,53 +61,57 @@ public abstract class PrefuseBetaAlgorithmFactory implements AlgorithmFactory {
 	 * @see org.cishell.framework.algorithm.AlgorithmFactory#createParameters(org.cishell.framework.data.Data[])
 	 */
 	public MetaTypeProvider createParameters(Data[] dm) {
-		
-		Graph graph = (Graph) dm[0].getData();
-		
-		ObjectClassDefinition oldDefinition = originalProvider.getObjectClassDefinition(this.pid, null);
-		
-		BasicObjectClassDefinition definition;
-		try {
-			definition = new BasicObjectClassDefinition(oldDefinition.getID(), oldDefinition.getName(), oldDefinition.getDescription(), oldDefinition.getIcon(16));
-		} catch (IOException e) {
-			definition = new BasicObjectClassDefinition(oldDefinition.getID(), oldDefinition.getName(), oldDefinition.getDescription(), null);
-		}
-		
-		String[] nodeAttributesArray = createKeyArray(graph.getNodeTable().getSchema());
-		String[] edgeAttributesArray = createKeyArray(graph.getEdgeTable().getSchema());
-		
-		System.err.println(oldDefinition.getID());
-		
-		AttributeDefinition[] definitions = oldDefinition.getAttributeDefinitions(ObjectClassDefinition.ALL);
-		
-		for(int ii = 0; ii < definitions.length; ii++) {
-			String id = definitions[ii].getID();
-			System.err.println(id);
-			if(id.equals("nodeSize")) {
-				definition.addAttributeDefinition(ObjectClassDefinition.REQUIRED,
-						new BasicAttributeDefinition("nodeSize", "Node Size", "The label for the node size property", AttributeDefinition.STRING, nodeAttributesArray, nodeAttributesArray));
-			} else if(id.equals("edgeSize")) {
-				definition.addAttributeDefinition(ObjectClassDefinition.REQUIRED,
-						new BasicAttributeDefinition("edgeSize", "Edge Size", "The label for the edge size property", AttributeDefinition.STRING, edgeAttributesArray, edgeAttributesArray));
-			} else if(id.equals("nodeColor")) {
-				definition.addAttributeDefinition(ObjectClassDefinition.REQUIRED,
-						new BasicAttributeDefinition("nodeColor", "Node Color", "The label for the node color property", AttributeDefinition.STRING, nodeAttributesArray, nodeAttributesArray));
-			} else if(id.equals("edgeColor")) {
-				definition.addAttributeDefinition(ObjectClassDefinition.REQUIRED,
-						new BasicAttributeDefinition("edgeColor", "Edge Color", "The label for the edge color property", AttributeDefinition.STRING, edgeAttributesArray, edgeAttributesArray));
-			} else if(id.equals("ringColor")) {
-				definition.addAttributeDefinition(ObjectClassDefinition.REQUIRED,
-						new BasicAttributeDefinition("ringColor", "Ring Color", "The label for the ring color property", AttributeDefinition.STRING, nodeAttributesArray, nodeAttributesArray));
-			} else if(id.equals("nodeShape")) {
-				definition.addAttributeDefinition(ObjectClassDefinition.REQUIRED,
-						new BasicAttributeDefinition("nodeShape", "Node Shape", "The label for the node shape property", AttributeDefinition.STRING, nodeAttributesArray, nodeAttributesArray));
-			} else {
-				definition.addAttributeDefinition(ObjectClassDefinition.REQUIRED, definitions[ii]);
-			}
-		}
 
-		MetaTypeProvider provider = new BasicMetaTypeProvider(definition);
-		return provider;
+		Graph graph = (Graph) dm[0].getData();
+
+		if(Arrays.binarySearch(originalProvider.getPids(), this.pid) >= 0) {
+			ObjectClassDefinition oldDefinition = originalProvider.getObjectClassDefinition(this.pid, null);
+
+			BasicObjectClassDefinition definition;
+			try {
+				definition = new BasicObjectClassDefinition(oldDefinition.getID(), oldDefinition.getName(), oldDefinition.getDescription(), oldDefinition.getIcon(16));
+			} catch (IOException e) {
+				definition = new BasicObjectClassDefinition(oldDefinition.getID(), oldDefinition.getName(), oldDefinition.getDescription(), null);
+			}
+
+			String[] nodeAttributesArray = createKeyArray(graph.getNodeTable().getSchema());
+			String[] edgeAttributesArray = createKeyArray(graph.getEdgeTable().getSchema());
+
+			System.err.println(oldDefinition.getID());
+
+			AttributeDefinition[] definitions = oldDefinition.getAttributeDefinitions(ObjectClassDefinition.ALL);
+
+			for(int ii = 0; ii < definitions.length; ii++) {
+				String id = definitions[ii].getID();
+				System.err.println(id);
+				if(id.equals("nodeSize")) {
+					definition.addAttributeDefinition(ObjectClassDefinition.REQUIRED,
+							new BasicAttributeDefinition("nodeSize", "Node Size", "The label for the node size property", AttributeDefinition.STRING, nodeAttributesArray, nodeAttributesArray));
+				} else if(id.equals("edgeSize")) {
+					definition.addAttributeDefinition(ObjectClassDefinition.REQUIRED,
+							new BasicAttributeDefinition("edgeSize", "Edge Size", "The label for the edge size property", AttributeDefinition.STRING, edgeAttributesArray, edgeAttributesArray));
+				} else if(id.equals("nodeColor")) {
+					definition.addAttributeDefinition(ObjectClassDefinition.REQUIRED,
+							new BasicAttributeDefinition("nodeColor", "Node Color", "The label for the node color property", AttributeDefinition.STRING, nodeAttributesArray, nodeAttributesArray));
+				} else if(id.equals("edgeColor")) {
+					definition.addAttributeDefinition(ObjectClassDefinition.REQUIRED,
+							new BasicAttributeDefinition("edgeColor", "Edge Color", "The label for the edge color property", AttributeDefinition.STRING, edgeAttributesArray, edgeAttributesArray));
+				} else if(id.equals("ringColor")) {
+					definition.addAttributeDefinition(ObjectClassDefinition.REQUIRED,
+							new BasicAttributeDefinition("ringColor", "Ring Color", "The label for the ring color property", AttributeDefinition.STRING, nodeAttributesArray, nodeAttributesArray));
+				} else if(id.equals("nodeShape")) {
+					definition.addAttributeDefinition(ObjectClassDefinition.REQUIRED,
+							new BasicAttributeDefinition("nodeShape", "Node Shape", "The label for the node shape property", AttributeDefinition.STRING, nodeAttributesArray, nodeAttributesArray));
+				} else {
+					definition.addAttributeDefinition(ObjectClassDefinition.REQUIRED, definitions[ii]);
+				}
+			}
+
+			MetaTypeProvider provider = new BasicMetaTypeProvider(definition);
+			return provider;
+		} else {
+			return null;
+		}
 	}
 
 	/**
