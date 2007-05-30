3a36c89aabd7be75c095957a4cec1020830e712a
Mayur Masrani <masterkh@indiana.edu>
2007-05-30 19:23:05 +0000

    Fixed this package so that edgelists can have multi-word quoted strings as nodes.  An example of such an edgelist:
    "node one"	"node two"
    "node three"	"node four"
    "node five"	"node six"
    
    There are a ton of printlns left in here but commented out.  I'm gonna remove those as soon as I'm comfortable with the overall performance of this package to a greater degree.  Right now it works pretty well, but there are some issues about how it deals with badly formatted files.  Namely, this package needs to recognize more instances of bad files and throw an error/popup a GUI dialog.

--- trunk/plugins/converter/edu.iu.nwb.converter.edgelist/src/edu/iu/nwb/converter/edgelist/EdgeListToNWB.java
+++ trunk/plugins/converter/edu.iu.nwb.converter.edgelist/src/edu/iu/nwb/converter/edgelist/EdgeListToNWB.java
@@ -49,36 +49,140 @@ public class EdgeListToNWB implements Algorithm {
         this.logger = (LogService)ciContext.getService(LogService.class.getName());
     }
 
+    /* handle this merging of tokens with tail recursion */
+    public String[] tokenmanage (String[] tokens, int startFrom) {
+    	//System.out.println("entering tokenmanage\n");
+    	int i;
+    	int j;
+    	String newtokens[];
+    	boolean breakflag = false;
+    	for (i = startFrom;i < tokens.length;i++) {
+    		if (breakflag) break;
+    		// find "\"\w" tokens (position i) and match them with end tokens (position j)
+    		// once matched and merged, set i = j, rinse and repeat
+    		if (tokens[i].matches("\".*\"")) { // it's  a single word, quoted
+    			// continue
+    			//System.out.println("token "+i+" MATCHES: single word, quoted.\n");
+    		}
+    		else if (tokens[i].matches("\".*\".*")) { // end-quote happens in the middle of the token
+    			// probably we should just continue...
+    			//System.out.println("token "+i+" MATCHES: end-quote in middle of token\n");
+    		}
+    		else if (tokens[i].startsWith("\"")) {
+    			// starts with ", but since previous ifs didn't match, we know it doesn't end
+    			// with " or have a " in the middle
+    			//System.out.println("token "+i+" MATCHES: multi-word string");
+    			j = i+1;
+    			if (j < tokens.length) {
+    				for (;j < tokens.length;j++) {
+    					if (tokens[j].matches(".*\"")) {
+    						newtokens = tokenmerge(tokens,i,j);
+    						return tokenmanage(newtokens, i+1);
+    		/*				breakflag = true;
+    						break;*/
+
+    					}
+    				}
+    			}
+    		}
+
+    	}
+    	/*
+    	if (i < tokens.length - 1 && breakflag) { // we broke from the loop, and there is more work to do
+    		return tokenmanage(newtokens, i+1);
+    	}*/
+    	return tokens;
+    }
     
+    /*
+     * Takes a String[] of tokens and merges all tokens from index i to index j
+     * Returns the number of 
+     */
+    public String[] tokenmerge(String[] tokens, int i, int j) {
+    	//System.out.println("entering tokenmerge, i = "+i+", j = "+j+"\n");
+    	int x,y;
+    	String [] out = new String[tokens.length - (j - i)];
+    	String merged = "";
+    	for (y = i;y < j+1;y++){
+    		//System.out.println("-  merging.  y="+y+"; j="+j+"\n");
+    	    if (y == j) {
+    	    	merged = merged.concat(tokens[y]);
+    	    } else {
+    	    	merged = merged.concat(tokens[y] + " ");	
+    	    }
+    		
+    	}
+    	for (x=0;x < tokens.length - (j - i);x++) {
+    		//System.out.println("-  constructing result string.  x="+x+"\n-  length of tokens = "+tokens.length+"; (j-i) = "+(j-i)+"\n");
+    		if (x < i) {
+    			out[x] = tokens[x];
+    		}
+    		else if (x == i) {
+    			out[x] = merged;
+    		} else { // x > i
+    			out[x] = tokens[x + (j - i)];
+    		}
+    	}
+    	return out;
+    }
     
     /*
      * Reads edgelist data in from reader and outputs it as NWB data to writer
      */
-    public void transform(BufferedReader reader, BufferedWriter writer) 
+    public void transform(BufferedReader reader, BufferedWriter writer)  
     	throws IOException {
     	ArrayList edgelist = new ArrayList();
 		String currentLine;
-		String[] tokens;
+		String[] tokens;	
 		int i=0,edgesCount=0;
 		HashMap map = new HashMap();
 		int mapCount = 1;
 		int weightCount = 0;
+		int l;
+		String [] mergedTokens;
+		int lineCounter = 1;
+		boolean badFormat = false;
 		
 		try {
 			while ((currentLine = reader.readLine()) != null) {
-
+				if (currentLine.matches("\\s*")) {
+					continue;
+				}
+				//System.out.println("...line "+lineCounter+"\n");
 				tokens = currentLine.trim().split("\\s+");
-			
+				if (tokens.length < 2) {
+					badFormat = true;
+					break;
+				}
+				mergedTokens = tokenmanage(tokens,0);
+				
+				//System.out.println("Merged tokens: \n");
+				//for (l = 0; l < mergedTokens.length;l++ ) {
+					//System.out.println("*  "+mergedTokens[l]+"\n");
+				//}
+				
+				// need to merge tokens that look like "\"\w"  with ending-quoted tokens that look like
+				// "\w\"" and all those in between...
+				// this should rewrite the array, tokens
+				// so that the following code can work (relatively) unchanged
 
-				for(i = 0;i < tokens.length;i++){
-					if (!map.containsKey(tokens[i]) && tokens[i].matches("\".*\"") && i < 2) 
+				for(i = 0;i < mergedTokens.length;i++){
+					if (!map.containsKey(mergedTokens[i]) && mergedTokens[i].matches("\".*\"") && i < 2) 
 					{ //	looks like "xyz" but we are not in third column
-						map.put(tokens[i], new Integer(mapCount++));
+						map.put(mergedTokens[i], new Integer(mapCount++));
+						//System.out.println(">  "+mergedTokens[i]+" looks QUOTED\n");
+						//System.out.println(">  put "+mergedTokens[i]+" with "+(mapCount-1)+"\n");
+						
 					}
-					else if (!map.containsKey(tokens[i]) && i < 2)
+					else if (!map.containsKey("\""+mergedTokens[i]+"\"") && i < 2)
 					{ // we are not in third column, unquoted data 
-						tokens[i] ="\""+tokens[i]+"\""; 
-						map.put(tokens[i], new Integer(mapCount++));
+						//System.out.println(">  "+mergedTokens[i]+" looks unquoted\n");
+						
+						mergedTokens[i] ="\""+mergedTokens[i]+"\""; 
+						map.put(mergedTokens[i], new Integer(mapCount++));
+						//System.out.println(">  put "+mergedTokens[i]+" with "+(mapCount-1)+"\n");
+					} else if (map.containsKey("\""+mergedTokens[i]+"\"") && i < 2) {
+						mergedTokens[i] ="\""+mergedTokens[i]+"\"";
 					}
 
 					if (i > 1) {
@@ -86,13 +190,40 @@ public class EdgeListToNWB implements Algorithm {
 					}
 				}
 
+				//System.out.println("adding current mergedTokens, length = "+mergedTokens.length+"\n");
 				
-				edgelist.add(tokens);
+				for (int j = 0;j< mergedTokens.length;j++) {
+					//System.out.println("*  adding mergedToken["+j+"] = "+mergedTokens[j]+" to edgelist\n");
+				}
+				edgelist.add(mergedTokens);
 				edgesCount++;
-				
+				lineCounter++;
+			}
+			if (badFormat) {
+				GUIBuilderService guiBuilder =   (GUIBuilderService)ciContext.getService(GUIBuilderService.class.getName());
+				guiBuilder.showError("Bad NWB Format",
+						"Sorry, your file does not comply with edge-list format specifications.",
+						"Sorry, your file does not comply with edge-list format specifications.\n"+
+						"Please review the latest edge-list format specification at "+
+						"http://nwb.slis.indiana.edu/software.html, and update your file. \n"
+				);
+				throw (new IOException("Improperly formatted edgelist file"));
 			}
 			// currentLine is null
-
+			//System.out.println("&  done with while\n");
+			//for (l=0 ;l < map.size(); l++) {
+				//System.out.println("-  map keys: "+map.keySet().toString()+"\n");
+				//System.out.println("-  map entries: "+map.entrySet().toString()+"\n");
+				/*for (l =0;l < edgelist.size();l++) {
+					int j;
+					System.out.println("edgelist "+l+"'s length: "+((String[])(edgelist.get(l))).length+"\n");
+					for (j=0;j < ((String[])(edgelist.get(l))).length;j++){
+						System.out.println("-  next in edgelist entry: "+ ((String[])(edgelist.get(l)))[j] +"\n");	
+					}
+					
+				}*/
+				
+			//}
 			writer.write("*Nodes "+map.size()+"\n");
 			writer.write("id*int  label*string\n");
 			Iterator it = map.entrySet().iterator();
@@ -107,15 +238,30 @@ public class EdgeListToNWB implements Algorithm {
 			} else {// weightCount > 1
 				writer.write("source*int  target*int  weight*int\n");
 			}
-			for (i=0;i<edgesCount;i++){
-			
+			for (i=0;i<edgelist.size();i++){
+				//System.out.println("&  In writer for-loop.  i="+i+"\n");
 				if (((String[])(edgelist.get(i))).length > 2) { // this tuple has a weight value
-					writer.write(((Integer)map.get(((String[])(edgelist.get(i)))[0])).intValue() + " " + ((Integer)map.get(((String[])(edgelist.get(i)))[1])).intValue() + " " + ((String[])(edgelist.get(i)))[2] +"\n");
+					//System.out.println("^  here\n");
+					try {
+					
+						writer.write(((Integer)map.get(((String[])(edgelist.get(i)))[0])).intValue() + " " + ((Integer)map.get(((String[])(edgelist.get(i)))[1])).intValue() + " " + ((String[])(edgelist.get(i)))[2] +"\n");
+					} catch (NullPointerException e) {
+						//System.out.println("caught at 1\n");
+						e.printStackTrace();
+					}
 				} else { // only source, target were found in this tuple
-					writer.write(((Integer)map.get(((String[])(edgelist.get(i)))[0])).intValue() + " " + ((Integer)map.get(((String[])(edgelist.get(i)))[1])).intValue() +"\n");
+					//System.out.println("^  here2\n");
+					try {
+						writer.write(((Integer)map.get(((String[])(edgelist.get(i)))[0])).intValue() + " " + ((Integer)map.get(((String[])(edgelist.get(i)))[1])).intValue() +"\n");
+					} catch (NullPointerException e) {
+						//System.out.println("caught.  length of string[] in this edge = "+((String[])(edgelist.get(i))).length+"\n");
+						e.printStackTrace();
+					}
+					
 				}
 
-			}						
+			}			
+			//System.out.println("^  flushing\n");
 			writer.flush();
 			
 		} catch (IOException e) {
