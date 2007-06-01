53895c9d13d64e1f966ac3a1d4665c0762ee9d7b
Mayur Masrani <masterkh@indiana.edu>
2007-06-01 12:20:40 +0000

    Validation of edgelists is handled more intelligently, though the logic used is still somewhat stupid.  More cleaning of redundant code should be done presently.  Validation now occurs on load, and catches occurrences of several bad cases of edgelist formats.

--- trunk/plugins/converter/edu.iu.nwb.converter.edgelist/src/edu/iu/nwb/converter/edgelist/EdgeListToNWB.java
+++ trunk/plugins/converter/edu.iu.nwb.converter.edgelist/src/edu/iu/nwb/converter/edgelist/EdgeListToNWB.java
@@ -51,7 +51,7 @@ public class EdgeListToNWB implements Algorithm {
 
     /* handle this merging of tokens with tail recursion */
     public String[] tokenmanage (String[] tokens, int startFrom) {
-    	//System.out.println("entering tokenmanage\n");
+    	
     	int i;
     	int j;
     	String newtokens[];
@@ -62,16 +62,16 @@ public class EdgeListToNWB implements Algorithm {
     		// once matched and merged, set i = j, rinse and repeat
     		if (tokens[i].matches("\".*\"")) { // it's  a single word, quoted
     			// continue
-    			//System.out.println("token "+i+" MATCHES: single word, quoted.\n");
+    	
     		}
     		else if (tokens[i].matches("\".*\".*")) { // end-quote happens in the middle of the token
     			// probably we should just continue...
-    			//System.out.println("token "+i+" MATCHES: end-quote in middle of token\n");
+    	
     		}
     		else if (tokens[i].startsWith("\"")) {
     			// starts with ", but since previous ifs didn't match, we know it doesn't end
     			// with " or have a " in the middle
-    			//System.out.println("token "+i+" MATCHES: multi-word string");
+    	
     			j = i+1;
     			if (j < tokens.length) {
     				for (;j < tokens.length;j++) {
@@ -87,10 +87,6 @@ public class EdgeListToNWB implements Algorithm {
     		}
 
     	}
-    	/*
-    	if (i < tokens.length - 1 && breakflag) { // we broke from the loop, and there is more work to do
-    		return tokenmanage(newtokens, i+1);
-    	}*/
     	return tokens;
     }
     
@@ -99,7 +95,7 @@ public class EdgeListToNWB implements Algorithm {
      * Returns the number of 
      */
     public String[] tokenmerge(String[] tokens, int i, int j) {
-    	//System.out.println("entering tokenmerge, i = "+i+", j = "+j+"\n");
+    	
     	int x,y;
     	String [] out = new String[tokens.length - (j - i)];
     	String merged = "";
@@ -113,7 +109,7 @@ public class EdgeListToNWB implements Algorithm {
     		
     	}
     	for (x=0;x < tokens.length - (j - i);x++) {
-    		//System.out.println("-  constructing result string.  x="+x+"\n-  length of tokens = "+tokens.length+"; (j-i) = "+(j-i)+"\n");
+    		//constructing result string.  
     		if (x < i) {
     			out[x] = tokens[x];
     		}
@@ -148,18 +144,12 @@ public class EdgeListToNWB implements Algorithm {
 				if (currentLine.matches("\\s*")) {
 					continue;
 				}
-				//System.out.println("...line "+lineCounter+"\n");
 				tokens = currentLine.trim().split("\\s+");
 				if (tokens.length < 2) {
 					badFormat = true;
 					break;
 				}
 				mergedTokens = tokenmanage(tokens,0);
-				
-				//System.out.println("Merged tokens: \n");
-				//for (l = 0; l < mergedTokens.length;l++ ) {
-					//System.out.println("*  "+mergedTokens[l]+"\n");
-				//}
 				
 				// need to merge tokens that look like "\"\w"  with ending-quoted tokens that look like
 				// "\w\"" and all those in between...
@@ -170,17 +160,13 @@ public class EdgeListToNWB implements Algorithm {
 					if (!map.containsKey(mergedTokens[i]) && mergedTokens[i].matches("\".*\"") && i < 2) 
 					{ //	looks like "xyz" but we are not in third column
 						map.put(mergedTokens[i], new Integer(mapCount++));
-						//System.out.println(">  "+mergedTokens[i]+" looks QUOTED\n");
-						//System.out.println(">  put "+mergedTokens[i]+" with "+(mapCount-1)+"\n");
 						
 					}
 					else if (!map.containsKey("\""+mergedTokens[i]+"\"") && i < 2)
 					{ // we are not in third column, unquoted data 
-						//System.out.println(">  "+mergedTokens[i]+" looks unquoted\n");
 						
 						mergedTokens[i] ="\""+mergedTokens[i]+"\""; 
 						map.put(mergedTokens[i], new Integer(mapCount++));
-						//System.out.println(">  put "+mergedTokens[i]+" with "+(mapCount-1)+"\n");
 					} else if (map.containsKey("\""+mergedTokens[i]+"\"") && i < 2) {
 						mergedTokens[i] ="\""+mergedTokens[i]+"\"";
 					}
@@ -190,11 +176,7 @@ public class EdgeListToNWB implements Algorithm {
 					}
 				}
 
-				//System.out.println("adding current mergedTokens, length = "+mergedTokens.length+"\n");
 				
-				for (int j = 0;j< mergedTokens.length;j++) {
-					//System.out.println("*  adding mergedToken["+j+"] = "+mergedTokens[j]+" to edgelist\n");
-				}
 				edgelist.add(mergedTokens);
 				edgesCount++;
 				lineCounter++;
@@ -210,20 +192,6 @@ public class EdgeListToNWB implements Algorithm {
 				throw (new IOException("Improperly formatted edgelist file"));
 			}
 			// currentLine is null
-			//System.out.println("&  done with while\n");
-			//for (l=0 ;l < map.size(); l++) {
-				//System.out.println("-  map keys: "+map.keySet().toString()+"\n");
-				//System.out.println("-  map entries: "+map.entrySet().toString()+"\n");
-				/*for (l =0;l < edgelist.size();l++) {
-					int j;
-					System.out.println("edgelist "+l+"'s length: "+((String[])(edgelist.get(l))).length+"\n");
-					for (j=0;j < ((String[])(edgelist.get(l))).length;j++){
-						System.out.println("-  next in edgelist entry: "+ ((String[])(edgelist.get(l)))[j] +"\n");	
-					}
-					
-				}*/
-				
-			//}
 			writer.write("*Nodes "+map.size()+"\n");
 			writer.write("id*int  label*string\n");
 			Iterator it = map.entrySet().iterator();
@@ -239,29 +207,23 @@ public class EdgeListToNWB implements Algorithm {
 				writer.write("source*int  target*int  weight*int\n");
 			}
 			for (i=0;i<edgelist.size();i++){
-				//System.out.println("&  In writer for-loop.  i="+i+"\n");
 				if (((String[])(edgelist.get(i))).length > 2) { // this tuple has a weight value
-					//System.out.println("^  here\n");
 					try {
 					
 						writer.write(((Integer)map.get(((String[])(edgelist.get(i)))[0])).intValue() + " " + ((Integer)map.get(((String[])(edgelist.get(i)))[1])).intValue() + " " + ((String[])(edgelist.get(i)))[2] +"\n");
 					} catch (NullPointerException e) {
-						//System.out.println("caught at 1\n");
 						e.printStackTrace();
 					}
 				} else { // only source, target were found in this tuple
-					//System.out.println("^  here2\n");
 					try {
 						writer.write(((Integer)map.get(((String[])(edgelist.get(i)))[0])).intValue() + " " + ((Integer)map.get(((String[])(edgelist.get(i)))[1])).intValue() +"\n");
 					} catch (NullPointerException e) {
-						//System.out.println("caught.  length of string[] in this edge = "+((String[])(edgelist.get(i))).length+"\n");
 						e.printStackTrace();
 					}
 					
 				}
 
 			}			
-			//System.out.println("^  flushing\n");
 			writer.flush();
 			
 		} catch (IOException e) {
--- trunk/plugins/converter/edu.iu.nwb.converter.edgelist/src/edu/iu/nwb/converter/edgelist/EdgeListValidatorFactory.java
+++ trunk/plugins/converter/edu.iu.nwb.converter.edgelist/src/edu/iu/nwb/converter/edgelist/EdgeListValidatorFactory.java
@@ -1,8 +1,10 @@
 package edu.iu.nwb.converter.edgelist;
 
 //Java
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Dictionary;
 //OSGi
@@ -15,6 +17,8 @@ import org.cishell.framework.algorithm.AlgorithmFactory;
 import org.cishell.framework.data.BasicData;
 import org.cishell.framework.data.Data;
 import org.cishell.framework.data.DataProperty;
+import org.cishell.service.guibuilder.GUIBuilderService;
+
 
 
 
@@ -22,7 +26,7 @@ import org.cishell.framework.data.DataProperty;
  * @author Weixia(Bonnie) Huang 
  */
 public class EdgeListValidatorFactory implements AlgorithmFactory {
-	   
+	
 	   protected void activate(ComponentContext ctxt) {}
 	   protected void deactivate(ComponentContext ctxt) { }
 	
@@ -55,18 +59,227 @@ public class EdgeListValidatorFactory implements AlgorithmFactory {
 
 	        public Data[] execute() {
 
+	        	GUIBuilderService guiBuilder = 
+	    			(GUIBuilderService)ciContext.getService(GUIBuilderService.class.getName());
 				String fileHandler = (String) data[0].getData();
+				
 				File inData = new File(fileHandler);
+				ValidateEdgeFile validator = new ValidateEdgeFile();
+				try{ 
+					validator.validateEdgeFormat(inData);
 
-				Data[] dm = new Data[] {new BasicData(inData, "file:text/edge")};
-				dm[0].getMetaData().put(DataProperty.LABEL, "edge file: " + fileHandler);
-				dm[0].getMetaData().put(DataProperty.TYPE, DataProperty.NETWORK_TYPE);
-            	return dm;
+					if(validator.getValidationResult()){	
+
+						Data[] dm = new Data[] {new BasicData(inData, "file:text/edge")};
+						dm[0].getMetaData().put(DataProperty.LABEL, "edge file: " + fileHandler);
+						dm[0].getMetaData().put(DataProperty.TYPE, DataProperty.NETWORK_TYPE);
+						return dm;
+
+					} else {
+						return null;
+					}
+
+				} catch (FileNotFoundException e){
+					guiBuilder.showError("File Not Found Exception", 
+							"Got an File Not Found Exception",e);	
+					return null;
+				} catch (IOException ioe){
+					guiBuilder.showError("IOException", 
+							"Got an IOException",ioe);
+					return null;
+				} catch (edu.iu.nwb.converter.edgelist.EdgeListValidatorFactory.ValidateEdgeFile.EdgeFormat ef) {
+					System.out.println(">>>wrong format: "+validator.getErrorMessages());
+					guiBuilder.showError("Bad edgelist Format", 
+							"Sorry, your file does not comply with the edgelist file format specification.",
+							"Sorry, your file does not comply with the edgelist file format specification.\n"+
+							"Please review the latest edgelist file format Specification at "+
+							"https://nwb.slis.indiana.edu/community/?n=LoadData.Edgelist, and update your file. \n"+
+							validator.getErrorMessages());
+					return null;	
+				}
+				
+				
 
 	        }
 	      
 
 	    }
+	   
+
+
+	   public class ValidateEdgeFile {
+		   
+		   private int currentLine;
+		   private StringBuffer errorMessages = new StringBuffer();
+		   private boolean isFileGood = true;
+
+		   /* handle this merging of tokens with tail recursion */
+		   public String[] tokenmanage (String[] tokens, int startFrom) {
+			   //System.out.println("entering tokenmanage\n");
+			   int i;
+			   int j;
+			   String newtokens[];
+			   boolean breakflag = false;
+			   for (i = startFrom;i < tokens.length;i++) {
+				   if (breakflag) break;
+				   // find "\"\w" tokens (position i) and match them with end tokens (position j)
+				   // once matched and merged, set i = j, rinse and repeat
+				   if (tokens[i].matches("\".*\"")) { // it's  a single word, quoted
+					   // continue
+				
+				   }
+				   else if (tokens[i].matches("\".*\".*")) { // end-quote happens in the middle of the token
+					   // probably we should just continue...
+				
+				   }
+				   else if (tokens[i].startsWith("\"")) {
+					   // starts with ", but since previous ifs didn't match, we know it doesn't end
+					   // with " or have a " in the middle
+				
+					   j = i+1;
+					   if (j < tokens.length) {
+						   for (;j < tokens.length;j++) {
+							   if (tokens[j].matches(".*\"")) {
+								   newtokens = tokenmerge(tokens,i,j);
+								   return tokenmanage(newtokens, i+1);
+								   /*				breakflag = true;
+		    						break;*/
+
+							   }
+						   }
+					   }
+				   }
+
+			   }
+			   return tokens;
+		   }
+
+		   /*
+		    * Takes a String[] of tokens and merges all tokens from index i to index j
+		    * Returns the number of 
+		    */
+		   public String[] tokenmerge(String[] tokens, int i, int j) {
+			   int x,y;
+			   String [] out = new String[tokens.length - (j - i)];
+			   String merged = "";
+			   for (y = i;y < j+1;y++){
+				   if (y == j) {
+					   merged = merged.concat(tokens[y]);
+				   } else {
+					   merged = merged.concat(tokens[y] + " ");	
+				   }
+
+			   }
+			   for (x=0;x < tokens.length - (j - i);x++) {
+				   if (x < i) {
+					   out[x] = tokens[x];
+				   }
+				   else if (x == i) {
+					   out[x] = merged;
+				   } else { // x > i
+					   out[x] = tokens[x + (j - i)];
+				   }
+			   }
+			   return out;
+		   }
+
+
+
+		   public String getErrorMessages() {
+			   return errorMessages.toString();
+		   }
+
+		   public boolean getValidationResult() {
+			   return isFileGood;
+		   }
+
+		  
+		   /*
+		    * validateEdgeFormat invokes processFile with a BufferedReader corresponding to the file
+		    * that we will validate.
+		    * -- Felix Terkhorn
+		    * -- May 31 2007
+		    */
+		   public void validateEdgeFormat(File fileHandler)
+		   throws FileNotFoundException, IOException, EdgeFormat {
+			   //System.out.println("inside validateEdgeFormat\n");
+			   currentLine = 0;
+			   BufferedReader reader = new BufferedReader(new FileReader(fileHandler));
+			   try {
+				   this.processFile(reader);
+			   } catch (EdgeFormat ef) {
+				   throw ef;
+			   }
+		   }
+		   
+		   public class EdgeFormat extends Exception {
+			   
+		   }
+		   
+		   public void processFile(BufferedReader reader) throws EdgeFormat, IOException {
+			   //System.out.println("inside processFile method\n");
+			   String line = reader.readLine();
+
+			   while (line != null && isFileGood) {
+				   currentLine++;
+				   //System.out.println("inside while loop at processFile. currentLine = "+currentLine+"\n");
+				   // process section header that looks like
+				   // *nodes or *nodes 1000
+				   if (this.validateEdge(line) && isFileGood) {
+					   //System.out.println("inside validateEdge clause\n");
+					   processEdge(line);
+					   if (isFileGood) {
+						   this.checkFile();
+					   }
+					   line = reader.readLine();
+					   continue;
+				   } else {
+					   throw new EdgeFormat();
+				   }
+				 
+				   
+			   }
+		   }
+
+		   /* 
+		    * This method saves information about the edge in this object
+		    */
+		   public void processEdge(String s) {
+			   
+		   }
+		   
+		   /*
+		    * This method checks to see if the given line validates edgelist rules.
+		    * Invalid edge formats:
+		    * 	case1-> node						  			<single node line>
+		    * 	case2-> "node1 node2							<no matching end-quote in this line>
+		    * 	case3-> node1 node2"							<no matching begin-quote in this line>
+		    * 	case4-> "" node2								<empty node name>
+		    * 	case5-> node1	node2	three				<weight must be an int OR float value>
+		    */
+		   public boolean validateEdge(String s) {
+			   //System.out.println("inside validateEdge\n");
+			   String[] tokens = tokenmanage(s.trim().split("\\s+"),0);
+			   if (tokens.length < 2) { // case 1 
+				   return false;
+			   } else if (s.matches("[^\"]*\"[^\"]*")) { // cases 2 & 3
+				   return false;
+			   } else if (s.matches(".*\"\".*")) { // case 4
+				   return false;
+			   } else if (tokens.length > 2 && !tokens[2].matches("[0-9]+")) { // case 5
+				   return false;
+			   } else {  // if none of the above clauses are true, this line is OK.
+				   return true;
+			   }
+		   }
+		   
+		   public void checkFile() {
+				
+				// not sure
+			}
+		   
+	   }
+
 }
 
 
