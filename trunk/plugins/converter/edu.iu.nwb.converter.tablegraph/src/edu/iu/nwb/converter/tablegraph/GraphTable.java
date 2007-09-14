9c7648a7f69eeffcdb98f5ba748a07395178aeb9
Russell Duhon <rduhon@indiana.edu>
2007-09-14 15:56:18 +0000

    Converts (but does not have the converter property set) from Graphs to Node and Edge Tables. Also has some code that might eventually take a Table and make a Graph.

---
+++ trunk/plugins/converter/edu.iu.nwb.converter.tablegraph/src/edu/iu/nwb/converter/tablegraph/GraphTable.java
@@ -0,0 +1,85 @@
+package edu.iu.nwb.converter.tablegraph;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.MutableTreeNode;
+import javax.swing.tree.TreeNode;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.INIConfiguration;
+import org.cishell.framework.CIShellContext;
+import org.cishell.framework.algorithm.Algorithm;
+import org.cishell.framework.data.BasicData;
+import org.cishell.framework.data.Data;
+import org.cishell.framework.data.DataProperty;
+import org.osgi.service.log.LogService;
+
+import cern.colt.function.DoubleDoubleFunction;
+import cern.colt.matrix.DoubleMatrix1D;
+import cern.colt.matrix.DoubleMatrix2D;
+import cern.colt.matrix.impl.DenseDoubleMatrix1D;
+import cern.colt.matrix.impl.DenseDoubleMatrix2D;
+import cern.jet.math.Functions;
+
+import prefuse.data.Graph;
+import prefuse.data.Schema;
+import prefuse.data.Table;
+
+public class GraphTable implements Algorithm {
+	Data[] data;
+	Dictionary parameters;
+	CIShellContext context;
+	private DateFormat format;
+
+	public GraphTable(Data[] data, Dictionary parameters, CIShellContext context) {
+		this.data = data;
+		this.parameters = parameters;
+		this.context = context;
+
+	}
+
+	public Data[] execute() {
+
+		LogService logger = (LogService)context.getService(LogService.class.getName());
+		
+		Graph graph = (Graph) this.data[0].getData();
+		
+		
+		
+		
+
+		Data nodeData = new BasicData(graph.getNodeTable(), Table.class.getName());
+		
+		Dictionary nodeMetadata = nodeData.getMetaData();
+		nodeMetadata.put(DataProperty.LABEL, "Node Table from Graph");
+		nodeMetadata.put(DataProperty.PARENT, this.data[0]);
+		nodeMetadata.put(DataProperty.TYPE, DataProperty.TEXT_TYPE);
+		
+		Data edgeData = new BasicData(graph.getNodeTable(), Table.class.getName());
+		
+		Dictionary edgeMetadata = edgeData.getMetaData();
+		edgeMetadata.put(DataProperty.LABEL, "Edge Table from Graph");
+		edgeMetadata.put(DataProperty.PARENT, this.data[0]);
+		edgeMetadata.put(DataProperty.TYPE, DataProperty.TEXT_TYPE);
+		
+
+		return new Data[]{nodeData, edgeData};
+	}
+}
