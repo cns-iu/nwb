fa1e9449a51427af9bfa2e31523a371b5ae62899
Russell Duhon <rduhon@indiana.edu>
2007-09-16 18:40:26 +0000

    Removed table to graph functionality (for now, at least) and unneeded dependencies.

--- trunk/plugins/converter/edu.iu.nwb.converter.tablegraph/src/edu/iu/nwb/converter/tablegraph/GraphTable.java
+++ trunk/plugins/converter/edu.iu.nwb.converter.tablegraph/src/edu/iu/nwb/converter/tablegraph/GraphTable.java
@@ -1,52 +1,21 @@
 package edu.iu.nwb.converter.tablegraph;
 
-import java.io.File;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.SortedMap;
-import java.util.SortedSet;
-import java.util.Stack;
-import java.util.TreeMap;
-import java.util.TreeSet;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.MutableTreeNode;
-import javax.swing.tree.TreeNode;
 
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.INIConfiguration;
 import org.cishell.framework.CIShellContext;
 import org.cishell.framework.algorithm.Algorithm;
 import org.cishell.framework.data.BasicData;
 import org.cishell.framework.data.Data;
 import org.cishell.framework.data.DataProperty;
-import org.osgi.service.log.LogService;
-
-import cern.colt.function.DoubleDoubleFunction;
-import cern.colt.matrix.DoubleMatrix1D;
-import cern.colt.matrix.DoubleMatrix2D;
-import cern.colt.matrix.impl.DenseDoubleMatrix1D;
-import cern.colt.matrix.impl.DenseDoubleMatrix2D;
-import cern.jet.math.Functions;
+//import org.osgi.service.log.LogService;
 
 import prefuse.data.Graph;
-import prefuse.data.Schema;
 import prefuse.data.Table;
 
 public class GraphTable implements Algorithm {
 	Data[] data;
 	Dictionary parameters;
 	CIShellContext context;
-	private DateFormat format;
 
 	public GraphTable(Data[] data, Dictionary parameters, CIShellContext context) {
 		this.data = data;
@@ -57,7 +26,7 @@ public class GraphTable implements Algorithm {
 
 	public Data[] execute() {
 
-		LogService logger = (LogService)context.getService(LogService.class.getName());
+		//LogService logger = (LogService)context.getService(LogService.class.getName());
 		
 		Graph graph = (Graph) this.data[0].getData();
 		
