7cce01d449a00e87a27f8c11b8762d69caaaec80
Timothy Kelley <kelleyt@indiana.edu>
2007-11-15 04:17:15 +0000

    Added some comments to clarify functions.

--- trunk/plugins/analysis/edu.iu.nwb.toolkit.networkanalysis/src/edu/iu/nwb/toolkit/networkanalysis/analysis/ComponentForest.java
+++ trunk/plugins/analysis/edu.iu.nwb.toolkit.networkanalysis/src/edu/iu/nwb/toolkit/networkanalysis/analysis/ComponentForest.java
@@ -5,9 +5,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
-import java.util.TreeSet;
 
-import prefuse.data.Edge;
 import prefuse.data.Graph;
 import prefuse.data.Node;
 
@@ -16,14 +14,14 @@ public class ComponentForest{
 	int weakComponentClusters = 0;
 	int maxWeakConnectedNodes = 0;
 
-	int strongComponentClusters = 0;
-	int maxStrongConnectedNodes = 0;
+	//int strongComponentClusters = 0;
+	//int maxStrongConnectedNodes = 0;
 
 	int weakNodes;
-	int strongNodes;
+	//int strongNodes;
 
 	BigDecimal averageWeakConnectedness = new BigDecimal(0.0);
-	BigDecimal averageStrongConnectedness = new BigDecimal(0.0);
+	//BigDecimal averageStrongConnectedness = new BigDecimal(0.0);
 
 	public ComponentForest(){
 
@@ -66,7 +64,7 @@ public class ComponentForest{
 					if(tree.size() > maxNodes)
 						maxNodes = tree.size();
 					
-					clusters.put(new Integer(cluster), new HashSet(tree));
+					clusters.put(new Integer(cluster), new HashSet(tree)); 
 					cluster++;
 				}
 			
@@ -77,198 +75,7 @@ public class ComponentForest{
 		this.maxWeakConnectedNodes = maxNodes;
 		return clusters;
 	}
-/*
-	private void strongComponentAssignment(final LinkedHashSet connectedNodes){
-		for(int ii = 0; ii < connectedNodes.size(); ii++){
-		}
-		if(connectedNodes.size() > this.maxStrongConnectedNodes)
-			this.maxStrongConnectedNodes = connectedNodes.size();
-		this.strongNodes += connectedNodes.size();	
-	}
-
-	private void testPrinter(final Queue first, final Queue second, final TreeSet seen, final Queue adding){
-		String s1 = "";
-		String s2 = "";
-		String s3 = "";
-		String s4 = "";
-		String s5 = "";
-		ArrayList firstArray = null;  //= first.toArray();
-		ArrayList secondArray = null;  //= second.toArray();
-		ArrayList addingArray = null;  //= adding.toArray();
-		int x = 0;
-		System.out.println("First Pass\tSecond Pass\tSeen?\tSeen Nodes\tAdding");
-		if(first != null && second != null){
-			firstArray = new ArrayList(first);
-			secondArray = new ArrayList(second);
-			x = Math.max(first.size(), Math.max(second.size(), seen.size()));
-		}
-		else if(first != null && second == null){
-			firstArray =  new ArrayList(first);
-			x = Math.max(first.size(), seen.size());
-		}
-		else if(second != null && first == null){
-			secondArray = new ArrayList(second);
-			x = Math.max(second.size(), seen.size());
-		}
-		else
-			x = seen.size();
-
-		if(adding != null){
-			addingArray = new ArrayList(adding);
-			x = Math.max(x, adding.size());
-		}
-		int ii = 0;
-		for(Iterator it3 = seen.iterator(); it3.hasNext();){
-			if(firstArray != null && ii < firstArray.size()){
-				s1 = firstArray.get(ii).toString();
-				s3 = new Boolean(seen.contains(firstArray.get(ii))).toString();
-			}
-			else{
-				s1 = s3 = "";
-			}
-			if(secondArray != null && ii < secondArray.size()){
-				s2 = secondArray.get(ii).toString();
-			}
-			else
-				s2 = "";
-			if(addingArray != null && ii < addingArray.size()){
-				s5 = addingArray.get(ii).toString();
-			}
-			else
-				s5 = "";
-			s4 = it3.next().toString();
-
-			System.out.println(s1 +"\t\t" + s2 + "\t\t" + s3 + "\t" + s4+"\t\t"+s5);
-
-			ii++;
-		}
-		for(int iii = ii;iii < x; iii++){
-			if(firstArray != null && iii < firstArray.size()){
-				s1 = firstArray.get(iii).toString();
-				s3 = new Boolean(seen.contains(firstArray.get(iii))).toString();
-			}
-			else{
-				s1 = s3 = "";
-			}
-			if(secondArray != null && iii < secondArray.size()){
-				s2 = secondArray.get(iii).toString();
-			}
-			else
-				s2 = "";
-			if(addingArray != null && iii < addingArray.size()){
-				s5 = addingArray.get(iii).toString();
-			}
-			else
-				s5 = "";
-
-			s4 = "";
-
-			System.out.println(s1 +"\t\t" + s2 + "\t\t" + s3 + "\t" + s4 + "\t\t" + s5);
-
-		}
-	}
 
 
-*/
-	private boolean strongComponentArrayCalc(TreeSet seenNodes,Graph g1,Graph g2){
-	/*	Queue strongTemp;		                    		 
-		LinkedHashSet secondPass = null;
-		LinkedHashSet firstPass = null;
-
-
-		for(Iterator it = g1.nodes(); it.hasNext();){
-			firstPass = null;
-			secondPass = null;
-			Node n = (Node)it.next();
-
-
-			Integer i = new Integer(n.getRow());
-
-			if(!seenNodes.contains(i)){
-				firstPass = NetworkProperties.dDFS(g1, i);
-				firstPass.removeAll(seenNodes);
-				//if(firstPass.size() != g1.getNodeCount()){
-				if(firstPass.size() == 1){
-					//return false;
-					strongComponentAssignment(firstPass);
-					//testPrinter(firstPass,secondPass,seenNodes,firstPass);
-					seenNodes.addAll(firstPass);
-					this.strongComponentClusters++;
-
-				}else{
-					while(!firstPass.isEmpty()){
-						Integer ii = (Integer)firstPass.;
-						if(!seenNodes.contains(ii)){
-							secondPass = NetworkProperties.dDFS(g2,ii);
-							secondPass.removeAll(seenNodes);
-							if(!ii.equals(i))
-								secondPass.retainAll(dDFS(g1,ii));
-							//if(secondPass.size() != firstPass.size()){
-							if(secondPass.size() == 1){
-								//return false;
-								strongComponentAssignment(secondPass);
-								seenNodes.addAll(secondPass);
-								//testPrinter(firstPass,secondPass,seenNodes,secondPass);
-
-								this.strongComponentClusters++;
-							}
-							else{
-
-								if(secondPass.size() <= firstPass.size()){
-
-									strongTemp = new LinkedList(firstPass);
-									strongTemp.retainAll(secondPass);
-
-
-								}
-								else{
-									strongTemp = new LinkedList(secondPass);
-									strongTemp.retainAll(firstPass);
-
-								}
-								//testPrinter(firstPass,secondPass,seenNodes,strongTemp);
-								strongComponentAssignment(strongTemp);
-								firstPass.removeAll(strongTemp);
-								secondPass.removeAll(strongTemp);
-								secondPass.removeAll(seenNodes);
-								seenNodes.addAll(strongTemp);
-								while(!secondPass.isEmpty()){
-									Integer g = (Integer)secondPass.poll();
-									seenNodes.add(g);
-									firstPass.remove(g);
-									this.strongNodes++;
-									this.strongComponentClusters++;
-								}
-
-								this.strongComponentClusters++;
-
-								if(strongTemp.size() == g1.getNodeCount())
-									return true;
-								else
-									return false;
-							}
-						}
-						firstPass.poll();
-					}
-				}
-			}
-		}*/
-		return true;
-	}
-
-	public boolean strongComponentCalculation(Graph grph){
-		TreeSet strongSeenNodes = new TreeSet();
-		Graph reverseGraph = new Graph(grph.getNodeTable(),true);
-		for(Iterator it = grph.edges(); it.hasNext();){
-			Edge e = (Edge)it.next();
-
-			reverseGraph.addEdge(e.getInt("target"),e.getInt("source"));
-		}
-
-		return(strongComponentArrayCalc(strongSeenNodes, grph, reverseGraph));
-
-
-
-	}
-
+	
 }
--- trunk/plugins/analysis/edu.iu.nwb.toolkit.networkanalysis/src/edu/iu/nwb/toolkit/networkanalysis/analysis/SelfLoopsParallelEdges.java
+++ trunk/plugins/analysis/edu.iu.nwb.toolkit.networkanalysis/src/edu/iu/nwb/toolkit/networkanalysis/analysis/SelfLoopsParallelEdges.java
@@ -13,36 +13,47 @@ public class SelfLoopsParallelEdges {
 	HashMap edges;
 	//int numberOfParallelEdges;
 	boolean directed = false;
-	
+
 	public SelfLoopsParallelEdges(boolean directed){
 		selfLoops = new HashSet();
 		parallelEdges = new HashSet();
 		edges = new HashMap();
 		this.directed = directed;
-	
+
 	}
+
+	/***
+	 * 
+	 * @param e
+	 * Here we take an Edge e, create a SimpleEdge object, which only stores
+	 * the source and target ids. We create a HashSet to hold edges.
+	 * We then link the SimpleEdge to the HashSet of real edges.
+	 */
+	
 	
 	public void addEdge(Edge e){
 		SimpleEdge se = new SimpleEdge(e);
-		
+
 		if(directed){
-		
-		if(edges.get(se) == null){
-			HashSet temp = new HashSet();
-			temp.add(e);
-			edges.put(se, temp);
-		}
-		else{
-			if(((HashSet)edges.get(se)).add(e)){
-			
-				parallelEdges.add(se);
+
+			if(edges.get(se) == null){ //We haven't seen this source and target
+				HashSet temp = new HashSet(); //create the HashSet to hold real edges.
+				temp.add(e);
+				edges.put(se, temp); //link real edges to source and target.
 			}
-			
-		}	
+			else{ //we've seen this source and target so we have a parallel edge.
+				if(((HashSet)edges.get(se)).add(e)){ //add the real edge to the mapping, 
+													//only if it is unique.
+
+					parallelEdges.add(se); 
+				}
+
+			}	
 		}
 		else{
+			//create a simple edge with the source and target swapped to handle undirected.
 			SimpleEdge se2 = new SimpleEdge(e.getTargetNode().getRow(), e.getSourceNode().getRow());
-			
+
 			if((edges.get(se) == null) && (edges.get(se2) == null)){
 				HashSet temp = new HashSet();
 				temp.add(e);
@@ -50,69 +61,69 @@ public class SelfLoopsParallelEdges {
 			}
 			else if((edges.get(se) != null)){
 				if(((HashSet)edges.get(se)).add(e)){
-					
+
 					parallelEdges.add(se);
 				}
 			}
 			else{
 				if(((HashSet)edges.get(se2)).add(e)){
-					
+
 					parallelEdges.add(se2);
 				}
 			}
-			
+
 		}
-		
+		//if the source and target are the same, add a selfLoop.
 		if(e.getSourceNode().getRow() == e.getTargetNode().getRow()){
 			selfLoops.add(e);
 		}
 	}
-	
+
 	public int getNumSelfLoops(){
 		return selfLoops.size();
 	}
-	
+
 	public int getNumParallelEdges(){
 		int numParallelEdges = 0;
-	
+
 		for(Iterator it1 = this.parallelEdges.iterator();it1.hasNext();){
 			SimpleEdge se = (SimpleEdge)it1.next();
 			numParallelEdges += ((HashSet)this.edges.get(se)).size();
 		}
-		
+
 		return numParallelEdges;
 	}
-	
+
 	public String printParallelEdges(){
 		StringBuffer sb = new StringBuffer();
 		for(Iterator it1 = this.parallelEdges.iterator();it1.hasNext();){
 			SimpleEdge se = (SimpleEdge)it1.next();
 			sb.append("There are " + ((HashSet)this.edges.get(se)).size() + " edges between node: \n\t");
 			Iterator it2 = ((HashSet)this.edges.get(se)).iterator();
-				Edge edg = (Edge)it2.next();
-				Node nd = edg.getSourceNode();
-				int columns = nd.getColumnCount();
-				for(int i = 0; i < columns; i++){
-					sb.append(nd.get(i) + " ");
-				}
-				sb.append(System.getProperty("line.separator"));
-				sb.append("and node: \n\t");
-				nd = edg.getTargetNode();
-				columns = nd.getColumnCount();
-				for(int i = 0; i < columns; i++){
-					sb.append(nd.get(i) + " ");
-				}
-				sb.append(System.getProperty("line.separator"));
-				//columns = edg.getSourceNode().getColumnCount();
-			
-				//sb.append(System.getProperty("line.separator"));
-			
-			
+			Edge edg = (Edge)it2.next();
+			Node nd = edg.getSourceNode();
+			int columns = nd.getColumnCount();
+			for(int i = 0; i < columns; i++){
+				sb.append(nd.get(i) + " ");
+			}
+			sb.append(System.getProperty("line.separator"));
+			sb.append("and node: \n\t");
+			nd = edg.getTargetNode();
+			columns = nd.getColumnCount();
+			for(int i = 0; i < columns; i++){
+				sb.append(nd.get(i) + " ");
+			}
+			sb.append(System.getProperty("line.separator"));
+			//columns = edg.getSourceNode().getColumnCount();
+
+			//sb.append(System.getProperty("line.separator"));
+
+
 		}
 		sb.append(System.getProperty("line.separator"));
 		return sb.toString();
 	}
-	
+
 	public String printSelfLoops(){
 		StringBuffer sb = new StringBuffer();
 		for(Iterator it = this.selfLoops.iterator(); it.hasNext();){
@@ -125,7 +136,7 @@ public class SelfLoopsParallelEdges {
 			}
 			sb.append("\nhas an edge with itself.");
 			sb.append(System.getProperty("line.separator"));
-			
+
 		}
 		sb.append(System.getProperty("line.separator"));
 		return sb.toString();
