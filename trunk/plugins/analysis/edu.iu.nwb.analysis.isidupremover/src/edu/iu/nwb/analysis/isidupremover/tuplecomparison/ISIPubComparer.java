4eb69fe3020d44c84f734b63e4bc3758677eaf52
Micah Linnemeier <mwlinnem@indiana.edu>
2007-11-27 22:24:12 +0000

    Had to make a new project do to weird configuration issues. Removing this one so the other (with the same name) can replace it.

--- trunk/plugins/analysis/edu.iu.nwb.analysis.isidupremover/src/edu/iu/nwb/analysis/isidupremover/tuplecomparison/ISIPubComparer.java
+++
@@ -1,8 +0,0 @@
-package edu.iu.nwb.analysis.isidupremover.tuplecomparison;
-
-import prefuse.data.Tuple;
-
-public interface ISIPubComparer {
-	
-	public int compare(Tuple tu1, Tuple tu2, StringBuilder log);
-}
