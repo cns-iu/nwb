e5ce1b2d260506854b470a05a3a6e1819d97ccf1
Micah Linnemeier <mwlinnem@indiana.edu>
2007-11-29 21:29:52 +0000

    Initial import.

---
+++ trunk/plugins/composite/edu.iu.nwb.composite.isiloadandclean/src/edu/iu/nwb/composite/isiloadandclean/ISILoadAndCleanAlgorithm.java
@@ -0,0 +1,65 @@
+package edu.iu.nwb.composite.isiloadandclean;
+
+import java.util.Dictionary;
+
+import org.cishell.framework.CIShellContext;
+import org.cishell.framework.algorithm.Algorithm;
+import org.cishell.framework.algorithm.AlgorithmFactory;
+import org.cishell.framework.data.Data;
+import org.osgi.service.log.LogService;
+
+import edu.iu.nwb.composite.isiloadandclean.loading.SWTISILoader;
+import edu.iu.nwb.composite.isiloadandclean.loading.SpecialISILoader;
+
+public class ISILoadAndCleanAlgorithm implements Algorithm {
+    Data[] data;
+    Dictionary parameters;
+    CIShellContext context;
+    
+    private LogService log;
+    
+    private SpecialISILoader isiLoader;
+    
+    private AlgorithmFactory isiValidator;
+    private AlgorithmFactory isiToPrefuseConverter;
+    private AlgorithmFactory isiDupRemover;
+   
+    public ISILoadAndCleanAlgorithm(Data[] data, Dictionary parameters, CIShellContext context,
+    		AlgorithmFactory isiValidator, AlgorithmFactory isiToPrefuseConverter,
+    		AlgorithmFactory isiDupRemover) {
+        this.data = data;
+        this.parameters = parameters;
+        this.context = context;
+        
+        this.log = (LogService)context.getService(LogService.class.getName());      
+        
+        //Change this if you want to do it with a web gui
+        this.isiLoader = new SWTISILoader();
+        
+        this.isiValidator = isiValidator;
+        this.isiToPrefuseConverter = isiToPrefuseConverter;
+        this.isiDupRemover = isiDupRemover;
+        
+    }
+
+    public Data[] execute() {
+    	
+    	Data[] loadedISIData = isiLoader.getISIDataFromUser();
+    
+    	Data[] validatedISIData = executeAlgorithm(isiValidator, loadedISIData);
+    	
+    	Data[] convertedToPrefuseISIData = executeAlgorithm(isiToPrefuseConverter, validatedISIData);
+    	
+    	Data[] dupRemovedISIData = executeAlgorithm(isiDupRemover, convertedToPrefuseISIData);
+    	
+    	Data[] origAndDupRemovedISIData = new Data[]{convertedToPrefuseISIData[0], dupRemovedISIData[0]};
+    	
+    	return origAndDupRemovedISIData;
+    }
+    
+    private Data[] executeAlgorithm(AlgorithmFactory algFactory, Data[] data) {
+    	Algorithm alg = algFactory.createAlgorithm(data, parameters, context);
+    	Data[] result = alg.execute();
+    	return result;
+    }
+}
---
+++ trunk/plugins/composite/edu.iu.nwb.composite.isiloadandclean/src/edu/iu/nwb/composite/isiloadandclean/ISILoadAndCleanAlgorithmFactory.java
@@ -0,0 +1,79 @@
+package edu.iu.nwb.composite.isiloadandclean;
+
+import java.util.Dictionary;
+
+import org.cishell.framework.CIShellContext;
+import org.cishell.framework.algorithm.Algorithm;
+import org.cishell.framework.algorithm.AlgorithmFactory;
+import org.cishell.framework.data.Data;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.log.LogService;
+import org.osgi.service.metatype.MetaTypeProvider;
+
+
+public class ISILoadAndCleanAlgorithmFactory implements AlgorithmFactory {
+    private MetaTypeProvider provider;
+    private BundleContext bContext;
+    
+    private LogService log;
+    private AlgorithmFactory isiValidator;
+    private AlgorithmFactory isiToPrefuseConverter;
+    private AlgorithmFactory isiDupRemover;
+    
+    protected void activate(ComponentContext ctxt) {
+    	this.log = (LogService) 
+    		ctxt.locateService("LOG");
+    	
+        bContext = ctxt.getBundleContext();
+    }
+    protected void deactivate(ComponentContext ctxt) {
+        provider = null;
+    }
+
+    public Algorithm createAlgorithm(Data[] data, Dictionary parameters, CIShellContext context) {
+    	String filter = "";
+        try {
+        	filter = "(& (in_data=file-ext:isi) (out_data=file:text/isi))";
+        	isiValidator = getAlgorithmFactory(filter);
+        	
+        	filter = "(& (in_data=file:text/isi) (out_data=prefuse.data.Table))";
+        	isiToPrefuseConverter = getAlgorithmFactory(filter);
+        	
+        	filter = "(service.pid=edu.iu.nwb.analysis.isidupremover.ISIDupRemoverAlgorithm)";
+        	isiDupRemover = getAlgorithmFactory(filter);
+			
+		} catch (InvalidSyntaxException e) {
+			log.log(LogService.LOG_ERROR, "Invalid syntax in filter " + filter);
+		}
+		
+        return new ISILoadAndCleanAlgorithm(data, parameters, context,
+        		isiValidator, isiToPrefuseConverter, isiDupRemover);
+    }
+    public MetaTypeProvider createParameters(Data[] data) {
+        return null;
+    }
+    
+    private AlgorithmFactory getAlgorithmFactory (String filter) 
+    throws InvalidSyntaxException {
+    	ServiceReference[] algFactoryRefs = bContext
+    	.getServiceReferences(AlgorithmFactory.class.getName(), filter);
+    	
+    	if (algFactoryRefs != null && algFactoryRefs.length != 0) {
+    		ServiceReference algFactoryRef = algFactoryRefs[0];
+    		
+    		AlgorithmFactory algFactory = 
+    			(AlgorithmFactory) bContext.getService(algFactoryRef);
+    		
+    		return algFactory;
+    	} else {
+    		this.log.log(LogService.LOG_ERROR, "ISI Load and Clean Algorithm" +
+    				" was unable to find an algorithm that satisfied the " +
+    				"following filter: " + filter);
+    		return null;
+    	}
+    	
+    }
+}
