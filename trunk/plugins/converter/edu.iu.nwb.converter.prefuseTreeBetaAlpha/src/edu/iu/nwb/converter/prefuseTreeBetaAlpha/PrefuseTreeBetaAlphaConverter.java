c04ff4fd3956f69ed1507aa16ca455c638ff51e6
Heng Zhang <zhang45@indiana.edu>
2007-12-06 01:37:25 +0000

    Ticket #166 Create a one way data converter from prefuse beta tree to prefuse alpha tree (iterative approach)

--- trunk/plugins/converter/edu.iu.nwb.converter.prefuseTreeBetaAlpha/src/edu/iu/nwb/converter/prefuseTreeBetaAlpha/PrefuseTreeBetaAlphaConverter.java
+++ trunk/plugins/converter/edu.iu.nwb.converter.prefuseTreeBetaAlpha/src/edu/iu/nwb/converter/prefuseTreeBetaAlpha/PrefuseTreeBetaAlphaConverter.java
@@ -10,6 +10,33 @@ import prefuse.data.Node;
 import prefuse.data.Schema;
 import prefuse.data.Tree;
 import prefuse.data.Tuple;
+import java.util.*;
+
+
+
+
+class NodePair
+{
+	private TreeNode nodeAlhpha;
+	private Node     nodeBeta;
+	
+	public NodePair(TreeNode tn, Node nd)
+	{
+		nodeAlhpha = tn;
+		nodeBeta   = nd;
+	}
+	
+	TreeNode getNodeAlpha()
+	{
+		return nodeAlhpha;
+	}
+	
+	Node getNodeBeta()
+	{
+		return nodeBeta;
+	}
+	
+}
 
 
 /**
@@ -17,27 +44,31 @@ import prefuse.data.Tuple;
  * To build a prefuse Alpha Tree based on an existing prefuse Beta Tree 
  * @author Heng Zhang
  */
-
 public class PrefuseTreeBetaAlphaConverter
 {
-	private edu.berkeley.guir.prefuse.graph.Tree AlphaTree;
+	private edu.berkeley.guir.prefuse.graph.Tree alphaTree;
+	private ArrayList alChildQue;
 	
     public PrefuseTreeBetaAlphaConverter()
     {
-        AlphaTree = new DefaultTree();
+        alphaTree = new DefaultTree();
+        alChildQue = new ArrayList(); 
     }
+    
 
     /**
      *  This method will read a beta tree and set the root node of alpha tree
      */
-    public edu.berkeley.guir.prefuse.graph.Tree getPrefuseBetaTree(Tree BetaTree)
+    public edu.berkeley.guir.prefuse.graph.Tree getPrefuseBetaTree(Tree betaTree)
     {
-        TreeNode rootNodeAlhpha = new DefaultTreeNode();
-        Node rootNodeBeta = BetaTree.getRoot();
+    	
+    	TreeNode rootNodeAlhpha = new DefaultTreeNode();
+        Node rootNodeBeta = betaTree.getRoot();
         merge(rootNodeBeta, rootNodeAlhpha);
-        AlphaTree.setRoot(rootNodeAlhpha);
+        alphaTree.setRoot(rootNodeAlhpha);
+        alChildQue.add(new NodePair(rootNodeAlhpha,rootNodeBeta));
         buildTreeAlpha(rootNodeAlhpha, rootNodeBeta);
-        return AlphaTree;
+        return alphaTree;
     }
 
     /**
@@ -56,24 +87,36 @@ public class PrefuseTreeBetaAlphaConverter
     /**
      *  This method will traverse all child nodes in beta tree and build alpha tree
      */
-    private void buildTreeAlpha(TreeNode nodeAlpha, Node nodeBeta)
+    private void buildTreeAlpha(TreeNode rootAlpha, Node rootBeta)
     {
-        Iterator iterBeta = nodeBeta.children();
-        Iterator iter = nodeBeta.children();
-        if(iter == null)
-            return;
-        edu.berkeley.guir.prefuse.graph.Edge edge;
-        for(; iter.hasNext(); nodeAlpha.addChild(edge))
-        {
-            TreeNode nodeAlphaChild = new DefaultTreeNode();
-            Node nodeBetaChild = (Node)iter.next();
-            merge(nodeBetaChild, nodeAlphaChild);
-            edge = new DefaultEdge(nodeAlpha, nodeAlphaChild);
+        TreeNode currentAlphaNode = rootAlpha;
+        Node     currentBetaNode  = rootBeta;
+        
+       while(!alChildQue.isEmpty()){ 
+           Iterator iter = currentBetaNode.children();
+           if(iter == null){
+        	   alChildQue.remove(0);
+        	   continue;
+           }      
+           edu.berkeley.guir.prefuse.graph.Edge edge;
+           for(; iter.hasNext();)
+           {
+        	   TreeNode nodeAlphaChild = new DefaultTreeNode();
+               Node nodeBetaChild = (Node)iter.next();
+               merge(nodeBetaChild, nodeAlphaChild);
+               edge = new DefaultEdge(currentAlphaNode, nodeAlphaChild);
+               currentAlphaNode.addChild(edge);
+               alChildQue.add(new NodePair(nodeAlphaChild,nodeBetaChild));
+           }
+           if (!alChildQue.isEmpty())
+           {
+        	   alChildQue.remove(0);
+           }
+           if (!alChildQue.isEmpty()){
+               currentBetaNode = ((NodePair)alChildQue.get(0)).getNodeBeta();
+               currentAlphaNode = ((NodePair)alChildQue.get(0)).getNodeAlpha();
+           }
+           
         }
-
-        Iterator iterAlpha = nodeAlpha.getChildren();
-        for(; iterBeta.hasNext(); buildTreeAlpha((TreeNode)iterAlpha.next(), (Node)iterBeta.next()));
     }
-
-    
 }
