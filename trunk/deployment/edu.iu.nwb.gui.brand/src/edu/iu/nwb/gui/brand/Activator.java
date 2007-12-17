21ef7925f4f23e05e3292fc9377a547708264496
Bonnie Huang <huangb@indiana.edu>
2007-12-17 19:15:52 +0000

    update the version and the list of authors

--- trunk/deployment/edu.iu.nwb.gui.brand/src/edu/iu/nwb/gui/brand/Activator.java
+++ trunk/deployment/edu.iu.nwb.gui.brand/src/edu/iu/nwb/gui/brand/Activator.java
@@ -29,8 +29,9 @@ public class Activator extends AbstractUIPlugin implements IStartup{
 		"The primary investigators are Katy Börner, Albert-László Barabási, Santiago Schnell, "+
 		"Alessandro Vespignani, Stanley Wasserman, Eric A. Wernert, and Craig Stewart. "+
 		"The NWB tool was developed by Weixia (Bonnie) Huang, Dr. Santo Fortunato, "+
-		"Bruce Herr, Russell Duhon, Timothy Kelley, M Felix Terkhorn, Cesar A. Hidalgo R., "+
-        "Ben Markines, Dr. Soma Sanyal, Ann McCranie, Ramya Sabbineni, Vivek S. Thakres., "+
+		"Bruce Herr, Russell Duhon, Timothy Kelley, Micah Walter Linnemeier, Heng (Michael) Zhang, "+
+		"Duygu Balcan, M Felix Terkhorn, Cesar A. Hidalgo R., Ben Markines, Mariano Beiró, "+
+        "Dr. Soma Sanyal, Ann McCranie, Megha Ramawat, Ramya Sabbineni, Vivek S. Thakres., "+
         "Dr. Alessandro Vespignani, and Dr. Katy Börner.\n"+
         "It builds on the Cyberinfrastructure Shell (http://cishell.org) developed at the "+
         "Information Visualization Laboratory and the Cyberinfrastructure for Network Science Center, "+
@@ -38,24 +39,6 @@ public class Activator extends AbstractUIPlugin implements IStartup{
         "For more information on the Network Workbench project, "+
         "see http://nwb.slis.indiana.edu and https://nwb.slis.indiana.edu/community/.";
 
-
-	
-/*		"Welcome to use Network Workbench Tool "+
-		"- A large-scale network analysis, modeling and visualization "+
-		"cyberinfrastructure for network scientists.\n"+
-		"The NWB Tool is funded by an NSF IIS-0513650 award to Katy Börner, "+
-		"Albert-Laszlo Barabasi, Santiago Schnell, Alessandro Vespignani, "+
-		"Stanley Wasserman, and Eric Wernert, see  http://nwb.slis.indiana.edu.\n"+
-		"It is developed by Weixia (Bonnie) Huang, Dr. Santo Fortunato, Ben Markines, Bruce Herr, "+
-		"Ramya Sabbineni, Vivek S Thakres, Dr. Soma Sanyal and Dr. Katy Börner.\n"+
-		"It builds on the Cyberinfrastructure Shell (http://cishell.org) developed "+
-		"at the InfoVis Lab and the Cyberinfrastructure for Network Science Center at Indiana University.\n\n"+
-        "Please acknowledge this effort by citing:\n"+
-        "Bruce Herr, Weixia Huang, Shashikant Penumarthy, and Katy Börner. (in press). "+
-        "Designing Highly Flexible and Usable Cyberinfrastructures for Convergence. "+
-        "William S. Bainbridge (Ed.) Progress in Convergence. Annals of the New York Academy of Sciences.\n"+
-        "http://cishell.org/papers/06-cishell.pdf";
-*/
 	
 	/**
 	 * The constructor
