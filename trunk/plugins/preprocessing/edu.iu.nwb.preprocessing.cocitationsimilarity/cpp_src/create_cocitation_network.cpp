0d4ceb09e2221b8ef24a98da489c0d8d480e11a1
Bruce Herr <bherr@indiana.edu>
2008-02-12 16:56:04 +0000

    Initial commit of the cocitation similarity network extraction algorithm

---
+++ trunk/plugins/preprocessing/edu.iu.nwb.preprocessing.cocitationsimilarity/cpp_src/create_cocitation_network.cpp
@@ -0,0 +1,146 @@
+//============================================================================
+// Name        : create_cocitation_network.cpp
+// Author      : Bruce Herr (bh2@bh2.net), Todd Holloway
+// Version     : 1.0
+// Copyright   : TBD
+// Description : Co-citation network creation for Wikipedia. Modeled after 
+//     			 Todd Holloway's Perl-based co-citation network creator.
+//============================================================================
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <stdlib.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    char* ARTICLE_NETWORK;
+    char* SIMILARITY_NETWORK;
+    char DELIMITER = '|';
+    int MAX_EDGES = -1;
+    
+	if (argc == 3) {
+		ARTICLE_NETWORK = argv[1];
+		SIMILARITY_NETWORK = argv[2];
+	} else if (argc == 4) {
+	    MAX_EDGES = atoi(argv[1]);
+		ARTICLE_NETWORK = argv[2];
+		SIMILARITY_NETWORK = argv[3];
+		
+		if (MAX_EDGES == 0) {
+		  MAX_EDGES = -1;
+		}
+	} else {
+		cout << argv[0] << ": <maxEdgesPerNode (optional)> <articleNetworkFile> <outSimilarityFile.sim>" << endl;
+		return -1;
+	}
+	
+	map< int, vector<int> > n2citations;
+	map< int, vector<int> > n2reverseCitations;
+	
+	//read in article network
+	ifstream in(ARTICLE_NETWORK);
+	
+	int n1, n2;
+	string line;
+
+	int numEdges = 0;
+	while (getline(in, line)) {
+		replace(line.begin(), line.end(), DELIMITER, ' ');
+		istringstream linestream(line);
+
+		linestream >> n1 >> n2;
+		
+		n2citations[n1].push_back(n2);
+		n2reverseCitations[n2].push_back(n1);
+		numEdges++;
+	}
+	cout << n2citations.size() << " nodes, "<< numEdges << " edges read from network." << endl;
+	in.close();
+	
+	//compute and output cocitation similarity network
+	ofstream out(SIMILARITY_NETWORK);
+	
+	map< int, vector<int> >::iterator i;
+	vector<int>::iterator j;
+	vector<int>::iterator k;
+	map<int,int>::iterator l;
+	multimap<double,int>::reverse_iterator m;
+	char formatted_sim [10];
+	double sim;
+	int node1, node2, citingNode, citationCount1, citationCount2, sharedCitations, edges_written;
+	
+	int num = 0, num_scores = 0;
+	unsigned long int total_edges = 0;
+	for (i = n2citations.begin(); i != n2citations.end(); i++) {
+		node1 = i->first;
+		
+		if (n2reverseCitations.count(node1) > 0) {
+			citationCount1 = n2reverseCitations[node1].size();
+			map<int,int> cocitationCounts;
+			multimap<double,int> simScores;
+			num++;
+			
+			//periodic feedback
+			if (num % 100 == 0) {
+				//cout << num << " nodes, " << num_scores << " scores written" << endl; 
+				num_scores = 0;
+			}
+			
+			//find and compute counts of cocited nodes
+			for (j = n2reverseCitations[node1].begin(); j != n2reverseCitations[node1].end(); j++) {
+				citingNode = *j;
+				
+				for (k = n2citations[citingNode].begin(); k != n2citations[citingNode].end(); k++) {
+					node2 = *k;
+					
+					if (cocitationCounts.count(node2) == 0) { //if doesn't exist
+						cocitationCounts[node2] = 1;
+					} else {
+						cocitationCounts[node2]++;
+					}
+				}
+			}
+			
+			//compute similarity of cocited nodes
+			for (l = cocitationCounts.begin(); l != cocitationCounts.end(); l++) {
+				node2 = l->first;
+				
+				if (node1 != node2) {
+					citationCount2 = n2reverseCitations[node2].size();
+					sharedCitations = l->second;
+					sim = ((double)(2*sharedCitations)) / (double)(citationCount1 + citationCount2);
+					
+					if (sim >= (double)0.001) {
+					    simScores.insert(pair<double,int>(sim,node2));
+					    
+						num_scores++;
+					}
+				}
+			}
+			
+			//write out the sim scores in descending similarity order
+			edges_written = 0;
+			for (m = simScores.rbegin(); m != simScores.rend() && edges_written != MAX_EDGES; m++) {
+			    sim = m->first;
+			    node2 = m->second;
+			    
+				sprintf(formatted_sim,"%.3f",sim);			
+    			out << node1 << '\t' << node2 << '\t' << formatted_sim << endl;
+    			edges_written++;
+			}
+			total_edges += edges_written;
+		}
+	}
+	
+	out.close();
+	cout << total_edges << " similarity scores written." << endl; 
+	
+	return 0;
+}
+
+
+
---
+++ trunk/plugins/preprocessing/edu.iu.nwb.preprocessing.cocitationsimilarity/src/edu/iu/nwb/preprocessing/cocitationsimilarity/CoCitationSimilarityAlgorithm.java
@@ -0,0 +1,73 @@
+package edu.iu.nwb.preprocessing.cocitationsimilarity;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Dictionary;
+
+import org.cishell.framework.CIShellContext;
+import org.cishell.framework.algorithm.Algorithm;
+import org.cishell.framework.data.BasicData;
+import org.cishell.framework.data.Data;
+import org.cishell.framework.data.DataProperty;
+import org.cishell.templates.staticexecutable.StaticExecutableAlgorithmFactory;
+import org.osgi.framework.BundleContext;
+
+import edu.iu.nwb.util.nwbfile.NWBFileParser;
+import edu.iu.nwb.util.nwbfile.ParsingException;
+
+public class CoCitationSimilarityAlgorithm implements Algorithm {
+    private static StaticExecutableAlgorithmFactory staticAlgorithmFactory;
+    Data[] data;
+    Dictionary parameters;
+    CIShellContext context;
+    
+    public CoCitationSimilarityAlgorithm(Data[] data, Dictionary parameters, CIShellContext context, BundleContext bContext) {
+        this.data = data;
+        this.parameters = parameters;
+        this.context = context;
+        if (staticAlgorithmFactory == null) {
+        	staticAlgorithmFactory = new StaticExecutableAlgorithmFactory("cocitation", bContext);
+        }
+    }
+
+    public Data[] execute() {
+    	try {
+    		File tmpEdgeFile = File.createTempFile("nwb-", ".edge");
+			File inputNWBFile = (File)data[0].getData();
+			File outputNWBFile = File.createTempFile("nwb-", ".nwb");
+			
+			//create the network File
+			NWBFileParser parser = new NWBFileParser(inputNWBFile);
+			parser.parse(new NWBToEdgeFileHandler(new FileOutputStream(tmpEdgeFile)));
+			Data simData = new BasicData(tmpEdgeFile,"file:text/edge");
+			
+			//Run cocitation on network File
+			Algorithm coCitationAlg = staticAlgorithmFactory.createAlgorithm(new Data[]{simData}, parameters, context);
+			Data[] edgeData = coCitationAlg.execute();
+			File edgeFile = (File) edgeData[0].getData();
+			
+			//Create a new NWB file w/ new similarity edges
+			if (edgeData != null) {
+				NWBEdgeMerger merger = new NWBEdgeMerger(edgeFile,inputNWBFile,outputNWBFile);
+				merger.merge();
+			} else {
+				throw new ParsingException("Co-Citation Failed!");
+			}
+			
+			//If all has gone well, return the new nwb file
+			Data outNWBData = new BasicData(outputNWBFile,"file:text/nwb");
+			outNWBData.getMetaData().put(DataProperty.LABEL, "Co-Citation Similarity Network");
+			outNWBData.getMetaData().put(DataProperty.TYPE, DataProperty.NETWORK_TYPE);
+			outNWBData.getMetaData().put(DataProperty.PARENT, data[0]);
+			return new Data[]{outNWBData};
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParsingException e) {
+			e.printStackTrace();
+		}
+    	
+        return null;
+    }
+}
