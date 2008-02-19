250bc9bf0262a8085d79d6cb401f4be8a02c10eb
Bruce Herr <bherr@indiana.edu>
2008-02-19 20:10:40 +0000

    had to remove uncompressed g95 installs and just extract g95 every time the fortran_compile script is called...

--- trunk/deployment/edu.iu.nwb.releng.fortran/fortran_compile.sh
+++ trunk/deployment/edu.iu.nwb.releng.fortran/fortran_compile.sh
@@ -6,48 +6,58 @@ then
     echo "fortran_compile.sh <PLATFORM TO COMPILE FOR>"
     exit -1
 fi
-
 PLATFORM_TO_USE="$1"
 
 #win32 does not work currently.. I can't get g95 to actually compile stuff...
-if [ $PLATFORM_TO_USE == "win32" ]
-then
+if [ $PLATFORM_TO_USE == "win32" ]; then
 	PLATFORM=win32
 	BINARY=/y/win32/sfw/g95/bin/g95
+	ARCHIVE=Must install manually.
+	echo "Win32 not supported.. yet"
+	exit -1
 fi
 
-if [ $PLATFORM_TO_USE == "linux.x86_64" ]
-then 
+if [ $PLATFORM_TO_USE == "linux.x86_64" ]; then 
 	PLATFORM=linux.x86_64
-	BINARY=`pwd`/g95/g95-install_$PLATFORM/bin/x86_64-unknown-linux-gnu-g95
+	BINARY=x86_64-unknown-linux-gnu-g95
+	ARCHIVE=g95-x86_64-64-linux.tgz
 fi
 
-if [ $PLATFORM_TO_USE == "linux.x86" ]
-then 
+if [ $PLATFORM_TO_USE == "linux.x86" ]; then 
 	PLATFORM=linux.x86
-	BINARY=`pwd`/g95/g95-install_$PLATFORM/bin/i686-suse-linux-gnu-g95
+	BINARY=i686-suse-linux-gnu-g95
+	ARCHIVE=g95-x86-linux.tgz
 fi
 
-if [ $PLATFORM_TO_USE == "solaris.sparc" ]
-then
+if [ $PLATFORM_TO_USE == "solaris.sparc" ]; then
 	PLATFORM=solaris.sparc
-	BINARY=`pwd`/g95/g95-install_$PLATFORM/bin/sparc-sun-solaris2.9-g95
+	BINARY=sparc-sun-solaris2.10-g95
+	ARCHIVE=g95-sparc-solaris.tgz
 fi
 
-if [ $PLATFORM_TO_USE == "macosx.x86" ]
-then 
+if [ $PLATFORM_TO_USE == "macosx.x86" ]; then 
 	PLATFORM=macosx.x86
-	BINARY=`pwd`/g95/g95-install_$PLATFORM/bin/i386-apple-darwin8.11.1-g95
+	BINARY=i386-apple-darwin8.11.1-g95
+	ARCHIVE=g95-x86-osx.tgz
 fi
 
-if [ $PLATFORM_TO_USE == "macosx.ppc" ]
-then
+if [ $PLATFORM_TO_USE == "macosx.ppc" ]; then
     PLATFORM=macosx.ppc
-    BINARY=`pwd`/g95/g95-install_$PLATFORM/bin/powerpc-apple-darwin6.8-g95
+    BINARY=powerpc-apple-darwin6.8-g95
+	ARCHIVE=g95-powerpc-osx.tgz
 fi
 
 
+if [ -e build/g95-install ]; then
+    rm -rf build/g95-install
+fi
+
+if [ $PLATFORM_TO_USE != "win32" ]; then
 mkdir -p build
+cd build
+tar zxf ../g95/$ARCHIVE
+G95=`pwd`/g95-install/bin/$BINARY
+cd ..
 
 echo "Checking out fortran algorithms..."
 cat algorithms.csv | while read line
@@ -71,8 +81,8 @@ do
 	src=`ls *.f90`
 	alg_name=${src%.f90}
 	mkdir -p "../ALGORITHM/$PLATFORM"
-	echo "$BINARY $src -O3 -o ../ALGORITHM/$PLATFORM/$alg_name"
-	$BINARY $src -O3 -o ../ALGORITHM/$PLATFORM/$alg_name
+	echo "$G95 $src -O3 -o ../ALGORITHM/$PLATFORM/$alg_name"
+	$G95 $src -O3 -o ../ALGORITHM/$PLATFORM/$alg_name
 
 	cd ../../../
 done
