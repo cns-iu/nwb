17f2b774242de3ec8408f42aa3dc7a9ea2789d6d
Micah Linnemeier <mwlinnem@indiana.edu>
2008-02-26 01:35:25 +0000

    Changed output label for Refer Reader.

--- trunk/plugins/converter/edu.iu.nwb.converter.prefuserefer/src/edu/iu/nwb/converter/prefuserefer/ReferReader.java
+++ trunk/plugins/converter/edu.iu.nwb.converter.prefuserefer/src/edu/iu/nwb/converter/prefuserefer/ReferReader.java
@@ -309,7 +309,7 @@ public class ReferReader implements Algorithm {
     private Data[] formatAsData(Table referTable, String referFilePath) {
     	Data[] tableToReturnData = 
 			new Data[] {new BasicData(referTable, Table.class.getName())};
-		tableToReturnData[0].getMetaData().put(DataProperty.LABEL, "EndNote reference file: " + referFilePath);
+		tableToReturnData[0].getMetaData().put(DataProperty.LABEL, "Parsed EndNote reference file: " + referFilePath);
 		//TODO: should this really be a text_type?
         tableToReturnData[0].getMetaData().put(DataProperty.TYPE, DataProperty.TEXT_TYPE);
         return tableToReturnData;
