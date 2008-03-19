7abac106245d4c0f845a42e0aaead6e60589f4ab
Micah Linnemeier <mwlinnem@indiana.edu>
2008-03-19 19:22:09 +0000

    Initial import (sort of). Moved from test-devel to production land (here).

---
+++ trunk/plugins/converter/edu.iu.nwb.converter.prefusescopus/src/edu/iu/nwb/converter/prefusescopus/ScopusValidatorAlgorithm.java
@@ -0,0 +1,41 @@
+package edu.iu.nwb.converter.prefusescopus;
+
+import java.io.File;
+import java.util.Dictionary;
+
+import org.cishell.framework.CIShellContext;
+import org.cishell.framework.algorithm.Algorithm;
+import org.cishell.framework.data.BasicData;
+import org.cishell.framework.data.Data;
+import org.cishell.framework.data.DataProperty;
+import org.osgi.service.log.LogService;
+
+public class ScopusValidatorAlgorithm implements Algorithm {
+    Data[] data;
+    Dictionary parameters;
+    CIShellContext context;
+    
+    public ScopusValidatorAlgorithm(Data[] data, Dictionary parameters, CIShellContext context) {
+        this.data = data;
+        this.parameters = parameters;
+        this.context = context;
+    }
+
+    public Data[] execute() {
+    	LogService logger = (LogService) context.getService(LogService.class.getName());
+
+		String fileHandler = (String) data[0].getData();
+		File inData = new File(fileHandler);
+
+		try{
+			Data[] dm = new Data[] {new BasicData(inData, "file:text/scopus")};
+			dm[0].getMetaData().put(DataProperty.LABEL, "Scopus csv file: " + fileHandler);
+			dm[0].getMetaData().put(DataProperty.TYPE, DataProperty.MATRIX_TYPE);
+			return dm;
+		}catch (SecurityException exception){
+			logger.log(LogService.LOG_ERROR, "SecurityException", exception);
+			exception.printStackTrace();
+			return null;
+		}
+    }
+}
