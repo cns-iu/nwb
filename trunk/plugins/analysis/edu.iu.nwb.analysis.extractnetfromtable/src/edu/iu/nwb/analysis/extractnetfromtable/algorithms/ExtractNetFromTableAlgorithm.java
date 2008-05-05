cc6a1429de29ec1b2ad6a8a679f022c036071f02
Timothy Kelley <kelleyt@indiana.edu>
2008-05-05 14:53:11 +0000

    Separated handling the properties into a separate file.

--- trunk/plugins/analysis/edu.iu.nwb.analysis.extractnetfromtable/src/edu/iu/nwb/analysis/extractnetfromtable/algorithms/ExtractNetFromTableAlgorithm.java
+++ trunk/plugins/analysis/edu.iu.nwb.analysis.extractnetfromtable/src/edu/iu/nwb/analysis/extractnetfromtable/algorithms/ExtractNetFromTableAlgorithm.java
@@ -1,15 +1,7 @@
 package edu.iu.nwb.analysis.extractnetfromtable.algorithms;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Dictionary;
 import java.util.Properties;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.cishell.framework.CIShellContext;
 import org.cishell.framework.algorithm.Algorithm;
@@ -21,6 +13,7 @@ import org.osgi.service.log.LogService;
 
 import edu.iu.nwb.analysis.extractnetfromtable.components.ExtractNetworkFromTable;
 import edu.iu.nwb.analysis.extractnetfromtable.components.InvalidColumnNameException;
+import edu.iu.nwb.analysis.extractnetfromtable.components.PropertyHandler;
 
 public class ExtractNetFromTableAlgorithm implements Algorithm {
 	Data[] data;
@@ -35,53 +28,7 @@ public class ExtractNetFromTableAlgorithm implements Algorithm {
 		logger = (LogService) context.getService(LogService.class.getName());
 	}
 	
-	public boolean validateProperties(final FileInputStream fis) throws IOException{
-		final BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-		
-		boolean wellFormed = true;
-		String line;
-		Pattern p = Pattern.compile("^.*\\..*=.*\\..*");
-		Matcher m;
-		
-		while((line = br.readLine()) != null){		
-			if(line.startsWith("node.") || line.startsWith("edge.")){				
-				m = p.matcher(line.subSequence(0, line.length())).reset();
-				if(!m.find()){
-					wellFormed = false;	
-				}
-			}	
-		}
-		br.close();	
-		return wellFormed;		
-	}
-
-	public Properties getProperties(String fileName) throws AlgorithmExecutionException {
-		final Properties aggregateDefs = new Properties();
-		boolean wellFormed = true;
-		
-		try {
-			File f = new File(fileName);
-			FileInputStream in = new FileInputStream(f);
-			
-			wellFormed = validateProperties(in);
-			
-			if(wellFormed){
-				in = new FileInputStream(f);
-				aggregateDefs.load(in);
-			}
-			else{
-				logger.log(LogService.LOG_WARNING, "Your Aggregate Function File did not follow the specified format.\n" +
-						"Continuing the extraction without additional analysis.");
-				//need to add a documentation link about how to specify the aggregate function file.
-			}
-		} 
-		catch (final FileNotFoundException fnfe) {
-			throw new AlgorithmExecutionException(fnfe.getMessage(), fnfe);
-		} catch (final IOException ie) {
-			throw new AlgorithmExecutionException(ie.getMessage(), ie);
-		}
-		return aggregateDefs;
-	}
+	
 	
 	public Data[] execute() throws AlgorithmExecutionException {
 		final prefuse.data.Table dataTable = (prefuse.data.Table) data[0].getData();
@@ -94,7 +41,7 @@ public class ExtractNetFromTableAlgorithm implements Algorithm {
 		extractColumn = this.parameters.get("colName").toString();
 
 		if(this.parameters.get("aff") != null){
-			p = this.getProperties((String)this.parameters.get("aff"));
+			p = PropertyHandler.getProperties((String)this.parameters.get("aff"),this.logger);
 		}
 
 		try{
