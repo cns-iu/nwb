0773b00842b3b2427559068ede7229d6189e7528
Timothy Kelley <kelleyt@indiana.edu>
2008-07-10 01:15:36 +0000

    More Generalized Form.

---
+++ trunk/plugins/analysis/edu.iu.nwb.analysis.java.nodedegree/src/edu/iu/nwb/analysis/java/nodedegree/components/DegreeType.java
@@ -0,0 +1,8 @@
+package edu.iu.nwb.analysis.java.nodedegree.components;
+
+public interface DegreeType {
+	static final int totalDegree = 0;
+	static final int inDegree = 1;
+	static final int outDegree = 2;
+	
+}
