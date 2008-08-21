9b6f93eb0a04a5c54cfc9ed141c8ec708823e91c
Bonnie Huang <huangb@indiana.edu>
2008-08-21 16:15:33 +0000

    clean up code

--- trunk/deployment/edu.iu.nwb.gui.brand/src/edu/iu/nwb/gui/brand/Activator.java
+++ trunk/deployment/edu.iu.nwb.gui.brand/src/edu/iu/nwb/gui/brand/Activator.java
@@ -1,6 +1,5 @@
 package edu.iu.nwb.gui.brand;
 
-//import org.eclipse.jface.resource.ImageDescriptor;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -21,24 +20,6 @@ public class Activator extends AbstractUIPlugin implements IStartup{
 	private static Activator plugin;	
 	private BundleContext bContext;
 	private boolean alreadyLogged;
-	private static final String nwb_greeting=
-		"Welcome to the Network Workbench (NWB) tool that supports "+
-		"the preprocessing, modeling, analysis, and visualization of "+
-		"small, medium, and large networks. \n"+
-		"The Network Workbench project is supported in part by the NSF IIS-0513650 award. "+
-		"The primary investigators are Katy Börner, Albert-László Barabási, Santiago Schnell, "+
-		"Alessandro Vespignani, Stanley Wasserman, Eric A. Wernert, and Craig Stewart. "+
-		"The NWB tool was developed by Weixia (Bonnie) Huang, Dr. Santo Fortunato, "+
-		"Bruce Herr, Russell Duhon, Timothy Kelley, Micah Walter Linnemeier, Heng (Michael) Zhang, "+
-		"Duygu Balcan, M Felix Terkhorn, Cesar A. Hidalgo R., Ben Markines, Mariano Beiró, "+
-        "Dr. Soma Sanyal, Ann McCranie, Megha Ramawat, Ramya Sabbineni, Vivek S. Thakres., "+
-        "Dr. Alessandro Vespignani, and Dr. Katy Börner.\n"+
-        "It builds on the Cyberinfrastructure Shell (http://cishell.org) developed at the "+
-        "Information Visualization Laboratory and the Cyberinfrastructure for Network Science Center, "+
-        "both at Indiana University.\n"+
-        "For more information on the Network Workbench project, "+
-        "see http://nwb.slis.indiana.edu and https://nwb.slis.indiana.edu/community/.";
-
 	
 	/**
 	 * The constructor
@@ -80,16 +61,7 @@ public class Activator extends AbstractUIPlugin implements IStartup{
 	}
 	
 	public void earlyStartup(){
-		//		TODO: Get log and print initial log blurb
-/*        if (bContext != null) {
-    		ServiceReference ref = bContext.getServiceReference(LogService.class.getName());
-            
-            if (ref != null) {
-                LogService logger = (LogService)bContext.getService(ref);
-                logger.log(LogService.LOG_INFO, nwb_greeting);
-            }            
-        }
-*/
+
 		if (bContext != null) {
             String greeting = null;
             Properties props = new Properties();
@@ -113,16 +85,4 @@ public class Activator extends AbstractUIPlugin implements IStartup{
         }
         
 	}
-
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-/*	public static ImageDescriptor getImageDescriptor(String path) {
-		return imageDescriptorFromPlugin(PLUGIN_ID, path);
-	}
-*/	
 }
