0880df8d72a603b462a5271f3bdc988b9c075386
Micah Linnemeier <mwlinnem@indiana.edu>
2008-09-03 16:13:45 +0000

    Updated to conform with change in endnote reader.

--- trunk/plugins/analysis/edu.iu.nwb.analysis.extractcoauthorship/src/edu/iu/nwb/analysis/extractcoauthorship/metadata/SupportedFileTypes.java
+++ trunk/plugins/analysis/edu.iu.nwb.analysis.extractcoauthorship/src/edu/iu/nwb/analysis/extractcoauthorship/metadata/SupportedFileTypes.java
@@ -6,7 +6,7 @@ public interface SupportedFileTypes {
 	static final CitationFormat bibtex = new CitationFormat("bibtex","author");
 	static final CitationFormat isi = new CitationFormat("isi","Authors");
 	static final CitationFormat scopus = new CitationFormat("scopus","Authors");
-	static final CitationFormat endnote = new CitationFormat("endnote","Author");
+	static final CitationFormat endnote = new CitationFormat("endnote","Authors");
 	
 	static final String[] supportedFormats = CitationFormat.getSupportedFormats();
 	
