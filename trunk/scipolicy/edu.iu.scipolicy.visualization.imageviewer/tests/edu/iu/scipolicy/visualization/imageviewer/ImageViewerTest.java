2ea5f833f0b1aa978cfdd3936d7a1ef51bf2599a
Patrick Phillips <pataphil@indiana.edu>
2009-01-16 15:34:23 +0000

    Wrote smoke screen test for Image Viewer.

---
+++ trunk/scipolicy/edu.iu.scipolicy.visualization.imageviewer/tests/edu/iu/scipolicy/visualization/imageviewer/ImageViewerTest.java
@@ -0,0 +1,60 @@
+package edu.iu.scipolicy.visualization.imageviewer;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+import junit.framework.TestCase;
+
+import org.cishell.framework.data.BasicData;
+import org.cishell.framework.data.Data;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import edu.iu.scipolicy.converter.psraster.psrasterproperties.PSRasterProperties;
+import edu.iu.scipolicy.converter.psraster.utilities.ImageUtilities;
+
+public class ImageViewerTest extends TestCase {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testExecute() {
+		// Create an image to view.
+		BufferedImage bufferedImageToView =
+			ImageUtilities.createBufferedImageFilledWithColor(Color.BLUE);
+		
+		// Wrap the image for the image viewer.
+		Data bufferedImageToViewData = new BasicData(bufferedImageToView,
+										PSRasterProperties.IMAGE_OBJECT_MIME_TYPE);
+		
+		// Create the image viewer.
+		ImageViewer imageViewer =
+			new ImageViewer(new Data[] { bufferedImageToViewData }, null, null);
+		
+		try {
+			if (imageViewer.execute() != null)
+				fail();
+		}
+		catch (Exception e) {
+			fail();
+		}
+	}
+
+}
