d70cc9a6f7e0c540005e6a343f84efb1c15e209d
Micah Linnemeier <mwlinnem@indiana.edu>
2009-01-26 20:03:48 +0000

    Refactored the implodeStringArray method into a utility plugin. Reviewed by Patrick.

--- trunk/scipolicy/edu.iu.scipolicy.converter.nsf.db_to_prefuse/src/edu/iu/scipolicy/converter/nsf/db_to_prefuse/DbToPrefuseAlgorithm.java
+++ trunk/scipolicy/edu.iu.scipolicy.converter.nsf.db_to_prefuse/src/edu/iu/scipolicy/converter/nsf/db_to_prefuse/DbToPrefuseAlgorithm.java
@@ -10,100 +10,89 @@ import org.cishell.framework.data.BasicData;
 import org.cishell.framework.data.Data;
 import org.cishell.framework.data.DataProperty;
 import org.cishell.service.database.DataSourceWithID;
-import org.cishell.service.database.DatabaseService;
 import org.osgi.service.log.LogService;
 
 import prefuse.data.Table;
 import prefuse.data.io.DataIOException;
 import prefuse.data.io.sql.ConnectionFactory;
 import prefuse.data.io.sql.DatabaseDataSource;
+import edu.iu.scipolicy.utilities.StringUtilities;
+import edu.iu.scipolicy.utilities.nsf.NsfNames;
 
 public class DbToPrefuseAlgorithm implements Algorithm {
 	
-	private static String[] awardExtractionFieldsInAwardTable = {
-		"AWARD_NUMBER",
-		"TITLE",
-		"START_DATE",
-		"EXPIRATION_DATE",
-		"AWARDED_AMOUNT_TO_DATE"
+	private static String[] BASIC_AWARD_TABLE_FIELDS = {
+		NsfNames.DB.AWARD_NUMBER,
+		NsfNames.DB.AWARD_TITLE,
+		NsfNames.DB.AWARD_START_DATE,
+		NsfNames.DB.AWARD_EXPIRATION_DATE,
+		NsfNames.DB.AWARDED_AMOUNT_TO_DATE
 	};
 
 	private Data[] data;
 
 	private LogService log;
-	private DatabaseService dbService;
 
 	public DbToPrefuseAlgorithm(Data[] data, Dictionary parameters, CIShellContext context) {
 		this.data = data;
 
 		this.log = (LogService) context.getService(LogService.class.getName());
-		this.dbService = (DatabaseService) context.getService(DatabaseService.class.getName());
 	}
 
+	/*
+	 * extract basic information about each awarded grant in the nsf db, 
+	 * and put it into a prefuse table to return.
+	 */
 	public Data[] execute() throws AlgorithmExecutionException {
-		//unpack nsf db
 		
 		DataSourceWithID nsfDb = (DataSourceWithID) data[0].getData();
-		
-		//extract prefuse table from nsf db
-		
-			Table awardTable;
-			DatabaseDataSource wf;
-			try {
-				wf = ConnectionFactory.getDatabaseConnection(nsfDb.getConnection());
-			    awardTable = wf.getData(formSQL(nsfDb.getID()));
-			    //TODO: Rename award table column names to be the same as original csv
-		 
-			} catch (SQLException e) {
-				throw new AlgorithmExecutionException(e);
-			}
-			 catch (DataIOException e) {
-					throw new AlgorithmExecutionException(e);
-			
-			 }
-			 
-		//wrap and annote prefuse table with metadata
-	    
+
+		Table awardTable = extractTable(nsfDb);	 
+
 	    Data awardTableData = wrapAndAnnotateAwardTable(awardTable);
 		
-		//return prefuse table data
-	    
 	    return new Data[]{ awardTableData };
 		
 	}
-		
-		   
-	   private static String implodeStringArray(String[] stringArray,
-	    										String separator) {
-	    	final int stringArrayLength = stringArray.length;
-	    	StringBuilder workingResultString = new StringBuilder();
-	    	
-	    	for (int ii = 0; ii < stringArrayLength; ii++) {
-	    		workingResultString.append(stringArray[ii]);
-	    		if (ii != stringArrayLength - 1) {
-	    			workingResultString.append(separator);
-	    		}
-	    	}
-	    	
-	    	return workingResultString.toString();
-	    }
-	    
-		private String formSQL(int dbID) {
-	    	String sql = "SELECT " +
-	    		implodeStringArray(awardExtractionFieldsInAwardTable, ",") + " FROM " +
-	    		NSFConstants.AWARD_TABLE + dbID;
-	    	System.out.println(sql);
-	    	return sql;
-	    }
-		
-		private Data wrapAndAnnotateAwardTable(Table awardTable) {
-			Data awardTableData = new BasicData(awardTable, Table.class.getName());
+	
+	private Table extractTable(DataSourceWithID nsfDb) 
+		throws AlgorithmExecutionException {
+		try {
+			DatabaseDataSource prefuseDataExtractor = 
+				ConnectionFactory.getDatabaseConnection(nsfDb.getConnection());
 			
-			Dictionary awardTableMetadata = awardTableData.getMetadata();
-			awardTableMetadata.put(DataProperty.LABEL, "NSF Awards Information");
-			awardTableMetadata.put(DataProperty.PARENT, data[0]);
-			awardTableMetadata.put(DataProperty.TYPE, DataProperty.TABLE_TYPE);
-			
-			return awardTableData;
+		    String extractBasicAwardTableSql = "SELECT " +
+	    		StringUtilities.implodeStringArray(BASIC_AWARD_TABLE_FIELDS, ",") + 
+	    		" FROM " +
+	    		NsfNames.DB.AWARD_TABLE + nsfDb.getID();
+		    
+		    Table awardTable = 
+		    	prefuseDataExtractor.getData(extractBasicAwardTableSql);
+		
+		    return awardTable;
+		
+		} catch (SQLException e) {
+			String message =
+				"An unexpected error occurred while extracting data from the nsf database." + 
+				"Aborting extraction algorithm.";
+			throw new AlgorithmExecutionException(message, e);
 		}
+		 catch (DataIOException e) {
+			 String message =
+				"An unexpected error occurred while extracting data from the nsf database." + 
+				"Aborting extraction algorithm.";
+				throw new AlgorithmExecutionException(message, e);
+		 } 
+	}
+
+	private Data wrapAndAnnotateAwardTable(Table awardTable) {
+		Data awardTableData = new BasicData(awardTable, Table.class.getName());
+			
+		Dictionary awardTableMetadata = awardTableData.getMetadata();
+		awardTableMetadata.put(DataProperty.LABEL, "Basic NSF Awards Information");
+		awardTableMetadata.put(DataProperty.PARENT, data[0]);
+		awardTableMetadata.put(DataProperty.TYPE, DataProperty.TABLE_TYPE);
+			
+		return awardTableData;
+	}
 }
