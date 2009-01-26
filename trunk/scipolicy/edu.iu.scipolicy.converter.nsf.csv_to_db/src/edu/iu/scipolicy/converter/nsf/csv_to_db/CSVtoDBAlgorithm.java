8eb54375ea3cd96a012c5213480eafc9592d1524
Micah Linnemeier <mwlinnem@indiana.edu>
2009-01-26 02:10:01 +0000

    Refactored and made several small changes. Added parsing for multiple date formats.

--- trunk/scipolicy/edu.iu.scipolicy.converter.nsf.csv_to_db/src/edu/iu/scipolicy/converter/nsf/csv_to_db/CSVtoDBAlgorithm.java
+++ trunk/scipolicy/edu.iu.scipolicy.converter.nsf.csv_to_db/src/edu/iu/scipolicy/converter/nsf/csv_to_db/CSVtoDBAlgorithm.java
@@ -11,12 +11,9 @@ import java.sql.Statement;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Dictionary;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 import org.cishell.framework.CIShellContext;
 import org.cishell.framework.algorithm.Algorithm;
@@ -30,8 +27,9 @@ import org.cishell.service.database.DatabaseService;
 import org.osgi.service.log.LogService;
 
 import au.com.bytecode.opencsv.CSVReader;
+import edu.iu.scipolicy.utilities.nsf.NsfNames;
 
-public class CSVtoDBAlgorithm implements Algorithm {
+public class CSVtoDBAlgorithm implements Algorithm  {
 	private Data[] data;
 
 	private LogService log;
@@ -43,97 +41,102 @@ public class CSVtoDBAlgorithm implements Algorithm {
 		this.log = (LogService) context.getService(LogService.class.getName());
 		this.dbService = (DatabaseService) context.getService(DatabaseService.class.getName());
 	}
-
+	
+	/*
+	 * Insert NSF grant data, held in a comma-separated value file, into a relational database.
+	 * (NSF awarded grant data comes from the National Science Foundation website at nsf.gov ).
+	 */
 	public Data[] execute() throws AlgorithmExecutionException {
-		try {
-		// unpack nsf-csv file
-
-		File nsfCSV = (File) data[0].getData();
-
-		// create nsf-db schema based on nsf-csv columns available
-
-		Connection nsfDBConnection = null;
-		try {
-			// (create empty database)
-			DataSourceWithID emptyDB = dbService.createDatabase();
-
-			// (begin reading nsf-csv file header)
-			CSVReader nsfCSVReader = new CSVReader(new FileReader(nsfCSV), ',', '"', 0, '\\');
-
-			String[] headerLine = nsfCSVReader.readNext();
-			if (headerLine == null || headerLine.length == 0) {
-				throw new AlgorithmExecutionException("Cannot read in an empty nsf file");
-			}
-
-			Map<String, Integer> columnNameToColumnIndex = new HashMap<String, Integer>();
-			for (int columnIndex = 0; columnIndex < headerLine.length; columnIndex++) {
-				columnNameToColumnIndex.put(headerLine[columnIndex], columnIndex);
-			}
-			
-			Set<String> columnsPresentInCSV = new HashSet<String>(Arrays.asList(headerLine));
-
-			DataSourceWithID nsfDBInProgress = emptyDB;
-			
-			nsfDBConnection = nsfDBInProgress.getConnection();
-
-			addEmptyAwardTable(nsfDBConnection, columnsPresentInCSV, nsfDBInProgress.getID());
-
-			// TODO: Add all tables to nsf DB
 		
-			// fill nsf-db with contents of nsf-csv
+		Connection nsfDBConnection = null;
+		
+		try {
+	
+		//prepare to create and fill the nsf database with nsf csv contents.
 			
-			fillNsfDbQuickly(nsfDBConnection, nsfCSVReader, columnNameToColumnIndex, columnsPresentInCSV, nsfDBInProgress.getID());
-			
-			// annotate nsf-db with appropriate metadata
+		File nsfCsv = (File) data[0].getData();
 
-			Data nsfDBData = annotateAndWrapAsData(nsfDBInProgress);
-			
-			// return nsf-db
-			
-			return new Data[]{nsfDBData};
+		CSVReader nsfCsvReader = createNsfCsvReader(nsfCsv);
 
+		Map<String, Integer> columnNameToColumnIndex = 
+			createMapFromNsfColumnNameToColumnIndex(nsfCsvReader);
+			
+		/*
+		 * create an empty database,
+		 * add nsf tables to it, and
+		 * fill those tables with data from csv.
+		 */
+		
+		DataSourceWithID emptyDb = dbService.createDatabase();
+		DataSourceWithID nsfDBInProgress = emptyDb;	
+		nsfDBConnection = nsfDBInProgress.getConnection();
+		
+		//(side-effects the nsf database to contain all the tables we have to fill.)
+		createDbTables(nsfDBConnection, nsfDBInProgress.getID());
+
+		//(side-effects the nsf database to fill the tables using the nsf csv contents.)
+		fillNsfDbQuickly(nsfDBConnection, nsfDBInProgress.getID(), nsfCsvReader, columnNameToColumnIndex);
+
+		//annotate and return the nsf database
+		
+		Data nsfDBData = annotateAndWrapAsData(nsfDBInProgress);
+			
+		return new Data[]{nsfDBData};
+
+		/*
+		 * Error handling: throw an exception, aborting the algorithm.
+		 */
+			
 		} catch (DatabaseCreationException e) {
-			throw new AlgorithmExecutionException("Error occurred while creating database", e);
+			String message = 
+				"Error occurred while creating nsf database." +
+				"Aborting nsf database creation.";
+			throw new AlgorithmExecutionException(message, e);
 		} catch (FileNotFoundException e) {
-			throw new AlgorithmExecutionException("Could not find nsf file to read in", e);
+			String message = e.getMessage() + "." + 
+				" Aborting nsf database creation.";
+			throw new AlgorithmExecutionException(message, e);
 		} catch (IOException e) {
-			throw new AlgorithmExecutionException("Error occurred while reading nsf file", e);
+			String message = 
+				e.getMessage() + "." +
+				"Aborting nsf database creation.";
+			throw new AlgorithmExecutionException(message, e);
 		} catch (SQLException e) {
-			throw new AlgorithmExecutionException("Could not establish connection to database", e);
+			String message = 
+				"Error occurred while creating nsf database." +
+				"Aborting nsf database creation.";
+			throw new AlgorithmExecutionException(message, e);
 		} finally {
 			if (nsfDBConnection != null) {
 					try {
 						nsfDBConnection.close();
 					} catch (SQLException e) {
-						throw new AlgorithmExecutionException("Error occurred while closing database connection", e);
+						String message = 
+							"Error occurred while closing database connection";
+						throw new AlgorithmExecutionException(message, e);
 					}
 			}
 		}
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new AlgorithmExecutionException(e);
-		}
-	}				
-
-	private void addEmptyAwardTable(Connection dbConnection, Set columnsPresentInCSV, int dbID)
+	}		
+	
+	private void createDbTables(Connection dbConnection, int dbID)
 			throws AlgorithmExecutionException {
-		// make the actual SQL to create the table
-
+		try {
 		// TODO: treat some columns as optional and others as mandatory (or is this too much of a hassle)
 		// TODO: Tie this in with constants that control the names of NSF columns and their corresponding db column
 		// names
-		String createAwardTableSQL = "" + "CREATE TABLE AWARD" + dbID + 
+		String createAwardTableSQL = "" + "CREATE TABLE " + NsfNames.DB.AWARD_TABLE + dbID + 
 		"(" + 
-		"AWARD_NUMBER INTEGER," +
-		"TITLE VARCHAR(500)," +
-		"START_DATE DATE," + 
-		"EXPIRATION_DATE DATE," +
-		"AWARDED_AMOUNT_TO_DATE INTEGER," + 
-		"PRIMARY KEY (AWARD_NUMBER)" + 
+		NsfNames.DB.AWARD_NUMBER + " INTEGER," +
+		NsfNames.DB.AWARD_TITLE + " VARCHAR(500)," +
+		NsfNames.DB.AWARD_START_DATE + " DATE," + 
+		NsfNames.DB.AWARD_EXPIRATION_DATE + " DATE," +
+		NsfNames.DB.AWARDED_AMOUNT_TO_DATE + " INTEGER," + 
+		"PRIMARY KEY (" + NsfNames.DB.AWARD_NUMBER + ")" + 
 		")";
 
 		// execute the award table creation SQL
-		try {
+
 			Statement createAwardTableStatement = dbConnection.createStatement();
 			createAwardTableStatement.execute(createAwardTableSQL);
 		} catch (SQLException e) {
@@ -141,20 +144,27 @@ public class CSVtoDBAlgorithm implements Algorithm {
 		}
 	}
 	
-	//Contract specifies this must be FAST! (potential bottleneck)
-	private void fillNsfDbQuickly(Connection nsfDbConnection, CSVReader nsfCSVReader, Map<String, Integer> columnNameToColumnIndex, Set<String> columnsPresentInCSV,
-			int dbID)
+	//this must be FAST! (potential bottleneck)
+	private void fillNsfDbQuickly(Connection nsfDbConnection, int dbID, CSVReader nsfCSVReader,
+			Map<String, Integer> columnNameToColumnIndex)
 		throws AlgorithmExecutionException {
-		
+		try {
+			
 		// TODO: Put data from every column (not just those currently needed) into nsf-db
-		// TODO: Tie column names of csv AND db to constants file
-		int awardNumberIndex = columnNameToColumnIndex.get("Award Number");
-		int awardTitleIndex = columnNameToColumnIndex.get("Title");
-		int startDateIndex = columnNameToColumnIndex.get("Start Date");
-		int expirationDateIndex = columnNameToColumnIndex.get("Expiration Date");
-		int awardAmountToDateIndex = columnNameToColumnIndex.get("Awarded Amount to Date");
+		int awardNumberIndex = columnNameToColumnIndex.get(NsfNames.CSV.AWARD_NUMBER);
+		int awardTitleIndex = columnNameToColumnIndex.get(NsfNames.CSV.AWARD_TITLE);
+		int startDateIndex = columnNameToColumnIndex.get(NsfNames.CSV.AWARD_START_DATE);
+		int expirationDateIndex = columnNameToColumnIndex.get(NsfNames.CSV.AWARD_EXPIRATION_DATE);
+		int awardedAmountToDateIndex = columnNameToColumnIndex.get(NsfNames.CSV.AWARDED_AMOUNT_TO_DATE);
 		
-		String insertIntoAwardSql = "INSERT INTO AWARD" + dbID + " (AWARD_NUMBER, TITLE, START_DATE, EXPIRATION_DATE, AWARDED_AMOUNT_TO_DATE)" +
+		String insertIntoAwardSql = "INSERT INTO " + NsfNames.DB.AWARD_TABLE + dbID + 
+		" (" + 
+		NsfNames.DB.AWARD_NUMBER + "," +
+		NsfNames.DB.AWARD_TITLE + "," +
+		NsfNames.DB.AWARD_START_DATE + "," + 
+		NsfNames.DB.AWARD_EXPIRATION_DATE + "," +
+		NsfNames.DB.AWARDED_AMOUNT_TO_DATE + 
+		") " +
 		"VALUES " 
 		+ "("
 		+ "?," //awardNumber
@@ -163,72 +173,56 @@ public class CSVtoDBAlgorithm implements Algorithm {
 		+ "?," //expirationDate
 		+ "?" //awardAmountToDate
 		+ ")";
-//		+ "'" + awardTitleString + "',"
-//		+ "'" + startDateString + "'," 
-//		+ "'" + expirationDateString + "'," 
-//		+ awardMoneyToDateString 
-//		+ ")";
 	
-		
-		try {
-			PreparedStatement insertIntoAward = nsfDbConnection.prepareStatement(insertIntoAwardSql);
-			String[] nextAwardLine = null;
-			int currentRowNumber = 1; //We read the header row earlier (kind of a hack)
-			while ((nextAwardLine = nsfCSVReader.readNext()) != null) {
-				//TODO: Maybe in the future we can be more lenient with improper formatting, but for now it's either all perfect or we abort.
-				String awardNumberString = nextAwardLine[awardNumberIndex];
-				int awardNumber = Integer.parseInt(awardNumberString);
-				insertIntoAward.setInt(1, awardNumber);
-				
-				String awardTitleString = nextAwardLine[awardTitleIndex];
-				String awardTitle = awardTitleString;
-				insertIntoAward.setString(2, awardTitle);
-				
-				String startDateString = nextAwardLine[startDateIndex];
-				java.util.Date startDate = DateFormat.getDateInstance().parse(startDateString);
-				
-				// The year in dates should be full (so 1984 instead 84).
-				// Unfortunately, the Date class is pretty stupid about this, so
-				// fix it if necessary.
-				if (startDate.getYear() < 1900)
-					startDate.setYear(startDate.getYear() + 1900);
-
-				java.sql.Date startDateForSQL = new java.sql.Date(startDate.getTime());
-				insertIntoAward.setDate(3, startDateForSQL);
-				
-				String expirationDateString = nextAwardLine[expirationDateIndex];
-				java.util.Date expirationDate = DateFormat.getDateInstance().parse(expirationDateString);
-				
-				// Fix the year on the expiration date as well.
-				if (expirationDate.getYear() < 1900)
-					expirationDate.setYear(expirationDate.getYear() + 1900);
-				
-				java.sql.Date expirationDateForSQL = new java.sql.Date(expirationDate.getTime());
-				insertIntoAward.setDate(4, expirationDateForSQL);
-				
-				
-				String awardAmountToDateString = nextAwardLine[awardAmountToDateIndex];
-				int awardAmountToDate = Integer.parseInt(awardAmountToDateString);
-				insertIntoAward.setInt(5, awardAmountToDate);
-				
-				//(This would be a huge no-no if we cared about security at all, but I'm assuming we don't have anything to secure)
-				
-				insertIntoAward.addBatch();
-				
-				currentRowNumber++;
-				System.out.println("Putting CSV row " + currentRowNumber + " into database");
-			}
+		PreparedStatement insertIntoAward = nsfDbConnection.prepareStatement(insertIntoAwardSql);
+		String[] nextAwardLine = null;
+		int currentRowNumber = 1;
+		while ((nextAwardLine = nsfCSVReader.readNext()) != null) {
 			
-			//TODO: Check for errors 
+			//TODO: Maybe in the future we can be more lenient with improper formatting, but for now it's either all perfect or we abort.
+			/*
+			 * TODO: When we start working with multiple tables, 
+			 * and splitting csv fields into multiple db fields..
+			 * we will need to do something smarter here, but this
+			 * should be good for now
+			 */
+			
+			String awardNumberString = nextAwardLine[awardNumberIndex];
+			int awardNumber = Integer.parseInt(awardNumberString);
+			insertIntoAward.setInt(1, awardNumber);
+				
+			String awardTitleString = nextAwardLine[awardTitleIndex];
+			String awardTitle = awardTitleString;
+			insertIntoAward.setString(2, awardTitle);
+				
+			String startDateString = nextAwardLine[startDateIndex];
+			java.sql.Date startDate = parseDate(startDateString);
+			insertIntoAward.setDate(3, startDate);
+				
+			String expirationDateString = nextAwardLine[expirationDateIndex];
+			java.sql.Date expirationDate = parseDate(expirationDateString);
+			insertIntoAward.setDate(4, expirationDate);
+				
+				
+			String awardAmountToDateString = nextAwardLine[awardedAmountToDateIndex];
+			int awardAmountToDate = Integer.parseInt(awardAmountToDateString);
+			insertIntoAward.setInt(5, awardAmountToDate);
+
+			insertIntoAward.addBatch();
+			//TODO: Check warnings
 			insertIntoAward.executeBatch();
 			
+			currentRowNumber++;
+		}
+		
+		int numRows = currentRowNumber;
+		System.out.println("Successfully loaded " + numRows + " rows into the NSF database");
+		
 		} catch (IOException e) {
 			throw new AlgorithmExecutionException("An error occurred while loading nsf data into the database", e);
 		} catch (SQLException e) {
+			e.printStackTrace();
 			throw new AlgorithmExecutionException("An error occurred while loading nsf data into the database", e);
-		} catch (ParseException e) {
-			//TODO: This should probably say which date was wrong
-			throw new AlgorithmExecutionException("An error occurred while parsing a date from the nsf csv file", e);
 		} catch (NumberFormatException e) {
 			//TODO: Should say which number failed.
 			throw new AlgorithmExecutionException("An error occurred while attempting to parse a number from the nsf csv file", e);
@@ -243,21 +237,88 @@ public class CSVtoDBAlgorithm implements Algorithm {
 		
 		//annotate data
 		
-		Dictionary<String, String> dbMetadata = dbData.getMetadata();
+		Dictionary dbMetadata = dbData.getMetadata();
 		
 		//TODO: Get more information and use it to create better label
 	  	dbMetadata.put(DataProperty.LABEL, "NSF award data");
+	  	dbMetadata.put(DataProperty.PARENT, data[0]);
 	  	//TODO: Make a database icon?
 	  	
 	  	return dbData;
 	}
 	
-	private void handleParsingException(int row, String columnName, String cellContents, String parseTypeName, Exception e) 
+	private CSVReader createNsfCsvReader(File nsfCsv) throws IOException {
+		//TODO: Currently we only support "csv" nsf files, not "excel" nsf files.
+		//TODO: Add flexibility to support tabbed separated and double-quote escape chars.
+		final char fieldSeparator = ',';
+		final char fieldQuoteCharacter = '"';
+		final int lineToStartReadingFrom = 0;
+		final char quoteEscapeCharacter = '\\';
+		
+		CSVReader nsfCsvReader = new CSVReader(new FileReader(nsfCsv),
+				fieldSeparator, fieldQuoteCharacter, lineToStartReadingFrom, quoteEscapeCharacter);
+		
+		return nsfCsvReader;
+	}
+	
+	private Map<String, Integer> createMapFromNsfColumnNameToColumnIndex(CSVReader nsfCsvReader) 
+		throws AlgorithmExecutionException, IOException {
+		String[] columnNames = nsfCsvReader.readNext();
+		if (columnNames == null || columnNames.length == 0) {
+			throw new AlgorithmExecutionException("Cannot read in an empty nsf file");
+		}
+
+		Map<String, Integer> columnNameToColumnIndex = new HashMap<String, Integer>();
+		for (int columnIndex = 0; columnIndex < columnNames.length; columnIndex++) {
+			columnNameToColumnIndex.put(columnNames[columnIndex].trim(), columnIndex);
+		}
+		
+		return columnNameToColumnIndex;
+	}
+	
+//	private void handleParsingException(int row, String columnName, String cellContents, String parseTypeName, Exception e) 
+//		throws AlgorithmExecutionException {
+//		String errorMessage = "" +
+//		"Encountered a formatting error in row " + row + ", in the '" + columnName + "' column. '" +
+//		cellContents + "' cannot be parsed as type '" + parseTypeName + "'. " + 
+//		"Aborting loading process.";
+//		throw new AlgorithmExecutionException(errorMessage, e);
+//	}
+	
+	private static final DateFormat[] ACCEPTED_DATE_FORMATS = { 
+		DateFormat.getDateInstance(DateFormat.FULL),
+		new SimpleDateFormat("dd/MM/yy"),
+		new SimpleDateFormat("dd/MM/yyyy"),
+		DateFormat.getDateInstance(DateFormat.SHORT),
+		DateFormat.getDateInstance(DateFormat.MEDIUM),
+		DateFormat.getDateInstance(DateFormat.LONG),
+		};
+	
+	private java.sql.Date parseDate(String dateString) 
 		throws AlgorithmExecutionException {
-		String errorMessage = "" +
-		"Encountered a formatting error in row " + row + ", in the '" + columnName + "' column. '" +
-		cellContents + "' cannot be parsed as type '" + parseTypeName + "'. " + 
-		"Aborting loading process.";
-		throw new AlgorithmExecutionException(errorMessage, e);
+		for (DateFormat format : ACCEPTED_DATE_FORMATS) {
+			try {
+				format.setLenient(false);
+				java.util.Date date = format.parse(dateString);
+				//WE PARSED THE DATE SUCCESSFULLY (if we get to this point)!
+				//Finish up our processing and return the date.
+				
+				//TODO: Methinks this is a hack we should eliminate
+				if (date.getYear() < 1900)
+					date.setYear(date.getYear() + 1900);
+				java.sql.Date dateForSQL = new java.sql.Date(date.getTime());
+				return dateForSQL;
+			} catch (ParseException e) {
+				continue;
+			}
+		}
+		
+		//we could not parse the date with any of the accepted formats.
+		
+		String exceptionMessage = 
+			"Could not parse the field " + 
+			"'" + dateString + "'" +
+			" as a date. Aborting the algorithm.";
+		throw new AlgorithmExecutionException(exceptionMessage);
 	}
 }
