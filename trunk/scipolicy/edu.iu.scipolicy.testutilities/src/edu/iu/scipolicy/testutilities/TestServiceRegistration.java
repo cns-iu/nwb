b3018a862639d9b3cff6ff1a16deb961613b9a17
Patrick Phillips <pataphil@indiana.edu>
2009-02-09 17:27:12 +0000

    Initial commit.

---
+++ trunk/scipolicy/edu.iu.scipolicy.testutilities/src/edu/iu/scipolicy/testutilities/TestServiceRegistration.java
@@ -0,0 +1,23 @@
+package edu.iu.scipolicy.testutilities;
+
+import java.util.Dictionary;
+
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+
+/**
+ * This class is necessary because TestContext.registerService is called by the
+   services themselves, and they expect the result (which is a ServiceRegistration)
+   to be non-null.
+ **/
+public class TestServiceRegistration implements ServiceRegistration {
+	public ServiceReference getReference() {
+		return null;
+	}
+
+	public void setProperties(Dictionary properties) {
+	}
+
+	public void unregister() {
+	}
+}
