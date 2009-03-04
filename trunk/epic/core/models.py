338b4178721846020688b5e6e191826cd933b218
Patrick Phillips <pataphil@indiana.edu>
2009-03-04 22:13:00 +0000

    * Added Profile and Item models.

--- trunk/epic/core/models.py
+++ trunk/epic/core/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+	user = models.ForeignKey(User)
+	affiliation = models.CharField(max_length = 512)
+
+class Item(models.Model):
+	user = models.ForeignKey(User)
+	name = models.CharField(max_length = 256)
+	description = models.CharField(max_length = 1024)
+	timestamp = models.DateTimeField("Date created")
