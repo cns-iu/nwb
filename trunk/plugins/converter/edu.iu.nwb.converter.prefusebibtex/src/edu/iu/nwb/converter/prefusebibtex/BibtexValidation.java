9fe1ad96b219c139d91c672ec95f601aceea30cb
Chintan Tank <cdtank@indiana.edu>
2009-03-04 15:53:53 +0000

    Refactored the code for File > Load mechanism suitably.Made changes in all the input file validators to include detailed comments. Fixed the non-validation issue for the ISI file format.on trying to load a file without any extension does not give an error but displays another dialog box for selecting the File Loader e.g. Bibtex, Pajek, ISI etc. Validation is performed on selected file loader throwing either ERROR or loading the file with that loader. Code Reviewed By Micah

--- trunk/plugins/converter/edu.iu.nwb.converter.prefusebibtex/src/edu/iu/nwb/converter/prefusebibtex/BibtexValidation.java
+++ trunk/plugins/converter/edu.iu.nwb.converter.prefusebibtex/src/edu/iu/nwb/converter/prefusebibtex/BibtexValidation.java
@@ -2,6 +2,7 @@ package edu.iu.nwb.converter.prefusebibtex;
 
 
 import java.io.File;
+import java.io.FileReader;
 import java.util.Dictionary;
 
 import org.cishell.framework.CIShellContext;
@@ -11,10 +12,15 @@ import org.cishell.framework.algorithm.AlgorithmFactory;
 import org.cishell.framework.data.BasicData;
 import org.cishell.framework.data.Data;
 import org.cishell.framework.data.DataProperty;
-import org.osgi.service.log.LogService;
+
+import bibtex.dom.BibtexFile;
+import bibtex.expansions.ExpansionException;
+import bibtex.expansions.MacroReferenceExpander;
+import bibtex.parser.BibtexParser;
 
 
 public class BibtexValidation implements AlgorithmFactory {
+	
 	public Algorithm createAlgorithm(Data[] data, Dictionary parameters, CIShellContext context) {
 		return new BibtexValidationAlgorithm(data, parameters, context);
 	}
@@ -24,26 +30,52 @@ public class BibtexValidation implements AlgorithmFactory {
 		Dictionary parameters;
 		CIShellContext context;
 
-		public BibtexValidationAlgorithm(Data[] data, Dictionary parameters, CIShellContext context) {
-			this.data = data;
+		public BibtexValidationAlgorithm(Data[] inputData, Dictionary parameters, CIShellContext ciShellContext) {
+			this.data = inputData;
 			this.parameters = parameters;
-			this.context = context;
+			this.context = ciShellContext;
+			
+			
 		}
 
 		public Data[] execute() throws AlgorithmExecutionException {
-			String fileHandler = (String) data[0].getData();
-			File inData = new File(fileHandler);
 
+			String fileHandler = (String) data[0].getData();
+			File inputData = new File(fileHandler);
 			try{
-				Data[] dm = new Data[] {new BasicData(inData, "file:text/bibtex")};
-				dm[0].getMetadata().put(DataProperty.LABEL, "BibTeX File: " + fileHandler);
-				dm[0].getMetadata().put(DataProperty.TYPE, DataProperty.TABLE_TYPE);
-				return dm;
+				/*
+				 * validator function is used to validate the data & only if the input file has valid bibtex data 
+				 * then its loaded into the workbench for further manipulations by the user. 
+				 * */
+				validateSelectedFileforBibtexFormat(fileHandler);
+				Data[] validationData = new Data[] {new BasicData(inputData, "file:text/bibtex")};
+				validationData[0].getMetadata().put(DataProperty.LABEL, "BibTeX File: " + fileHandler);
+				validationData[0].getMetadata().put(DataProperty.TYPE, DataProperty.TABLE_TYPE);
+				return validationData;
 			}catch (SecurityException exception){
 				throw new AlgorithmExecutionException(exception);
 			}
 
+		}
 
+		private void validateSelectedFileforBibtexFormat(String fileHandler)
+				throws AlgorithmExecutionException {
+			BibtexFile bibtexFile = new BibtexFile();
+	    	BibtexParser parser = new BibtexParser(true);
+	    	try {
+	    	parser.parse(bibtexFile, new FileReader(fileHandler));
+	    	} catch (Exception e) {
+	    		throw new AlgorithmExecutionException("Fatal exception occurred while parsing bibtex file.", e);
+	    	} finally {
+//	    		logger.log(LogService.LOG_ERROR, "Error occured while loading " + fileHandler);
+	    	}
+	    	try {
+	    	MacroReferenceExpander macroExpander = 
+	    		new MacroReferenceExpander(true, true, false, false);
+	    	macroExpander.expand(bibtexFile);
+	    	} catch (ExpansionException e) {
+	    		throw new AlgorithmExecutionException("Error occurred while parsing bibtex file.", e);
+	    	}
 		}
 	}
 }
