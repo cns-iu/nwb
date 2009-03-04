270e68ea0280f87975449e67e1b7b14ef3f23d97
Micah Linnemeier <mwlinnem@indiana.edu>
2009-03-04 22:18:23 +0000

    Added DataRequest model code.

--- trunk/epic/datarequests/models.py
+++ trunk/epic/datarequests/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from epic.core.models import Item
 
-# Create your models here.
+REQUEST_STATUS = (
+    ('U' , 'unfulfilled'),
+    ('F' , 'fulfilled'),
+    ('C' , 'canceled'),
+)
+
+class DataRequest(models.Model):
+    item = models.OneToOneField(Item, primary_key=true)
+    status = models.CharField(max_length=1, choices=REQUEST_STATUS)
