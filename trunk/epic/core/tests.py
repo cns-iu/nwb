bfc3dc7bb1155dc1260722f334d1082d73dc2a7c
David Coe <dmcoe@indiana.edu>
2009-03-05 21:24:44 +0000

    Added tests.

---
+++ trunk/epic/core/tests.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+
+from django.contrib.auth.models import User
+
+class UserTestCase(TestCase):
+    fixtures = ['initial_users']
+    
+    def setUp(self):
+        pass
+    
+    def testLoginSuccess(self):
+        response = self.client.get('/login/')
+        self.failUnlessEqual(response.status_code, 200, "Error reaching login!")
+        response = self.client.post("/login/", data={'username': 'bob', 'password':'bob',})
+        self.failUnlessEqual(response.status_code, 302, "Error logging in!")
+        #TODO Once the follow=true actually works it should be used here.
+                                
+    def testLoginFailure(self):
+        response = self.client.post("/login/", {'username': 'bob', 'password':'bob2',})
+        self.failUnlessEqual(response.status_code, 200)
+        response = self.client.post("/login/", {'username': 'bob', 'password':'bob2',})
+        self.failUnless("Your username and password didn't match. Please try again." in response.content)
+        
+    def testLogout(self):
+        response = self.client.get('/logout/')
+        self.failUnlessEqual(response.status_code, 302)
+        response = self.client.get('/')
+        self.failUnless("login" in response.content)
