dd6f16aa108492d052cf3eae0bc2e0a538a40a3e
Micah Linnemeier <mwlinnem@indiana.edu>
2009-03-05 17:05:58 +0000

    Switched Dataset to inherit from Item instead of having it as a explicit one-to-one relationship.

--- trunk/epic/datasets/models.py
+++ trunk/epic/datasets/models.py
@@ -8,34 +8,28 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class DataSet(models.Model):
+class DataSet(Item):
     """
     This is the DataSet model.  You work with it as follows:
-    
+     
     >>> user = User(username="bob", password="blah")
     >>> user.save()
     >>> user
     <User: bob>
     
-    >>> from datetime import datetime
-    >>> item = Item(creator=user, name="Item #1", description="This is the first item", created_at=datetime.now())
-    >>> item
-    <Item: Item object>
-    >>> item.save()
-    
-    >>> dataset = DataSet(item=item, timestamp=datetime.now())
+    >>> from datetime import datetime 
+    >>> dataset = DataSet(creator=user, name="Item #1", description="This is the first item", created_at=datetime.now(),timestamp=datetime.now())
     >>> dataset.save()
-    >>> dataset.item
-    <Item: Item object>
+    
     """
-    item = models.ForeignKey(Item)
+    
     timestamp = models.DateTimeField('timestamp')
     
     class Admin:
         pass
     
     def __unicode__(self):
-        return "Dataset %s created at %s" % (self.item, self.timestamp)
+        return "Dataset %s created at %s" % (self.name, self.timestamp)
     
     def get_absolute_url(self):
         return "/datasets/%i" % self.id
