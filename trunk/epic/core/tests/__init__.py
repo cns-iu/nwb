3835de3c00779f02318824545575478cc8ac9e15
Patrick Phillips <pataphil@indiana.edu>
2009-03-06 22:38:14 +0000

    * Moved all tests into tests folder. * Split up tests into separate files (for cleanliness).

---
+++ trunk/epic/core/tests/__init__.py
@@ -0,0 +1,6 @@
+from tests import *
+from change_password_tests import *
+from display_user_profile_tests import *
+from log_in_and_out_tests import *
+from profile_dataset_tests import *
+from profile_link_tests import *
