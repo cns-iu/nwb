d03ba024f36735d47eef022f3c0df880a087681c
David Coe <dmcoe@indiana.edu>
2009-03-06 22:34:52 +0000

    TODO make time less ugly Index page now has links to datasets and requests and displays all the datasets.

--- trunk/epic/core/views.py
+++ trunk/epic/core/views.py
@@ -4,9 +4,13 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 
+from epic.datarequests.models import DataRequest
+from epic.datasets.models import DataSet
+
 def index(request):
-    return render_to_response('core/index.html', {'user':request.user})
+	datasets = DataSet.objects.all().order_by('-created_at')
+	return render_to_response('core/index.html', {'user':request.user, 'datasets':datasets,})
 
 def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect(reverse('epic.core.views.index',))
+	logout(request)
+	return HttpResponseRedirect(reverse('epic.core.views.index',))
