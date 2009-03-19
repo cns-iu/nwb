b87426bc0d089184d9444a0e3c4ee0d8b90df694
David Coe <dmcoe@indiana.edu>
2009-03-19 18:00:03 +0000

    * Removed the test for only datasets on the profile page because datarequests are now also shown on this page.

--- trunk/epic/core/tests/profile_dataset_tests.py
+++ trunk/epic/core/tests/profile_dataset_tests.py
@@ -22,11 +22,3 @@ class ProfileDatasetTestCase(TestCase):
 		self.failUnless(response.status_code, 200)
 		self.failUnless("Your Datasets" in response.content, response.content)
 		self.failUnless("edit" in response.content, response.content)
-	
-	def testForOnlyDataSets(self):
-		login = self.client.login(username='bill', password='bill')
-		self.failUnless(login, 'Could not login')
-		response = self.client.get('/user/')
-		self.failUnless(response.status_code, 200)
-		#This is a datarequest object and should not be here
-		self.failIf("Item 3" in response.content)
