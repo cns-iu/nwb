e998aeb2fe28b9566d1de76a13f10ff67d0f059e
David Coe <dmcoe@indiana.edu>
2009-03-19 19:07:58 +0000

    * Got the user out of the context rather than getting the request from the context and getting the user from the request.

--- trunk/epic/datasets/templatetags/dataset_templatetags.py
+++ trunk/epic/datasets/templatetags/dataset_templatetags.py
@@ -5,8 +5,8 @@ register = template.Library()
 
 @register.inclusion_tag('templatetags/list_datasets.html', takes_context=True)
 def list_datasets(context, datasets):
-	request = context['request']
-	return {'datasets':datasets, 'user':request.user}
+	user = context['user']
+	return {'datasets':datasets, 'user':user}
 
 @register.inclusion_tag("templatetags/show_edit_dataset_metadata_form.html")
 def show_edit_dataset_metadata_form(dataset, edit_dataset_metadata_form):
