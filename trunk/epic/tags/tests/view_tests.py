266e8527591fd1379acf0202d9ae322891e550ae
David Coe <dmcoe@indiana.edu>
2009-03-25 20:01:20 +0000

    BUGFIX * Tests for tags were not correct so I fixed them. * The template for tags was not correct so I fixed that too.

--- trunk/epic/tags/tests/view_tests.py
+++ trunk/epic/tags/tests/view_tests.py
@@ -1,12 +1,17 @@
 from django.test import TestCase
-
+from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
+from epic.tags.models import Tagging
 
 class ViewTests(TestCase):
 	fixtures = ['tags']
 	
 	def setUp(self):
-		pass
+		self.tag1 = Tagging.objects.get(pk=1)
+		self.tag2 = Tagging.objects.get(pk=2)
+		self.tag3 = Tagging.objects.get(pk=3)
+		# Fixtures are broken tag3 is in twice
+		self.tag4 = Tagging.objects.get(pk=5)
 	
 	def tearDown(self):
 		pass
@@ -15,10 +20,12 @@ class ViewTests(TestCase):
 		# Verify that all the tags show up on the index page
 		response = self.client.get('/tags/')
 		self.assertEqual(response.status_code, 200)
-		self.assertTrue('<a href="/tags/tag1/">tag1</a>' in response.content, response.content)
-		self.assertTrue('<a href="/tags/tag2/">tag2</a>' in response.content, response.content)
-		self.assertTrue('<a href="/tags/tag3/">tag3</a>' in response.content, response.content)
-		self.assertTrue('<a href="/tags/tag4/">tag4</a>' in response.content, response.content)
+		
+		self.assertContains(response, '<a href="'+ reverse('epic.tags.views.view_items_for_tag', kwargs={'tag_name': self.tag1.tag}) +'"')
+		self.assertContains(response, '<a href="'+ reverse('epic.tags.views.view_items_for_tag', kwargs={'tag_name': self.tag2.tag}) +'"')
+		self.assertContains(response, '<a href="'+ reverse('epic.tags.views.view_items_for_tag', kwargs={'tag_name': self.tag3.tag}) +'"')
+		self.assertContains(response, '<a href="'+ reverse('epic.tags.views.view_items_for_tag', kwargs={'tag_name': self.tag4.tag}) +'"')
+		
 	
 	def testTagsPage(self):
 		# Test the page for tag 1
@@ -46,14 +53,15 @@ class ViewTests(TestCase):
 		# Test the page for dataset 1
 		response = self.client.get('/datasets/1/')
 		self.assertEqual(response.status_code, 200)
-		self.assertTrue('<a href="/tags/tag1/">tag1</a>' in response.content, response.content)
-		self.assertTrue('<a href="/tags/tag2/">tag2</a>' in response.content, response.content)
-		self.assertTrue('<a href="/tags/tag3/">tag3</a>' in response.content, response.content)
-		self.assertTrue('<a href="/tags/tag4/">tag4</a>' not in response.content, response.content)
+		self.assertContains(response, '<a href="'+ reverse('epic.tags.views.view_items_for_tag', kwargs={'tag_name': self.tag1.tag}) +'"')
+		self.assertContains(response, '<a href="'+ reverse('epic.tags.views.view_items_for_tag', kwargs={'tag_name': self.tag2.tag}) +'"')
+		self.assertContains(response, '<a href="'+ reverse('epic.tags.views.view_items_for_tag', kwargs={'tag_name': self.tag3.tag}) +'"')
+		self.assertNotContains(response, '<a href="'+ reverse('epic.tags.views.view_items_for_tag', kwargs={'tag_name': self.tag4.tag}) +'"')
+		
 		# Test the page for dataset2
 		response = self.client.get('/datasets/2/')
 		self.assertEqual(response.status_code, 200)
-		self.assertTrue('<a href="/tags/tag1/">tag1</a>' not in response.content, response.content)
-		self.assertTrue('<a href="/tags/tag2/">tag2</a>' not in response.content, response.content)
-		self.assertTrue('<a href="/tags/tag3/">tag3</a>' in response.content, response.content)
-		self.assertTrue('<a href="/tags/tag4/">tag4</a>' in response.content, response.content)
+		self.assertNotContains(response, '<a href="'+ reverse('epic.tags.views.view_items_for_tag', kwargs={'tag_name': self.tag1.tag}) +'"')
+		self.assertNotContains(response, '<a href="'+ reverse('epic.tags.views.view_items_for_tag', kwargs={'tag_name': self.tag2.tag}) +'"')
+		self.assertContains(response, '<a href="'+ reverse('epic.tags.views.view_items_for_tag', kwargs={'tag_name': self.tag3.tag}) +'"')
+		self.assertContains(response, '<a href="'+ reverse('epic.tags.views.view_items_for_tag', kwargs={'tag_name': self.tag4.tag}) +'"')
