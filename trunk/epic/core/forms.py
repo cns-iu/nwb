31766be440e35e8e24b5145bc86714cf516a7db3
Micah Linnemeier <mwlinnem@indiana.edu>
2009-04-01 22:51:16 +0000

    Made login form slightly prettier. Will need some attention from Elisha.

--- trunk/epic/core/forms.py
+++ trunk/epic/core/forms.py
@@ -25,3 +25,49 @@ class ProfileForm(ModelForm):
 	class Meta:
 		model = Profile
 		exclude = ['user']
+		
+#TODO: This is a copy and paste of Django's AuthenticationForm, with a different max_length. Make this extend AuthenticationForm or something other than this hack
+class ShortAuthenticationForm(forms.Form):
+    """
+    Base class for authenticating users. Extend this to get a form that accepts
+    username/password logins.
+    """
+    username = forms.CharField(label=("Username"), max_length=30, widget=forms.TextInput(attrs={'size':15}))
+    password = forms.CharField(label=("Password"), widget=forms.PasswordInput(attrs={'size': 15}))
+
+    def __init__(self, request=None, *args, **kwargs):
+        """
+        If request is passed in, the form will validate that cookies are
+        enabled. Note that the request (a HttpRequest object) must have set a
+        cookie with the key TEST_COOKIE_NAME and value TEST_COOKIE_VALUE before
+        running this validation.
+        """
+        self.request = request
+        self.user_cache = None
+        super(ShortAuthenticationForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username and password:
+            self.user_cache = authenticate(username=username, password=password)
+            if self.user_cache is None:
+                raise forms.ValidationError(_("Please enter a correct username and password. Note that both fields are case-sensitive."))
+            elif not self.user_cache.is_active:
+                raise forms.ValidationError(_("This account is inactive."))
+
+        # TODO: determine whether this should move to its own method.
+        if self.request:
+            if not self.request.session.test_cookie_worked():
+                raise forms.ValidationError(_("Your Web browser doesn't appear to have cookies enabled. Cookies are required for logging in."))
+
+        return self.cleaned_data
+
+    def get_user_id(self):
+        if self.user_cache:
+            return self.user_cache.id
+        return None
+
+    def get_user(self):
+        return self.user_cache
--- trunk/epic/core/templatetags/core_templatetags.py
+++ trunk/epic/core/templatetags/core_templatetags.py
@@ -1,6 +1,7 @@
 from django import template
 from django.template import RequestContext
 from django.contrib.auth.forms import AuthenticationForm
+from epic.core.forms import ShortAuthenticationForm
 	
 from epic.datasets.models import DataSet
 
@@ -8,5 +9,5 @@ register = template.Library()
 
 @register.inclusion_tag('core/login_box.html')
 def login_box():
-	form = AuthenticationForm()
+	form = ShortAuthenticationForm()
 	return {'login_form':form}
