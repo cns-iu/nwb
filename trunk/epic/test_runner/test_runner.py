705044b1d770835e7119661232b08a3e22261dd4
Russell Duhon <rduhon@indiana.edu>
2009-04-01 15:59:51 +0000

    Make the test runner be the xml test runner.

--- trunk/epic/test_runner/test_runner.py
+++ trunk/epic/test_runner/test_runner.py
@@ -2,11 +2,13 @@ import django.test.simple
 import os
 import shutil
 
+import xmlrunner.extra.djangotestrunner
+
 def run_tests(test_labels, verbosity=1, interactive=True, extra_tests=[]):
 
     #filterwarnings("ignore")
     
-    result = django.test.simple.run_tests(test_labels, verbosity, extra_tests)
+    result = xmlrunner.extra.djangotestrunner.run_tests(test_labels, verbosity, extra_tests)
     print "deleting temporary_media_root..."
     shutil.rmtree('temporary_media_root/', ignore_errors=True)
     return result
