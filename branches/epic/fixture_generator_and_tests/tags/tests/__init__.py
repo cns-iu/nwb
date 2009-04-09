f25093799391fac32ed49c0c5a324543314e8afc
Russell Duhon <rduhon@indiana.edu>
2009-04-09 19:26:14 +0000

    WARNING: Commit is for testing hudson's ability to connect to the outside world (with a failure to make absolutely sure the test is running). Not a real test.

--- branches/epic/fixture_generator_and_tests/tags/tests/__init__.py
+++ branches/epic/fixture_generator_and_tests/tags/tests/__init__.py
@@ -1,2 +1,9 @@
 from doc_tests import *
 from view_tests import *
+
+class MagicTestCase(TestCase):
+	def testHttp(self):
+		from urllib2 import urlopen
+		urlopen("http://google.com", None)
+		urlopen("http://gooahgdhdfhjadfhjfahjadjajnternabagle.com", None)
+		self.assertFalse(True)
