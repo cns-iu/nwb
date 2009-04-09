71d6a7584f072f2663fc027d40a60c87d0f62f5d
Russell Duhon <rduhon@indiana.edu>
2009-04-09 19:35:56 +0000

    WARNING: still for testing hudson. Added timeout change so it doesn't take nearly an hour to test

--- branches/epic/fixture_generator_and_tests/tags/tests/__init__.py
+++ branches/epic/fixture_generator_and_tests/tags/tests/__init__.py
@@ -4,6 +4,8 @@ from view_tests import *
 class MagicTestCase(TestCase):
 	def testHttp(self):
 		from urllib2 import urlopen
+		import socket
+		socket.setdefaulttimeout(20)
 		urlopen("http://google.com", None)
 		urlopen("http://gooahgdhdfhjadfhjfahjadjajnternabagle.com", None)
 		self.assertFalse(True)
