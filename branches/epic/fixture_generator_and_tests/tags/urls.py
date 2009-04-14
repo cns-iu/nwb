1bc4076a1948d785c38cce673979c99435e77d2b
David Coe <dmcoe@indiana.edu>
2009-04-14 20:26:25 +0000

    * A user can delete his tags from the view dataset page!

--- branches/epic/fixture_generator_and_tests/tags/urls.py
+++ branches/epic/fixture_generator_and_tests/tags/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('epic.tags.views',
     (r'^$', 'index'),
+    (r'^delete_tag/$', 'delete_tag'),
     (r'^(?P<tag_name>.+)/$', 'view_items_for_tag'),
 )
--- branches/epic/fixture_generator_and_tests/tags/views.py
+++ branches/epic/fixture_generator_and_tests/tags/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.utils import simplejson
+from django.utils.datastructures import MultiValueDictKeyError
 
 from epic.core.models import Item
 from epic.tags.models import Tagging
@@ -17,3 +20,26 @@ def view_items_for_tag(request, tag_name):
 	for tag in tags:
 		datasets.append(tag.item.specific)
 	return render_to_response('tags/tag_view.html', {'tags':tags, 'tag_name':tag_name, 'datasets':datasets}, context_instance=RequestContext(request))
+
+@login_required
+def delete_tag(request):
+    user = request.user
+    responseData = {}
+    try:
+        tag_name = request.POST['tag_name']
+        dataset = request.POST['dataset_id']
+        try:
+            Tagging.objects.delete_tag(tag_name, dataset)
+            responseData['success'] = 'The tag "%(tag_name)s" was removed.' % {'tag_name': tag_name,}
+        except Tagging.DoesNotExist:
+            responseData['failure'] = 'The tag "%(tag_name)s" on dataset "%(dataset)s" does not exist.' % {'tag_name': tag_name, 'dataset': dataset,}
+
+    except MultiValueDictKeyError:
+        responseData['failure'] = 'No tag name was provided'
+    
+    json = simplejson.dumps(responseData)
+    return HttpResponse(json, mimetype='application/json')
+    
+    
+        
+    
