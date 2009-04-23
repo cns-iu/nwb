1505561ce6c86906696ccbe0d5bf881a2ef088ca
Patrick Phillips <pataphil@indiana.edu>
2009-04-23 19:47:52 +0000

    Changed/abstracted the navigation bar so its actual display template code is in one location, as opposed to being duplicated throughout the site.

--- trunk/epic/core/tests/__init__.py
+++ trunk/epic/core/tests/__init__.py
@@ -4,6 +4,7 @@ from edit_profile_tests import *
 from forgot_password_tests import *
 from logo_tests import *
 from log_in_and_out_tests import *
+from nav_bar_tests import *
 from profile_datarequests_tests import *
 from profile_dataset_tests import *
 from profile_link_tests import *
