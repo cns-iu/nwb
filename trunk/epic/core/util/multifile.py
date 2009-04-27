6ae7faa213e17172084fdb7962083441fd249c91
David Coe <dmcoe@indiana.edu>
2009-04-27 20:52:35 +0000

    * BUGFIX: Adding help text made the attach more files be added int he wrong place
    -> Reviewed by Micah

--- trunk/epic/core/util/multifile.py
+++ trunk/epic/core/util/multifile.py
@@ -69,20 +69,34 @@ class MultiFileInput(FileInput):
         <!--
         %(id)s_counter=%(count)d;
         function new_%(name)s() {
-            b=document.getElementById('%(id)s' + (%(id)s_counter - 1));
-            c=document.createElement("input");
-            c.type="file";
-            c.name="files[]";
-            c.id='%(id)s'+(%(id)s_counter++);
+        
+            // Find the last element in our list of input fields (add new stuff to the end of this)
+            
+            last_input = document.getElementById('%(id)s' + (%(id)s_counter - 1));
+            insertBeforeElement = last_input.parentNode.lastChild.previousSibling;
+           
+            // Insert the actual input field.
+            
+            input_to_add = document.createElement("input");
+            input_to_add.type = "file";
+            input_to_add.name = "files[]";
+            input_to_add.id = '%(id)s'+(%(id)s_counter++);
+            last_input.parentNode.insertBefore(input_to_add, insertBeforeElement);
+            
+            // Insert the "remove" link.
+            
             removeLink = document.createElement("a");
-            removeLink.id='remove_' + c.id;
-            removeLink.setAttribute('onclick', "javascript:remove_%(name)s('" + c.id + "'); return false;");
+            removeLink.id = 'remove_' + input_to_add.id;
+            removeLink.setAttribute('onclick', "javascript:remove_%(name)s('" + input_to_add.id + "'); return false;");
             removeLink.innerHTML = '&nbsp;&nbsp;Remove this file.'; /** TODO: style this instead of using nbsp **/
-            br=document.createElement("br");
-            br.id = 'break_' + c.id;
-            b.parentNode.insertBefore(c,b.parentNode.lastChild);
-            b.parentNode.insertBefore(removeLink, b.parentNode.lastChild);
-            b.parentNode.insertBefore(br,b.parentNode.lastChild);
+            last_input.parentNode.insertBefore(removeLink, insertBeforeElement);
+            
+            // Insert the final line break.
+            
+            br = document.createElement("br");
+            br.id = 'break_' + input_to_add.id;
+            last_input.parentNode.insertBefore(br, insertBeforeElement);
+            
         }
         function remove_%(name)s(id) {
 			removeNode = document.getElementById(id);
