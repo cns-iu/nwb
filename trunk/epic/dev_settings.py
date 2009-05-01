1ae974e1213630aa9a1f00a76e400db4ba3fd627
Russell Duhon <rduhon@indiana.edu>
2009-05-01 17:05:23 +0000

    added database settings to dev_settings

--- trunk/epic/dev_settings.py
+++ trunk/epic/dev_settings.py
@@ -1,1 +1,8 @@
 from settings import *
+
+DATABASE_ENGINE = 'postgresql_psycopg2'
+DATABASE_NAME = 'epic_web'
+DATABASE_USER = 'epic_appuser'
+DATABASE_PASSWORD = 'du3T9Y'
+DATABASE_HOST = 'cns-dbdev.slis.indiana.edu'
+DATABASE_PORT = ''
