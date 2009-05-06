4fafc84a74b7743fe605980b3881081def8836be
Micah Linnemeier <mwlinnem@indiana.edu>
2009-05-06 15:41:44 +0000

    Adding new Google Maps API key for dev server.

--- trunk/epic/dev_settings.py
+++ trunk/epic/dev_settings.py
@@ -7,3 +7,5 @@ DATABASE_PASSWORD = 'du3T9Y'
 DATABASE_HOST = 'cns-dbdev.slis.indiana.edu'
 DATABASE_PORT = ''
 DATABASE_OPTIONS = {'sslmode': 'require'}
+
+GOOGLE_KEY = 'ABQIAAAAXvJKaXXv-aFZ9QFPDtLf2RRYbDcT7AU1sqlHKk7y1uR7IcGNVBRPMeVhk8y3ZgqAMdZmpULqIktKzw'
