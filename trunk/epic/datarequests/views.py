45fd9f74b1bbf09685964964ed1a41a981ba6946
Patrick Phillips <pataphil@indiana.edu>
2009-05-14 16:44:20 +0000

    * Changed the choose-request-fulfilling-Item page to have a more user-friendly message.

--- trunk/epic/datarequests/views.py
+++ trunk/epic/datarequests/views.py
@@ -12,6 +12,7 @@ from epic.datarequests.forms import DataRequestForm
 from epic.datarequests.models import DataRequest
 from epic.tags.models import Tagging
 
+
 def view_datarequests(request):
     datarequests = \
         DataRequest.objects.active().exclude(status='C').order_by('-created_at')
@@ -118,17 +119,14 @@ def cancel_datarequest(request, item_id, slug):
 @login_required
 def fulfill_datarequest(request, item_id, slug, fulfilling_item_id=None):
     user = request.user
-    datarequest = get_object_or_404(DataRequest,pk=item_id)
+    datarequest = get_object_or_404(DataRequest, pk=item_id)
     
+    # Only a request's owner should be able to change it.
     if datarequest.creator != user:
         view_datarequest_url = get_item_url(
             datarequest, 'epic.datarequests.views.view_datarequest')
         
         return HttpResponseRedirect(view_datarequest_url)
-        #only a request's owner should be able to change it
-        return HttpResponseRedirect(reverse('epic.datarequests.views.view_datarequest', 
-                                            kwargs={'item_id':datarequest.id, 
-                                                    'slug':datarequest.slug}))
     elif fulfilling_item_id:
         fulfilling_item = get_object_or_404(Item, pk=fulfilling_item_id)
         datarequest.fulfilling_item = fulfilling_item
@@ -147,8 +145,11 @@ def fulfill_datarequest(request, item_id, slug, fulfilling_item_id=None):
 def choose_fulfilling_item(request, fulfilling_item_id):
     user = request.user
     item = get_object_or_404(Item, pk=fulfilling_item_id)
-    requests = DataRequest.objects.active().filter(status='U', creator=user).order_by('-created_at')
+    requests = DataRequest.objects.active(). \
+        filter(status='U', creator=user).order_by('-created_at')
+    
     return render_to_response('datarequests/choose_fulfilling_item.html',
-                              {'item':item,
-                               'requests':requests},
-                              context_instance=RequestContext(request))
+                              {'item': item,
+                               'specific': item.specific,
+                               'requests': requests},
+                               context_instance=RequestContext(request))
