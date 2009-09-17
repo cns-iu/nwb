f20c0a3642ad5f31c5ddbe744a0871d3457fa1c2
Micah Linnemeier <mwlinnem@indiana.edu>
2009-09-17 19:43:41 +0000

    Committing scipolicy plugin contents, disguised as EpiC stuff. The plan is to work from having the EpiC build making SciPolicy to the EpiC build really making EpiC.

---
+++ trunk/epic_tool/deployment/edu.iu.epic.gui.brand/src/edu/iu/scipolicy/gui/brand/DataManagerView.java
@@ -0,0 +1,11 @@
+package edu.iu.scipolicy.gui.brand;
+
+import org.cishell.reference.gui.datamanager.AbstractDataManagerView;
+
+public class DataManagerView extends AbstractDataManagerView {
+	public DataManagerView() {
+		super("edu.iu.scipolicy.gui.brand");
+		
+//		Activator.getDefault().earlyStartup();
+	}
+}
