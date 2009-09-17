31f2c5f5e59da47578256d4259c22f49382a17b3
Micah Linnemeier <mwlinnem@indiana.edu>
2009-09-17 22:28:00 +0000

    Reverting to undo accidental deletion. SVN saves the day!

---
+++ trunk/scipolicy/deployment/edu.iu.scipolicy.gui.brand.welcometext/src/edu/iu/scipolicy/gui/brand/welcometext/WelcomeTextActivator.java
@@ -0,0 +1,58 @@
+package edu.iu.scipolicy.gui.brand.welcometext;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.log.LogService;
+
+public class WelcomeTextActivator {
+	public static final String PLUGIN_ID =
+		"edu.iu.scipolicy.gui.brand.welcometext";
+	
+	private static WelcomeTextActivator plugin;
+
+	public WelcomeTextActivator() {
+		plugin = this;
+	}
+	
+	protected void activate(ComponentContext componentContext) {
+		if (componentContext != null) {
+        	String greeting = null;
+        	Properties props = new Properties();
+        	BundleContext bundleContext = componentContext.getBundleContext();
+
+        	try {
+        		Bundle bundle = bundleContext.getBundle();
+        		URL pluginPropertiesEntry =
+        			bundle.getEntry("/plugin.properties");
+            	props.load(pluginPropertiesEntry.openStream());                
+        	}
+        	catch (IOException ioException) {
+        		ioException.printStackTrace();
+        	}
+        
+        	greeting = props.getProperty("greeting", null);
+        	
+        	LogService logService =
+        		(LogService)componentContext.locateService("LOG");
+        	
+        	logService.log(LogService.LOG_INFO, greeting);
+		}
+	}
+	
+	protected void deactivate(ComponentContext componentContext) {
+	}
+	
+	public static WelcomeTextActivator getDefault() {
+		return plugin;
+	}
+	
+	public void earlyStartup(){
+	}
+}
