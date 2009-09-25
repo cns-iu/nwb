a2f421f1407d649da99989d035b423a2f6e7bd0f
Joseph Biberstine <jrbibers@indiana.edu>
2009-09-25 17:09:43 +0000

    Refactored.

--- trunk/epic_tool/plugins/converters/edu.iu.epic.converter.spemshellmodel/src/edu/iu/epic/converter/spemshellmodel/SPEMShellValidatorAlgorithm.java
+++ trunk/epic_tool/plugins/converters/edu.iu.epic.converter.spemshellmodel/src/edu/iu/epic/converter/spemshellmodel/SPEMShellValidatorAlgorithm.java
@@ -5,36 +5,45 @@ import java.util.Dictionary;
 
 import org.cishell.framework.CIShellContext;
 import org.cishell.framework.algorithm.Algorithm;
-import org.cishell.framework.algorithm.AlgorithmExecutionException;
+import org.cishell.framework.algorithm.AlgorithmFactory;
 import org.cishell.framework.data.BasicData;
 import org.cishell.framework.data.Data;
 import org.cishell.framework.data.DataProperty;
 
 public class SPEMShellValidatorAlgorithm implements Algorithm {
-    private String inMDLFilePath;
+    public static final String MODEL_FILE_MIME_TYPE = "file:text/mdl";
+	private String inMDLFilePath;
     
-    public SPEMShellValidatorAlgorithm(
-    		Data[] data,
-    		Dictionary<String, Object> parameters,
-    		CIShellContext ciShellContext) {
+	
+	public static class Factory implements AlgorithmFactory {
+		@SuppressWarnings("unchecked") // Raw Dictionary
+		public Algorithm createAlgorithm(
+				Data[] data,
+				Dictionary parameters,
+				CIShellContext ciShellContext) {
+	        return new SPEMShellValidatorAlgorithm(data);
+	    }
+	}
+	
+    public SPEMShellValidatorAlgorithm(Data[] data) {
     	this.inMDLFilePath = (String) data[0].getData();
     }
 
+    
     /* TODO Note that this is currently completely trivial.  No validation
      * of any kind is performed.  This is just for testing SPEMShell integration
      * (we need to be able to load .mdl files into the data manager).
      */
-    public Data[] execute() throws AlgorithmExecutionException {
-    	File inputData = new File(inMDLFilePath);
-
-		return createOutData(inputData);
+    public Data[] execute() {
+		return createOutData(new File(this.inMDLFilePath));
     }
     
-    @SuppressWarnings("unchecked") // TODO
+    @SuppressWarnings("unchecked") // Dictionary conversion
 	private Data[] createOutData(File inputData) {
-		Data[] validationData = new Data[]{ new BasicData(inputData, "file:text/mdl") };
+		Data[] validationData =
+			new Data[]{ new BasicData(inputData, MODEL_FILE_MIME_TYPE) };
 		Dictionary<String, Object> metadata = validationData[0].getMetadata();
-		metadata.put(DataProperty.LABEL, "Model: " + inMDLFilePath);
+		metadata.put(DataProperty.LABEL, "Model: " + this.inMDLFilePath);
 		metadata.put(DataProperty.TYPE, DataProperty.OTHER_TYPE);
 		return validationData;
 	}
