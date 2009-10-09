e34c55a068fd1f3366934ad75396992f4db634b2
Joseph Biberstine <jrbibers@indiana.edu>
2009-10-09 20:51:44 +0000

    Delete journal locater branch.

--- branches/edu.iu.scipolicy.journallocater/src/edu/iu/scipolicy/journallocater/Utils.java
+++
@@ -1,8 +0,0 @@
-package edu.iu.scipolicy.journallocater;
-
-public class Utils {
-	public static String postScriptEscape(String rawReference) {
-		// TODO Auto-generated method stub
-		return rawReference.replace("\\", "\\\\").replace(")","\\)").replace("(", "\\(");
-	}
-}
