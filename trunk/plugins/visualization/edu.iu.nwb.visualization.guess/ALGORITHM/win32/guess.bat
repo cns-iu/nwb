7a0906a43bd7dac9b8254ba0bef9e2fa83640f3c
Joseph Biberstine <jrbibers@indiana.edu>
2009-10-21 19:26:01 +0000

    For consistency, changed the console message reporting the log file path to use the more common Windows path separator (\ rather than /) in the win32 batch.

--- trunk/plugins/visualization/edu.iu.nwb.visualization.guess/ALGORITHM/win32/guess.bat
+++ trunk/plugins/visualization/edu.iu.nwb.visualization.guess/ALGORITHM/win32/guess.bat
@@ -9,7 +9,7 @@ echo Starting GUESS...
 echo 
 echo The initial layout for your visualization is random.  For a clearer visualization, please run a layout from the Layout menu.  (We recommend GEM.)
 echo 
-echo GUESS log file for this session can be found in %GUESS_HOME%/guesslog.txt
+echo GUESS log file for this session can be found in %GUESS_HOME%\guesslog.txt
 
 set GUESS_LIB=%GUESS_HOME%\lib
 set GCLASSPATH=guess.jar;%GUESS_LIB%\guess.jar;%GUESS_LIB%\piccolo.jar;%GUESS_LIB%\piccolox.jar;%GUESS_LIB%\jung.jar;%GUESS_LIB%\commons-collections.jar;%GUESS_LIB%\hsqldb.jar;%GUESS_LIB%\freehep-all.jar;%GUESS_LIB%\colt.jar;%GUESS_LIB%\prefuse.jar;%GUESS_LIB%\TGGraphLayout.jar;%GUESS_LIB%\looks.jar;%GUESS_LIB%\mascoptLib.jar;%GUESS_LIB%\jfreechart.jar;%GUESS_LIB%\jide-components.jar;%GUESS_LIB%\jide-common.jar;%GUESS_LIB%\forms.jar;%GUESS_LIB%\jcommon.jar
