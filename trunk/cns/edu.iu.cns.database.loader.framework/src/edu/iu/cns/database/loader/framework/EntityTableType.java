ca0d0bb437837bd874dda573e11a553d4d26b388
Patrick Phillips <pataphil@indiana.edu>
2009-12-14 23:33:06 +0000

    * Renamed the EntityObjectType typename in EntityTableType to just EntityType. * Renamed EntityTableType.addEntity to EntityTableType.getOrAddEntity.

--- trunk/cns/edu.iu.cns.database.loader.framework/src/edu/iu/cns/database/loader/framework/EntityTableType.java
+++ trunk/cns/edu.iu.cns.database.loader.framework/src/edu/iu/cns/database/loader/framework/EntityTableType.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 import edu.iu.cns.database.loader.framework.utilities.DatabaseTableKeyGenerator;
 
-public class EntityTableType<EntityObjectType extends Entity> {
+public class EntityTableType<EntityType extends Entity> {
 	private DatabaseTableKeyGenerator keyGenerator = new DatabaseTableKeyGenerator();
-	private List<EntityObjectType> entities = new ArrayList<EntityObjectType>();
+	private List<EntityType> entities = new ArrayList<EntityType>();
 	private String entityTypeName;
 	private String entityTableName;
 
@@ -36,8 +36,8 @@ public class EntityTableType<EntityObjectType extends Entity> {
 		return this.entityTableName;
 	}
 
-	public EntityObjectType addEntity(EntityObjectType newEntity) {
-		EntityObjectType duplicateEntity = findDuplicateEntity(newEntity);
+	public EntityType getOrAddEntity(EntityType newEntity) {
+		EntityType duplicateEntity = findDuplicateEntity(newEntity);
 
 		if (duplicateEntity == null) {
 			this.entities.add(newEntity);
@@ -48,8 +48,8 @@ public class EntityTableType<EntityObjectType extends Entity> {
 		}
 	}
 
-	private EntityObjectType findDuplicateEntity(EntityObjectType newEntity) {
-		for (EntityObjectType originalEntity : this.entities) {
+	private EntityType findDuplicateEntity(EntityType newEntity) {
+		for (EntityType originalEntity : this.entities) {
 			if (originalEntity.equals(newEntity)) {
 				return originalEntity;
 			}
