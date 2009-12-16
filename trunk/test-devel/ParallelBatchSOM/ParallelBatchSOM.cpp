3d77d2d9e15fc8a03cb1d8c73a7d0e194c9065b5
Joseph Biberstine <jrbibers@indiana.edu>
2009-12-16 21:08:06 +0000

    More.

--- trunk/test-devel/ParallelBatchSOM/ParallelBatchSOM.cpp
+++ trunk/test-devel/ParallelBatchSOM/ParallelBatchSOM.cpp
@@ -40,8 +40,16 @@ float calculateDistance(vector<float> vector, vector<int> indexToOnes) {
 	for (vector<int>::iterator it = indexToOnes.begin(); it != indexToOnes.end(); it++) {
 		int indexToOne = *it;
 
-		leftSum += (1 - 2 * )
+		// TODO Either correct input to be zero-indexed or use one-indexed here.
+		leftSum += (1 - 2 * vector.at(indexToOne));
 	}
+
+	/* TODO Add in the right summand (either here or by the caller), which is computed per-epoch
+	 * rather than per-timestep.
+	 */
+	float rightSum = 0;
+
+	return leftSum + rightSum;
 }
 
 // TODO int or float?
@@ -70,7 +78,7 @@ void train(Map* map/*TODO ?*/) {
 		float eq5den = 0.0;
 
 		/* TODO Get my piece of the training set.
-		 * How?  Divide up the input file by the number of processed before starting even?
+		 * How?  Divide up the input file (by the number of processes) before starting even?
 		 */
 		for (float trainingVector[];;/* training vector in my training vectors */) {
 			t++;
@@ -83,6 +91,9 @@ void train(Map* map/*TODO ?*/) {
 			Node winningNode = NULL;
 			// TODO Note we use equation 8 here rather than equation 6, to exploit sparseness.
 			for (Node node;;/* in map */) {
+				/* TODO Take note of the comment just after equation 8: we don't need a live
+				 * weight vector here; only needs to be as fresh as the latest epoch.
+				 */
 				float distance =
 						calculateDistance(
 								node->weightVector, trainingVector, map->weightDimension);
