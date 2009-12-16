70798f712184948dbd20c7a31644555599d44428
Joseph Biberstine <jrbibers@indiana.edu>
2009-12-16 17:14:50 +0000

    More.

--- trunk/test-devel/ParallelBatchSOM/ParallelBatchSOM.cpp
+++ trunk/test-devel/ParallelBatchSOM/ParallelBatchSOM.cpp
@@ -6,8 +6,10 @@
  */
 
 #include <iostream>
+#include <vector>
 #include <limits>
 
+using namespace std;
 using std::cerr;
 using std::endl;
 
@@ -15,16 +17,46 @@ const int NUMBER_OF_EPOCHS = 1;
 const int INITIAL_WIDTH = 50;
 const int FINAL_WIDTH = 1;
 
+struct Node {
+	int row;
+	int column;
+	vector<float> weight;
+};
+
 struct Map {
 	int xdim;
 	int ydim;
-/* Map data structure:
- * 	weight vectors (a rectangle of dense, float-valued vectors)
- *
- */
+	int weightDimension;
+	/* TODO A good data structure for a rectangle of Nodes */
 };
 
+/* According to equation 8, exploiting sparseness.
+ * vector is w_k and indexToOnes represents a binary-valued x.
+ */
+float calculateDistance(vector<float> vector, vector<int> indexToOnes) {
+	float leftSum = 0.0;
 
+	// TODO Double-check edge cases.
+	for (vector<int>::iterator it = indexToOnes.begin(); it != indexToOnes.end(); it++) {
+		int indexToOne = *it;
+
+		leftSum += (1 - 2 * )
+	}
+}
+
+// TODO int or float?
+int calculateWidthAtTime(int t, int tFinal) {
+	return (int) interpolate(t, 0, tFinal, INITIAL_WIDTH, FINAL_WIDTH);
+}
+
+float interpolate(float x, float x0, float x1, float y0, float y1) {
+	if (x0 == x1) {
+		cerr << "Can't interpolate over an empty input range." << endl;
+		exit(1);
+	} else {
+		return y0 + (x - x0) * (y1 - y0) / (x1 - x0);
+	}
+}
 
 void train(Map* map/*TODO ?*/) {
 	initializeWeightVectors(weightVectors);
@@ -40,29 +72,31 @@ void train(Map* map/*TODO ?*/) {
 		/* TODO Get my piece of the training set.
 		 * How?  Divide up the input file by the number of processed before starting even?
 		 */
-		for (;;/* training vector in my training vectors */) {
+		for (float trainingVector[];;/* training vector in my training vectors */) {
 			t++;
 
+			/* Long term TODO: In late training (or when convergence can be presumed decent),
+			 * start recording the map (i, j) for the BMU on the previous timestep or two, then
+			 * search only in neighborhoods of that rather than the whole map.
+			 */
 			float shortestDistance = numeric_limits<float>::max();
+			Node winningNode = NULL;
 			// TODO Note we use equation 8 here rather than equation 6, to exploit sparseness.
-			for (;;/* weight vector in my map */) {
+			for (Node node;;/* in map */) {
+				float distance =
+						calculateDistance(
+								node->weightVector, trainingVector, map->weightDimension);
 
+				if (distance < shortestDistance) {
+					shortestDistance = distance;
+					winningNode = node;
+				}
+			}
+
+			if (winningNode == NULL) {
+				cerr << "Horrible error calculating best-matching node." << endl;
+				exit (2);
 			}
 		}
-	}
-}
-
-
-// TODO int or float?
-int calculateWidthAtTime(int t, int tFinal) {
-	return (int) interpolate(t, 0, tFinal, INITIAL_WIDTH, FINAL_WIDTH);
-}
-
-float interpolate(float x, float x0, float x1, float y0, float y1) {
-	if (x0 == x1) {
-		cerr << "Can't interpolate over an empty input range." << endl;
-		exit(1);
-	} else {
-		return y0 + (x - x0) * (y1 - y0) / (x1 - x0);
 	}
 }
