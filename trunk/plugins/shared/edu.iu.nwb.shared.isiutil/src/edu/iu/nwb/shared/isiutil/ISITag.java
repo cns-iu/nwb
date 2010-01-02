4e8664496606f3ec48ee4ae3eeccc18e2b99fa0b
Patrick Phillips <pataphil@indiana.edu>
2010-01-02 19:37:29 +0000

    * Reorganized the constants in ISIDatabase and ISITag to be alphabetical, so sanity-keeping purposes. * Also added a few newly-discovered tags to ISITag.

--- trunk/plugins/shared/edu.iu.nwb.shared.isiutil/src/edu/iu/nwb/shared/isiutil/ISITag.java
+++ trunk/plugins/shared/edu.iu.nwb.shared.isiutil/src/edu/iu/nwb/shared/isiutil/ISITag.java
@@ -10,129 +10,172 @@ public class ISITag {
 	
 	
 	
-	//IMPORTANT: If you add a tag here, make sure to add it in the isiTagArray below as well.
+	/*
+	 * IMPORTANT: If you add a tag here, make sure to add it in the isiTagArray below as well.
+	 * Sorting these alphabetically is a little more sane.
+	 */
 	
-	public static final ISITag FILE_TYPE = new ISITag("FN", "File Type", ContentType.TEXT, true);
-	public static final ISITag VERSION_NUMBER =
-		new ISITag("VR", "Version Number", ContentType.TEXT, true);
-	public static final ISITag END_OF_FILE =
-		new ISITag("EF", "End of File", ContentType.NULL, true);
 	public static final ISITag ABSTRACT = new ISITag("AB", "Abstract", ContentType.TEXT);
 	public static final ISITag ARTICLE_NUMBER_OF_NEW_APS_JOURNALS =
 		new ISITag("AR", "New Article Number",  ContentType.TEXT);
-	public static final ISITag AUTHORS_FULL_NAMES =
-		new ISITag("AF", "Authors (Full Names)", ContentType.MULTI_VALUE_TEXT, "\n");
 	public static final ISITag AUTHORS =
 		new ISITag("AU", "Authors", ContentType.MULTI_VALUE_TEXT, "\n");
+	public static final ISITag AUTHORS_FULL_NAMES =
+		new ISITag("AF", "Authors (Full Names)", ContentType.MULTI_VALUE_TEXT, "\n");
+
 	public static final ISITag BEGINNING_PAGE =
 		new ISITag("BP", "Beginning Page", ContentType.TEXT);
-	public static final ISITag RESEARCH_ADDRESSES =
-		new ISITag("C1", "Research Addresses", ContentType.MULTI_VALUE_TEXT, "\n");
-	public static final ISITag CITED_PATENT = new ISITag("CP", "Cited Patent", ContentType.TEXT);
-	public static final ISITag CITED_REFERENCES =
-		new ISITag("CR", "Cited References", ContentType.MULTI_VALUE_TEXT, "\n");
-	public static final ISITag ORIGINAL_KEYWORDS =
-		new ISITag("DE", "Original Keywords", ContentType.MULTI_VALUE_TEXT,  ";");
-	public static final ISITag DOI = new ISITag("DI", "DOI", ContentType.TEXT);
-	public static final ISITag DOCUMENT_TYPE = new ISITag("DT", "Document Type", ContentType.TEXT);
-	public static final ISITag EMAIL_PRIMARY_AUTHOR =
-		new ISITag("EM", "E-mail Address", ContentType.TEXT);
-	public static final ISITag ENDING_PAGE = new ISITag("EP", "Ending Page", ContentType.TEXT);
-	public static final ISITag END_OF_RECORD =
-		new ISITag("ER", "End of RecordtagName", ContentType.NULL);
-	public static final ISITag ISI_DOCUMENT_DELIVERY_NUMBER =
-		new ISITag("GA", "ISI Document Delivery Number", ContentType.TEXT);
-	public static final ISITag NEW_KEYWORDS_GIVEN_BY_ISI =
-		new ISITag("ID", "New ISI Keywords", ContentType.MULTI_VALUE_TEXT,  ";");
-	public static final ISITag ISSUE = new ISITag("IS", "Issue", ContentType.TEXT);
-	public static final ISITag TWENTY_NINE_CHAR_JOURNAL_ABBREVIATION =
-		new ISITag("J9", "Journal Name (Abbreviated)", ContentType.TEXT);
-	public static final ISITag ISO_JOURNAL_TITLE_ABBREVIATION =
-		new ISITag("JI", "Journal Name (Abbreviated ISO)", ContentType.TEXT);
-	public static final ISITag LANGUAGE = new ISITag("LA", "Language", ContentType.TEXT);
-	public static final ISITag CITED_REFERENCE_COUNT =
-		new ISITag("NR", "Cited Reference Count", ContentType.INTEGER);
-	public static final ISITag PUBLICATION_DATE =
-		new ISITag("PD", "Publication Date", ContentType.TEXT);
-	public static final ISITag NUMBER_OF_PAGES =
-		new ISITag("PG", "Number of Pages", ContentType.INTEGER);
-	public static final ISITag PUBLISHER_CITY =
-		new ISITag("PI", "City of Publisher", ContentType.TEXT);
-	public static final ISITag PART_NUMBER = new ISITag("PN", "Part Number", ContentType.TEXT);
-	public static final ISITag PUBLICATION_TYPE =
-		new ISITag("PT", "Publication Type", ContentType.TEXT);
-	public static final ISITag PUBLISHER = new ISITag("PU", "Publisher", ContentType.TEXT);
-	public static final ISITag PUBLISHER_ADDRESS =
-		new ISITag("PA", "Publisher Address", ContentType.TEXT);
-	public static final ISITag PUBLICATION_YEAR =
-		new ISITag("PY", "Publication Year", ContentType.INTEGER);
-	public static final ISITag REPRINT_ADDRESS =
-		new ISITag("RP", "Reprint Address", ContentType.TEXT);
 	public static final ISITag BOOK_SERIES_TITLE =
 		new ISITag("SE", "Book Series Title", ContentType.TEXT);
 	public static final ISITag BOOK_SERIES_SUBTITLE =
 		new ISITag("BS", "Book Series Subtitle", ContentType.TEXT);
-	public static final ISITag SPECIAL_ISSUE =
-		new ISITag("SI", "Special Issue",  ContentType.TEXT);
-	public static final ISITag ISBN = new ISITag("BN", "ISBN", ContentType.TEXT);
-	public static final ISITag ISSN = new ISITag("SN",  "ISSN", ContentType.TEXT);
+
+	public static final ISITag CITED_PATENT = new ISITag("CP", "Cited Patent", ContentType.TEXT);
+	public static final ISITag CITED_REFERENCE_COUNT =
+		new ISITag("NR", "Cited Reference Count", ContentType.INTEGER);
+	public static final ISITag CITED_REFERENCES =
+		new ISITag("CR", "Cited References", ContentType.MULTI_VALUE_TEXT, "\n");
+
+	public static final ISITag DOCUMENT_TYPE = new ISITag("DT", "Document Type", ContentType.TEXT);
+	public static final ISITag DOI = new ISITag("DI", "DOI", ContentType.TEXT);
+
+	public static final ISITag EDITORS =
+		new ISITag("ED", "Editors", ContentType.MULTI_VALUE_TEXT, "\n");
+	public static final ISITag EMAIL_ADDRESSES =
+		new ISITag("EM", "E-mail Addresses", ContentType.MULTI_VALUE_TEXT, "\n");
+	public static final ISITag END_OF_FILE =
+		new ISITag("EF", "End of File", ContentType.NULL, true);
+	public static final ISITag ENDING_PAGE = new ISITag("EP", "Ending Page", ContentType.TEXT);
+	public static final ISITag END_OF_RECORD =
+		new ISITag("ER", "End of RecordtagName", ContentType.NULL);
+
+	public static final ISITag FILE_TYPE = new ISITag("FN", "File Type", ContentType.TEXT, true);
 	public static final ISITag FULL_JOURNAL_TITLE =
 		new ISITag("SO", "Journal Title (Full)",ContentType.TEXT);
-	public static final ISITag SUPPLEMENT = new ISITag("SU", "Supplement", ContentType.TEXT);
-	public static final ISITag TIMES_CITED = new ISITag("TC", "Times Cited", ContentType.INTEGER);
-	public static final ISITag TITLE = new ISITag("TI", "Title", ContentType.TEXT);
-	public static final ISITag UNIQUE_ID = new ISITag("UT", "Unique ID", ContentType.TEXT);
-	public static final ISITag VOLUME = new ISITag("VL", "Volume", ContentType.TEXT);
+	public static final ISITag FUNDING_AGENCY_AND_GRANT_NUMBER =
+		new ISITag("FU", "Funding Agency and Grant Number", ContentType.TEXT);
+	public static final ISITag FUNDING_TEXT = new ISITag("FX", "Funding Text", ContentType.TEXT);
+
+	public static final ISITag ISBN = new ISITag("BN", "ISBN", ContentType.TEXT);
+	public static final ISITag ISSN = new ISITag("SN",  "ISSN", ContentType.TEXT);
+	public static final ISITag ISI_DOCUMENT_DELIVERY_NUMBER =
+		new ISITag("GA", "ISI Document Delivery Number", ContentType.TEXT);
+	public static final ISITag ISO_JOURNAL_TITLE_ABBREVIATION =
+		new ISITag("JI", "Journal Name (Abbreviated ISO)", ContentType.TEXT);
+	public static final ISITag ISSUE = new ISITag("IS", "Issue", ContentType.TEXT);
+
+	public static final ISITag LANGUAGE = new ISITag("LA", "Language", ContentType.TEXT);
+
+	public static final ISITag NEW_KEYWORDS_GIVEN_BY_ISI =
+		new ISITag("ID", "New ISI Keywords", ContentType.MULTI_VALUE_TEXT, ";");
+	public static final ISITag NUMBER_OF_PAGES =
+		new ISITag("PG", "Number of Pages", ContentType.INTEGER);
+
+	public static final ISITag ORIGINAL_KEYWORDS =
+		new ISITag("DE", "Original Keywords", ContentType.MULTI_VALUE_TEXT, ";");
+
+	public static final ISITag PART_NUMBER = new ISITag("PN", "Part Number", ContentType.TEXT);
+	public static final ISITag PUBLICATION_DATE =
+		new ISITag("PD", "Publication Date", ContentType.TEXT);
+	public static final ISITag PUBLICATION_TYPE =
+		new ISITag("PT", "Publication Type", ContentType.TEXT);
+	public static final ISITag PUBLICATION_YEAR =
+		new ISITag("PY", "Publication Year", ContentType.INTEGER);
+	public static final ISITag PUBLISHER = new ISITag("PU", "Publisher", ContentType.TEXT);
+	public static final ISITag PUBLISHER_ADDRESS =
+		new ISITag("PA", "Publisher Address", ContentType.TEXT);
+	public static final ISITag PUBLISHER_CITY =
+		new ISITag("PI", "City of Publisher", ContentType.TEXT);
 	public static final ISITag PUBLISHER_WEB_ADDRESS =
 		new ISITag("WP", "Publisher Web Address", ContentType.TEXT);
-		
+
+	public static final ISITag REPRINT_ADDRESS =
+		new ISITag("RP", "Reprint Address", ContentType.TEXT);
+	public static final ISITag RESEARCH_ADDRESSES =
+		new ISITag("C1", "Research Addresses", ContentType.MULTI_VALUE_TEXT, "\n");
+
+	public static final ISITag SPECIAL_ISSUE =
+		new ISITag("SI", "Special Issue",  ContentType.TEXT);
+	public static final ISITag SUBJECT_CATEGORY =
+		new ISITag("SC", "Subject Category", ContentType.TEXT);
+	public static final ISITag SUPPLEMENT = new ISITag("SU", "Supplement", ContentType.TEXT);
+
+	public static final ISITag TIMES_CITED = new ISITag("TC", "Times Cited", ContentType.INTEGER);
+	public static final ISITag TITLE = new ISITag("TI", "Title", ContentType.TEXT);
+	public static final ISITag TWENTY_NINE_CHAR_JOURNAL_ABBREVIATION =
+		new ISITag("J9", "Journal Name (Abbreviated)", ContentType.TEXT);
+
+	public static final ISITag UNIQUE_ID = new ISITag("UT", "Unique ID", ContentType.TEXT);
+
+	public static final ISITag VERSION_NUMBER =
+		new ISITag("VR", "Version Number", ContentType.TEXT, true);
+	public static final ISITag VOLUME = new ISITag("VL", "Volume", ContentType.TEXT);		
 	
 	private static final ISITag[] isiTagArray = {
-		FILE_TYPE,
-		VERSION_NUMBER,
-		END_OF_FILE,
 		ABSTRACT,
 		ARTICLE_NUMBER_OF_NEW_APS_JOURNALS,
 		AUTHORS,
+		AUTHORS_FULL_NAMES,
+
 		BEGINNING_PAGE,
-		RESEARCH_ADDRESSES,
-		CITED_PATENT,
-		CITED_REFERENCES,
-		ORIGINAL_KEYWORDS,
-		DOI,
-		DOCUMENT_TYPE,
-		ENDING_PAGE, 
-		END_OF_RECORD,
-		ISI_DOCUMENT_DELIVERY_NUMBER, 
-		NEW_KEYWORDS_GIVEN_BY_ISI,
-		ISSUE,
-		TWENTY_NINE_CHAR_JOURNAL_ABBREVIATION,
-		ISO_JOURNAL_TITLE_ABBREVIATION,
-		LANGUAGE,
-		CITED_REFERENCE_COUNT,
-		PUBLICATION_DATE,
-		NUMBER_OF_PAGES,
-		PUBLISHER_CITY,
-		PART_NUMBER,
-		PUBLICATION_TYPE,
-		PUBLISHER,
-		PUBLISHER_ADDRESS,
-		PUBLICATION_YEAR,
-		REPRINT_ADDRESS,
 		BOOK_SERIES_TITLE,
 		BOOK_SERIES_SUBTITLE,
-		SPECIAL_ISSUE,
+
+		CITED_PATENT,
+		CITED_REFERENCE_COUNT,
+		CITED_REFERENCES,
+
+		DOCUMENT_TYPE,
+		DOI,
+
+		EDITORS,
+		EMAIL_ADDRESSES,
+		END_OF_FILE,
+		ENDING_PAGE, 
+		END_OF_RECORD,
+
+		FILE_TYPE,
+		FULL_JOURNAL_TITLE,
+		FUNDING_AGENCY_AND_GRANT_NUMBER,
+		FUNDING_TEXT,
+
 		ISBN,
 		ISSN,
-		FULL_JOURNAL_TITLE,
+		ISI_DOCUMENT_DELIVERY_NUMBER, 
+		ISO_JOURNAL_TITLE_ABBREVIATION,
+		ISSUE,
+
+		LANGUAGE,
+
+		NEW_KEYWORDS_GIVEN_BY_ISI,
+		NUMBER_OF_PAGES,
+
+		ORIGINAL_KEYWORDS,
+
+		PART_NUMBER,
+		PUBLICATION_DATE,
+		PUBLICATION_TYPE,
+		PUBLICATION_YEAR,
+		PUBLISHER,
+		PUBLISHER_ADDRESS,
+		PUBLISHER_CITY,
+		PUBLISHER_WEB_ADDRESS,
+
+		REPRINT_ADDRESS,
+		RESEARCH_ADDRESSES,
+
+		SPECIAL_ISSUE,
+		SUBJECT_CATEGORY,
 		SUPPLEMENT,
+
 		TIMES_CITED,
 		TITLE,
+		TWENTY_NINE_CHAR_JOURNAL_ABBREVIATION,
+
 		UNIQUE_ID,
+
+		VERSION_NUMBER,
 		VOLUME, 
-		PUBLISHER_WEB_ADDRESS,
-		AUTHORS_FULL_NAMES,
-		EMAIL_PRIMARY_AUTHOR
 	};
 	
 	private static List isiTags = new ArrayList();
---
+++ trunk/plugins/shared/edu.iu.nwb.shared.isiutil/src/edu/iu/nwb/shared/isiutil/exception/ISILoadingException.java
@@ -0,0 +1,21 @@
+package edu.iu.nwb.shared.isiutil.exception;
+
+public class ISILoadingException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public ISILoadingException() {
+		super();
+	}
+
+	public ISILoadingException(String arg0) {
+		super(arg0);
+	}
+
+	public ISILoadingException(Throwable arg0) {
+		super(arg0);
+	}
+
+	public ISILoadingException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+}
