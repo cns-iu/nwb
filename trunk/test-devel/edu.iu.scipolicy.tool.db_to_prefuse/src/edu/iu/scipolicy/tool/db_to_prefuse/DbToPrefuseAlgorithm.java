70e6e50001ac1372aae17097ff310fac62a2d05b
Micah Linnemeier <mwlinnem@indiana.edu>
2010-01-04 17:31:03 +0000

    Various cosmetic changes. Reviewed by Patrick.

--- trunk/test-devel/edu.iu.scipolicy.tool.db_to_prefuse/src/edu/iu/scipolicy/tool/db_to_prefuse/DbToPrefuseAlgorithm.java
+++ trunk/test-devel/edu.iu.scipolicy.tool.db_to_prefuse/src/edu/iu/scipolicy/tool/db_to_prefuse/DbToPrefuseAlgorithm.java
@@ -23,103 +23,91 @@ import prefuse.data.io.sql.ConnectionFactory;
 import prefuse.data.io.sql.DatabaseDataSource;
 
 public class DbToPrefuseAlgorithm implements Algorithm {
-	
+
 	private Data inputData;
 
 	private LogService log;
 
 	public DbToPrefuseAlgorithm(Data[] data, Dictionary parameters, CIShellContext context) {
-		this.inputData =  data[0];
-
+		this.inputData = data[0];
 		this.log = (LogService) context.getService(LogService.class.getName());
 	}
 
 	public Data[] execute() throws AlgorithmExecutionException {
 		try {
-		Database db = (Database) inputData.getData();
-		Connection dbConnection = db.getConnection();
-		
-		List<Data> outputTables = new ArrayList<Data>();
-		List<String> tableNames = getTableNames(dbConnection);
-		for (String tableName : tableNames) {
-			Table prefuseTable = extractTable(tableName, dbConnection);	 
-			
-			Data prefuseTableData = wrapAndAnnotateTable(tableName, prefuseTable);
-			outputTables.add(prefuseTableData);
-		}
+			Database db = (Database) inputData.getData();
+			String applicationSchemaName = db.getApplicationSchemaName();
+			Connection dbConnection = db.getConnection();
 
-	   
-		
-	    return (Data[]) outputTables.toArray(new Data[outputTables.size()]);
+			List<Data> outputTables = new ArrayList<Data>();
+			// for each table in the database...
+			List<String> tableNames = getTableNames(dbConnection, applicationSchemaName);
+			for (String tableName : tableNames) {
+				// create a prefuse table from the database table
+				Table prefuseTable = extractTable(tableName, dbConnection);
+				Data prefuseTableData = wrapAndAnnotateTable(tableName, prefuseTable);
+				outputTables.add(prefuseTableData);
+			}
+
+			// return all the prefuse tables we created
+			return (Data[]) outputTables.toArray(new Data[outputTables.size()]);
 		} catch (Exception e) {
 			throw new AlgorithmExecutionException(e);
 		}
 	}
-	
-	private Table extractTable(String tableName, Connection dbConnection) 
-		throws AlgorithmExecutionException {
+
+	private Table extractTable(String tableName, Connection dbConnection)
+			throws AlgorithmExecutionException {
 		try {
-			DatabaseDataSource prefuseDataExtractor = 
-				ConnectionFactory.getDatabaseConnection(dbConnection);
-			
-		    String extractBasicAwardTableSql = "SELECT * FROM " + tableName;
-		    
-		    Table table = 
-		    	prefuseDataExtractor.getData(extractBasicAwardTableSql);
-		
-		    return table;
-		
+			DatabaseDataSource prefuseDataExtractor = ConnectionFactory
+					.getDatabaseConnection(dbConnection);
+
+			String extractBasicAwardTableSQL = "SELECT * FROM " + tableName;
+
+			Table table = prefuseDataExtractor.getData(extractBasicAwardTableSQL);
+
+			return table;
+
 		} catch (SQLException e) {
-			String message =
-				"An unexpected error occurred while extracting data from the nsf database." + 
-				"Aborting extraction algorithm.";
+			String message = "An unexpected error occurred while extracting data from the database."
+					+ "Aborting extraction algorithm.";
+			throw new AlgorithmExecutionException(message, e);
+		} catch (DataIOException e) {
+			String message = "An unexpected error occurred while extracting data from the database."
+					+ "Aborting extraction algorithm.";
 			throw new AlgorithmExecutionException(message, e);
 		}
-		 catch (DataIOException e) {
-			 String message =
-				"An unexpected error occurred while extracting data from the nsf database." + 
-				"Aborting extraction algorithm.";
-				throw new AlgorithmExecutionException(message, e);
-		 } 
 	}
 
 	private Data wrapAndAnnotateTable(String name, Table table) {
 		Data tableData = new BasicData(table, Table.class.getName());
-			
-		Dictionary awardTableMetadata = tableData.getMetadata();
-		awardTableMetadata.put(DataProperty.LABEL, "Database Table: " + name);
-		awardTableMetadata.put(DataProperty.PARENT, this.inputData);
-		awardTableMetadata.put(DataProperty.TYPE, DataProperty.TABLE_TYPE);
-			
+
+		Dictionary tableMetadata = tableData.getMetadata();
+		tableMetadata.put(DataProperty.LABEL, "Database Table: " + name);
+		tableMetadata.put(DataProperty.PARENT, this.inputData);
+		tableMetadata.put(DataProperty.TYPE, DataProperty.TABLE_TYPE);
+
 		return tableData;
 	}
-	
-	
-	private static final int SCHEMA_NAME_INDEX = 2;
-	private static final int TABLE_NAME_INDEX = 3;
 
-	 private static final String NONSYSTEM_SCHEMA_NAME = "APP";
-	 
-	 private static List<String> getTableNames(Connection dbConnection) throws SQLException {
-		   DatabaseMetaData dbMetadata = dbConnection.getMetaData();
-		   ResultSet allTableNames = dbMetadata.getTables(null, "APP", null, null);
-		   
-		   List<String> nonSystemTableNames = new ArrayList<String>();
-		   while (allTableNames.next()) {
-			   String schemaName = allTableNames.getString(SCHEMA_NAME_INDEX);
-			   if (isNonSystemSchemaName(schemaName)) {
-				   String tableName = allTableNames.getString(TABLE_NAME_INDEX);
-				   nonSystemTableNames.add(tableName);
-			   }
-		   }
-		   
-		   return nonSystemTableNames;
+	private static List<String> getTableNames(Connection dbConnection, String applicationSchemaName)
+			throws SQLException {
+		DatabaseMetaData dbMetadata = dbConnection.getMetaData();
+		ResultSet allTableNames = dbMetadata.getTables(null, applicationSchemaName, null, null);
+
+		List<String> nonSystemTableNames = new ArrayList<String>();
+		while (allTableNames.next()) {
+			String schemaName = allTableNames.getString("TABLE_SCHEM");
+			if (isNonSystemSchemaName(schemaName, applicationSchemaName)) {
+				String tableName = allTableNames.getString("TABLE_NAME");
+				nonSystemTableNames.add(tableName);
+			}
+		}
+
+		return nonSystemTableNames;
 	}
-  
-  
 
-	
-	private static boolean isNonSystemSchemaName(String tableName) {
-		return tableName.indexOf(NONSYSTEM_SCHEMA_NAME) != -1;
+	private static boolean isNonSystemSchemaName(String tableName, String nonSystemSchemaName) {
+		return tableName.indexOf(nonSystemSchemaName) != -1;
 	}
 }
--- trunk/test-devel/edu.iu.scipolicy.tool.db_to_prefuse/src/edu/iu/scipolicy/tool/db_to_prefuse/DbToPrefuseAlgorithmFactory.java
+++ trunk/test-devel/edu.iu.scipolicy.tool.db_to_prefuse/src/edu/iu/scipolicy/tool/db_to_prefuse/DbToPrefuseAlgorithmFactory.java
@@ -7,13 +7,8 @@ import org.cishell.framework.algorithm.Algorithm;
 import org.cishell.framework.algorithm.AlgorithmFactory;
 import org.cishell.framework.data.Data;
 
-/*
- * TODO: This algorithm probably shouldn't be called db to prefuse,
- *  since there can (and will) be more than one type of extraction 
- *  from an nsf db to prefuse table.
- */
 public class DbToPrefuseAlgorithmFactory implements AlgorithmFactory {
-    public Algorithm createAlgorithm(Data[] data, Dictionary parameters, CIShellContext context) {
-        return new DbToPrefuseAlgorithm(data, parameters, context);
-    }
+	public Algorithm createAlgorithm(Data[] data, Dictionary parameters, CIShellContext context) {
+		return new DbToPrefuseAlgorithm(data, parameters, context);
+	}
 }
