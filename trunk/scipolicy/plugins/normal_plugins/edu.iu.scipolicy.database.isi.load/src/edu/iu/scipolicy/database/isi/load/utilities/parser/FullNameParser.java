eb5ae294f4f9a7b5e1cc19072ba285aa83ec0e9b
Patrick Phillips <pataphil@indiana.edu>
2010-02-04 17:05:58 +0000

    * Fixed a name parsing issue.

--- trunk/scipolicy/plugins/normal_plugins/edu.iu.scipolicy.database.isi.load/src/edu/iu/scipolicy/database/isi/load/utilities/parser/FullNameParser.java
+++ trunk/scipolicy/plugins/normal_plugins/edu.iu.scipolicy.database.isi.load/src/edu/iu/scipolicy/database/isi/load/utilities/parser/FullNameParser.java
@@ -11,27 +11,33 @@ public class FullNameParser {
 
 	public FullNameParser(String rawFullNameString) throws PersonParsingException {
 		if (!StringUtilities.isNull_Empty_OrWhitespace(rawFullNameString)) {
+			StringBuilder additionalNamesBuilder = new StringBuilder();
+			StringBuilder fullNameBuilder = new StringBuilder();
+
 			String fullNameWithoutComma = rawFullNameString.replaceFirst(",", "");
 			String[] fullNameTokens = fullNameWithoutComma.split("\\s");
 			String fullNameFamilyName = StringUtilities.simpleClean(fullNameTokens[0]);
-			this.fullName = fullNameFamilyName;
+			fullNameBuilder.append(fullNameFamilyName);
 
 			if (fullNameTokens.length >= 2) {
 				this.personalName = StringUtilities.simpleClean(fullNameTokens[1]);
-				this.fullName += ", " + this.personalName;
+				fullNameBuilder.append(", " + this.personalName);
 
 				if (fullNameTokens.length >= 3) {
 					for (int ii = 2; ii < fullNameTokens.length; ii++) {
-						this.additionalNamesString += fullNameTokens[ii];
+						additionalNamesBuilder.append(fullNameTokens[ii]);
 
 						if ((ii + 1) < fullNameTokens.length) {
-							this.additionalNamesString += " ";
+							additionalNamesBuilder.append(" ");
 						}
 					}
 
-					this.fullName += " " + this.additionalNamesString;
+					fullNameBuilder.append(" " + additionalNamesBuilder);
 				}
 			}
+
+			this.additionalNamesString = additionalNamesBuilder.toString();
+			this.fullName = fullNameBuilder.toString();
 		}
 	}
 }
