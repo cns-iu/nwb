b4d700733b29069cc56bd5285159016a21e7c4b1
Russell Duhon <rduhon@indiana.edu>
2010-03-25 22:45:38 +0000

    New Comparison classes used by modified journal merging. Reviewed by Joseph.

---
+++ trunk/cns/edu.iu.cns.database.merge.generic/src/edu/iu/cns/database/merge/generic/maker/AlphabeticalColumn.java
@@ -0,0 +1,23 @@
+package edu.iu.cns.database.merge.generic.maker;
+
+import java.util.Comparator;
+
+import org.cishell.utilities.StringUtilities;
+
+import prefuse.data.Tuple;
+
+public class AlphabeticalColumn implements Comparator<Tuple> {
+	
+	private String column;
+
+	public AlphabeticalColumn(String column) {
+		this.column = column;
+	}
+	
+	public int compare(Tuple o1, Tuple o2) {
+		String v1 = StringUtilities.emptyStringIfNull(o1.get(column));
+		String v2 = StringUtilities.emptyStringIfNull(o2.get(column));
+		return String.CASE_INSENSITIVE_ORDER.compare(v1, v2);
+	}
+
+}
