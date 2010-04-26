2e18f8a37fde42c979120f4ae2803f15bd81e1f1
Russell Duhon <rduhon@indiana.edu>
2010-04-26 17:58:15 +0000

    much simpler media rearranging

--- trunk/epic/media_move.sh
+++ trunk/epic/media_move.sh
@@ -1,38 +1,21 @@
 #!/bin/bash
 
 
-if [ ! -a "/home/epic_website/epic_media/core" ]; then
-	mkdir /home/epic_website/epic_media/core
+if [ -a "/home/epic_website/epic_media/core" ]; then
+	rm -rf /home/epic_website/epic_media/core
 fi
-if [ ! -a "/home/epic_website/epic_media/projects" ]; then
-	mkdir /home/epic_website/epic_media/projects
+if [ -a "/home/epic_website/epic_media/projects" ]; then
+	rm -rf /home/epic_website/epic_media/projects
 fi
-if [ ! -a "/home/epic_website/epic_media/search" ]; then
-	mkdir /home/epic_website/epic_media/search
+if [ -a "/home/epic_website/epic_media/search" ]; then
+	rm -rf /home/epic_website/epic_media/search
 fi
-if [ ! -a "/home/epic_website/epic_media/tags" ]; then
-	mkdir /home/epic_website/epic_media/tags
+if [ -a "/home/epic_website/epic_media/tags" ]; then
+	rm -rf /home/epic_website/epic_media/tags
 fi
 
 
-for f in /home/epic_website/epic_code/epic/core/media/*
-do
-	mv "$f" /home/epic_website/epic_media/core/
-done
-
-for f in /home/epic_website/epic_code/epic/projects/media/*
-do
-	mv "$f" /home/epic_website/epic_media/projects/
-done
-
-for f in /home/epic_website/epic_code/epic/search/media/*
-do
-	mv "$f" /home/epic_website/epic_media/search/
-done
-
-for f in /home/epic_website/epic_code/epic/tags/media/*
-do
-	mv "$f" /home/epic_website/epic_media/tags/
-done
-
-
+mv /home/epic_website/epic_code/epic/core/media /home/epic_website/epic_media/core
+mv /home/epic_website/epic_code/epic/projects/media /home/epic_website/epic_media/projects
+mv /home/epic_website/epic_code/epic/search/media /home/epic_website/epic_media/search
+mv /home/epic_website/epic_code/epic/tags/media /home/epic_website/epic_media/tags
