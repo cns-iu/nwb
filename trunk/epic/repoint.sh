76b26f8bb655634089fa20f45f483c6e83808674
Russell Duhon <rduhon@indiana.edu>
2010-04-27 20:03:36 +0000

    still needs --pythonpath, making parent directory

--- trunk/epic/repoint.sh
+++ trunk/epic/repoint.sh
@@ -12,8 +12,8 @@ echo "cd epic" >> remote.sh
 echo "chmod +x media_move.sh" >> remote.sh
 echo "./media_move.sh" >> remote.sh
 echo "touch /home/epic_website/epic.wsgi" >> remote.sh
-echo "python2.6 manage.py shell --settings=epic.dev_settings --pythonpath=. < wipeout.py" >> remote.sh
-echo "python2.6 manage.py syncdb --noinput --settings=epic.dev_settings --pythonpath=." >> remote.sh
+echo "python2.6 manage.py shell --settings=epic.dev_settings --pythonpath=.. < wipeout.py" >> remote.sh
+echo "python2.6 manage.py syncdb --noinput --settings=epic.dev_settings --pythonpath=.." >> remote.sh
 #echo "python2.6 /home/epic_website/epic_code/epic/manage.py loaddata /home/epic_website/epic_code/epic/inital_data.json --settings=epic.dev_settings --pythonpath=/home/epic_website/epic_code" >> remote.sh
 
 
