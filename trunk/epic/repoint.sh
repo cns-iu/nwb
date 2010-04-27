b0759ce9d9a31607f9bc84fc8c1002a384893654
Russell Duhon <rduhon@indiana.edu>
2010-04-27 13:45:30 +0000

    trying svn export to ditch .svn directories; not sure if compatible with how hudson checks out

--- trunk/epic/repoint.sh
+++ trunk/epic/repoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-scp -r epic cns-epic-dev:/home/epic_website/epic_code/epic-$SVN_REVISION-$BUILD_NUMBER
+svn export epic epic2
+scp -r epic2 cns-epic-dev:/home/epic_website/epic_code/epic-$SVN_REVISION-$BUILD_NUMBER
 
 echo 'if [ -a "/home/epic_website/epic_code/epic" ]; then' > remote.sh
 echo "	CURRENT_LOC=`readlink -n /home/epic_website/epic_code/epic`" >> remote.sh
