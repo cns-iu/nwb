a8daef40641a8a608578ba0caa0089ba5b00a3c9
Russell Duhon <rduhon@indiana.edu>
2010-04-27 19:59:38 +0000

    try simplifying script paths & get initial_data into working directory

--- trunk/epic/repoint.sh
+++ trunk/epic/repoint.sh
@@ -2,17 +2,19 @@
 svn export epic epic2
 scp -r epic2 cns-epic-dev:/home/epic_website/epic_code/epic-$SVN_REVISION-$BUILD_NUMBER
 
-echo 'if [ -a "/home/epic_website/epic_code/epic" ]; then' > remote.sh
-echo "	CURRENT_LOC=`readlink -n /home/epic_website/epic_code/epic`" >> remote.sh
+echo "cd /home/epic_website/epic_code" > remote.sh
+echo 'if [ -a "epic" ]; then' >> remote.sh
+echo "	CURRENT_LOC=`readlink -n epic`" >> remote.sh
 echo "fi" >> remote.sh
 
-echo "ln -s -f -n /home/epic_website/epic_code/epic-$SVN_REVISION-$BUILD_NUMBER /home/epic_website/epic_code/epic" >> remote.sh
-echo "chmod +x /home/epic_website/epic_code/epic/media_move.sh" >> remote.sh
-echo "/home/epic_website/epic_code/epic/media_move.sh" >> remote.sh
+echo "ln -s -f -n /home/epic_website/epic_code/epic-$SVN_REVISION-$BUILD_NUMBER epic" >> remote.sh
+echo "cd epic" >> remote.sh
+echo "chmod +x media_move.sh" >> remote.sh
+echo "./media_move.sh" >> remote.sh
 echo "touch /home/epic_website/epic.wsgi" >> remote.sh
-echo "python2.6 /home/epic_website/epic_code/epic/manage.py shell --settings=epic.dev_settings --pythonpath=/home/epic_website/epic_code < /home/epic_website/epic_code/epic/wipeout.py" >> remote.sh
-echo "python2.6 /home/epic_website/epic_code/epic/manage.py syncdb --noinput --settings=epic.dev_settings --pythonpath=/home/epic_website/epic_code" >> remote.sh
-echo "python2.6 /home/epic_website/epic_code/epic/manage.py loaddata /home/epic_website/epic_code/epic/inital_data.json --settings=epic.dev_settings --pythonpath=/home/epic_website/epic_code" >> remote.sh
+echo "python2.6 manage.py shell --settings=epic.dev_settings < wipeout.py" >> remote.sh
+echo "python2.6 manage.py syncdb --noinput --settings=epic.dev_settings" >> remote.sh
+#echo "python2.6 /home/epic_website/epic_code/epic/manage.py loaddata /home/epic_website/epic_code/epic/inital_data.json --settings=epic.dev_settings --pythonpath=/home/epic_website/epic_code" >> remote.sh
 
 
 echo 'if [ -n "$CURRENT_LOC" ]; then' >> remote.sh
