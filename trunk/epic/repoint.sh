5196c04334cde9e1ad49eca61b5d4a2faad17ec5
Russell Duhon <rduhon@indiana.edu>
2010-05-10 18:22:34 +0000

    pythonpath manipulation

--- trunk/epic/repoint.sh
+++ trunk/epic/repoint.sh
@@ -12,9 +12,9 @@ echo "cd epic" >> remote.sh
 echo "chmod +x media_move.sh" >> remote.sh
 echo "./media_move.sh" >> remote.sh
 echo "touch /home/epic_website/epic.wsgi" >> remote.sh
-echo "python2.6 manage.py shell --settings=dev_settings  < wipeout.py" >> remote.sh
-echo "python2.6 manage.py syncdb --noinput --settings=dev_settings " >> remote.sh
-echo "python2.6 manage.py loaddata demo_data --settings=dev_settings " >> remote.sh
+echo "python2.6 manage.py shell --settings=dev_settings --pythonpath=../.. < wipeout.py" >> remote.sh
+echo "python2.6 manage.py syncdb --noinput --settings=dev_settings --pythonpath=../.." >> remote.sh
+echo "python2.6 manage.py loaddata demo_data --settings=dev_settings --pythonpath=../.." >> remote.sh
 
 
 echo 'if [ -n "$CURRENT_LOC" ]; then' >> remote.sh
