1fcf34685dc93e4bdfbbae13a62f5c8bc0f6a682
Russell Duhon <rduhon@indiana.edu>
2010-05-10 17:52:35 +0000

    rearranging for deployment ease

--- trunk/epic/dev_settings.py
+++ trunk/epic/dev_settings.py
@@ -2,24 +2,10 @@ from base_settings import *
 import log_settings
 
 
-DATABASE_ENGINE = 'postgresql_psycopg2'
-DATABASE_NAME = 'epic_web'
-DATABASE_USER = 'epic_appuser'
-DATABASE_PASSWORD = 'du3T9Y'
-DATABASE_HOST = 'cns-dbdev.slis.indiana.edu'
-DATABASE_PORT = ''
-#DATABASE_OPTIONS = {'sslmode': 'require'}
+from deploy_settings import *
+
 
 GOOGLE_KEY = \
     'ABQIAAAAPID1hqHQtPywH_7NchOzFBQaeT_z_s7ky1aQpddgjZAedvZ1kBQhCnJYwofdlXqWAQDTT8TbD7FrGA'
-MEDIA_ROOT = '/home/epic_website/epic_data/'
 
 DEBUG = True
-
-csrfProtect()
-
-log_settings.safeSetup('/home/epic_website/epic_data/server.log')
-
-TEMPLATE_DIRS = (
-	'/home/epic_website/epic_code/epic/templates',
-)
