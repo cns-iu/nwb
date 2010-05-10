e382da78b945e78e6960497ca71b4aab65c1228b
Russell Duhon <rduhon@indiana.edu>
2010-05-10 20:26:09 +0000

    remove SSL requirement, keep port spec

--- trunk/epic/deploy_settings.py
+++ trunk/epic/deploy_settings.py
@@ -7,7 +7,7 @@ from database_credentials import *
 DATABASE_ENGINE = 'postgresql_psycopg2'
 DATABASE_NAME = 'epic_web'
 DATABASE_PORT = '5432'
-DATABASE_OPTIONS = {'sslmode': 'require'}
+#DATABASE_OPTIONS = {'sslmode': 'require'}
 
 MEDIA_ROOT = '/home/epic_website/epic_data/'
 
