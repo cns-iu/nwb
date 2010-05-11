d58a5be7cf6b4f16818f247fecf2eaba13150e44
Russell Duhon <rduhon@indiana.edu>
2010-05-11 21:02:17 +0000

    ready copying from production, fix typo

--- trunk/epic/acquire_production_data.sh
+++ trunk/epic/acquire_production_data.sh
@@ -4,7 +4,7 @@ echo "cd /home/epic_website/epic_code/epic" > remote.sh
 echo "export PYTHONPATH=/home/epic_website:/home/epic_website/epic_code" >> remote.sh
 echo "python2.6 manage.py shell --settings=$EPIC_SETTINGS < wipeout.py" >> remote.sh
 echo "rsync -rz epic:/home/epic_website/epic_data/ /home/epic_website/epic_data" >> remote.sh
-echo "pg_dump -h epic -U epic_user --schema=public epic_web | psql -h $EPIC_HOST -U epic_appuser epic_web
+echo "pg_dump -h cns-dbp -U epic_user --schema=public epic_web | psql -h $DATABASE_HOST -U epic_appuser epic_web" >> remote.sh
 
 scp remote.sh $EPIC_HOST:/tmp/remote.sh
 ssh $EPIC_HOST "chmod +x /tmp/remote.sh"
