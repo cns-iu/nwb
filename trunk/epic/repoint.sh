4c3c97c6e08e568432b700d69a26d1846667098c
Russell Duhon <rduhon@indiana.edu>
2010-05-11 20:02:17 +0000

    forgot to save this prior to previous commit

--- trunk/epic/repoint.sh
+++ trunk/epic/repoint.sh
@@ -12,16 +12,10 @@ echo "cd epic-$SVN_REVISION-$BUILD_NUMBER" >> remote.sh
 echo "chmod +x media_move.sh" >> remote.sh
 echo "./media_move.sh" >> remote.sh
 echo "touch /home/epic_website/epic.wsgi" >> remote.sh
-echo "export PYTHONPATH=/home/epic_website:/home/epic_website/epic_code" >> remote.sh
-echo "python2.6 manage.py shell --settings=$EPIC_SETTINGS < wipeout.py" >> remote.sh
-echo "python2.6 manage.py syncdb --noinput --settings=$EPIC_SETTINGS " >> remote.sh
-echo "python2.6 manage.py loaddata demo_data --settings=$EPIC_SETTINGS " >> remote.sh
-
 
 echo 'if [ -n "$CURRENT_LOC" ]; then' >> remote.sh
 echo '	rm -rf $CURRENT_LOC' >> remote.sh
 echo "fi" >> remote.sh
-echo "rm -rf /home/epic_website/epic_data/*" >> remote.sh
 
 scp remote.sh $EPIC_HOST:/tmp/remote.sh
 ssh $EPIC_HOST "chmod +x /tmp/remote.sh"
