525e2fc3ae240182bfb04116f47ad099f96ad3f3
Russell Duhon <rduhon@indiana.edu>
2010-05-11 19:57:34 +0000

    revert to port 5432, keep ssl. This is probably the correct port, per postgres docs

--- trunk/epic/deploy_settings.py
+++ trunk/epic/deploy_settings.py
@@ -6,7 +6,7 @@ from database_credentials import *
 
 DATABASE_ENGINE = 'postgresql_psycopg2'
 DATABASE_NAME = 'epic_web'
-DATABASE_PORT = '5433'
+DATABASE_PORT = '5432'
 DATABASE_OPTIONS = {'sslmode': 'require'}
 
 MEDIA_ROOT = '/home/epic_website/epic_data/'
