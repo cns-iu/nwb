ef8706414f952a7a5c3cf7e59377c64fb68cdbfa
Russell Duhon <rduhon@indiana.edu>
2010-05-12 18:15:20 +0000

    try different set of commands for database move

--- trunk/epic/acquire_production_data.sh
+++ trunk/epic/acquire_production_data.sh
@@ -6,7 +6,7 @@ echo "python2.6 manage.py shell --settings=$EPIC_SETTINGS < wipeout.py" >> remot
 echo "python2.6 manage.py syncdb --noinput --settings=$EPIC_SETTINGS " >> remote.sh
 echo "python2.6 manage.py reset sessions admin sites contenttypes auth --noinput --settings=$EPIC_SETTINGS " >> remote.sh
 echo "rsync -rvz epic:/home/epic_website/epic_data/ /home/epic_website/epic_data" >> remote.sh
-echo "pg_dump -h cns-dbp -U epic_dumper --data-only --schema=public epic_web | psql -h $DATABASE_HOST -U epic_appuser epic_web" >> remote.sh
+echo "pg_dump -h cns-dbp -U epic_dumper --schema=public -Fc epic_web | pg_restore --clean --now-owner -h $DATABASE_HOST -U epic_appuser epic_web" >> remote.sh
 
 scp remote.sh $EPIC_HOST:/tmp/remote.sh
 ssh $EPIC_HOST "chmod +x /tmp/remote.sh"
