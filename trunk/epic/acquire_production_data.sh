b322a99d7edab92a2dd6d63b5f70c985ed6e740b
Russell Duhon <rduhon@indiana.edu>
2010-05-12 18:18:29 +0000

    fix typo and get rid of now-extraneous bits

--- trunk/epic/acquire_production_data.sh
+++ trunk/epic/acquire_production_data.sh
@@ -3,10 +3,8 @@
 echo "cd /home/epic_website/epic_code/epic" > remote.sh
 echo "export PYTHONPATH=/home/epic_website:/home/epic_website/epic_code" >> remote.sh
 echo "python2.6 manage.py shell --settings=$EPIC_SETTINGS < wipeout.py" >> remote.sh
-echo "python2.6 manage.py syncdb --noinput --settings=$EPIC_SETTINGS " >> remote.sh
-echo "python2.6 manage.py reset sessions admin sites contenttypes auth --noinput --settings=$EPIC_SETTINGS " >> remote.sh
 echo "rsync -rvz epic:/home/epic_website/epic_data/ /home/epic_website/epic_data" >> remote.sh
-echo "pg_dump -h cns-dbp -U epic_dumper --schema=public -Fc epic_web | pg_restore --clean --now-owner -h $DATABASE_HOST -U epic_appuser epic_web" >> remote.sh
+echo "pg_dump -h cns-dbp -U epic_dumper --schema=public -Fc epic_web | pg_restore --clean --no-owner -h $DATABASE_HOST -U epic_appuser epic_web" >> remote.sh
 
 scp remote.sh $EPIC_HOST:/tmp/remote.sh
 ssh $EPIC_HOST "chmod +x /tmp/remote.sh"
