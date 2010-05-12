e8c47edde9fa4d79031b6a84db43d212eaed0c01
Russell Duhon <rduhon@indiana.edu>
2010-05-12 18:42:00 +0000

    get rid of info lines

--- trunk/epic/acquire_production_data.sh
+++ trunk/epic/acquire_production_data.sh
@@ -4,8 +4,6 @@ echo "cd /home/epic_website/epic_code/epic" > remote.sh
 echo "export PYTHONPATH=/home/epic_website:/home/epic_website/epic_code" >> remote.sh
 echo "python2.6 manage.py shell --settings=$EPIC_SETTINGS < wipeout.py" >> remote.sh
 echo "rsync -rvz epic:/home/epic_website/epic_data/ /home/epic_website/epic_data" >> remote.sh
-echo "pg_dump -h cns-dbp -U epic_dumper --schema=public -Fc epic_web > /tmp/dumped.dump" >> remote.sh
-echo "pg_restore -l /tmp/dumped.dump" >> remote.sh
 echo "pg_dump -h cns-dbp -U epic_dumper --schema=public -Fc epic_web | pg_restore -r --no-acl --no-owner -h $DATABASE_HOST -U epic_appuser -d epic_web" >> remote.sh
 
 scp remote.sh $EPIC_HOST:/tmp/remote.sh
