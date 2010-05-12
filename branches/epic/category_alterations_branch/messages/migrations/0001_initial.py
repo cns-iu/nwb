97db65bcf6aee032418657c29c91d1271f7c15ff
Russell Duhon <rduhon@indiana.edu>
2010-05-12 23:58:37 +0000

    Branch for category alterations deployment to staging.

---
+++ branches/epic/category_alterations_branch/messages/migrations/0001_initial.py
@@ -0,0 +1,111 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+    
+    def forwards(self, orm):
+        
+        # Adding model 'Message'
+        db.create_table('messages_message', (
+            ('sender', self.gf('django.db.models.fields.related.ForeignKey')(related_name='sender', to=orm['auth.User'])),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, db_index=True, blank=True)),
+            ('message', self.gf('django.db.models.fields.CharField')(max_length=16384)),
+            ('recipient', self.gf('django.db.models.fields.related.ForeignKey')(related_name='recipient', to=orm['auth.User'])),
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('subject', self.gf('django.db.models.fields.CharField')(max_length=128)),
+        ))
+        db.send_create_signal('messages', ['Message'])
+
+        # Adding model 'ReceivedMessage'
+        db.create_table('messages_receivedmessage', (
+            ('read', self.gf('django.db.models.fields.BooleanField')(default=False, blank=True)),
+            ('replied', self.gf('django.db.models.fields.BooleanField')(default=False, blank=True)),
+            ('deleted', self.gf('django.db.models.fields.BooleanField')(default=False, blank=True)),
+            ('message_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['messages.Message'], unique=True, primary_key=True)),
+        ))
+        db.send_create_signal('messages', ['ReceivedMessage'])
+
+        # Adding model 'SentMessage'
+        db.create_table('messages_sentmessage', (
+            ('deleted', self.gf('django.db.models.fields.BooleanField')(default=False, blank=True)),
+            ('message_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['messages.Message'], unique=True, primary_key=True)),
+        ))
+        db.send_create_signal('messages', ['SentMessage'])
+    
+    
+    def backwards(self, orm):
+        
+        # Deleting model 'Message'
+        db.delete_table('messages_message')
+
+        # Deleting model 'ReceivedMessage'
+        db.delete_table('messages_receivedmessage')
+
+        # Deleting model 'SentMessage'
+        db.delete_table('messages_sentmessage')
+    
+    
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '80', 'unique': 'True'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'max_length': '30', 'unique': 'True'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'messages.message': {
+            'Meta': {'object_name': 'Message'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'db_index': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'message': ('django.db.models.fields.CharField', [], {'max_length': '16384'}),
+            'recipient': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'recipient'", 'to': "orm['auth.User']"}),
+            'sender': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'sender'", 'to': "orm['auth.User']"}),
+            'subject': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        'messages.receivedmessage': {
+            'Meta': {'object_name': 'ReceivedMessage', '_ormbases': ['messages.Message']},
+            'deleted': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'message_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['messages.Message']", 'unique': 'True', 'primary_key': 'True'}),
+            'read': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'replied': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'})
+        },
+        'messages.sentmessage': {
+            'Meta': {'object_name': 'SentMessage', '_ormbases': ['messages.Message']},
+            'deleted': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'message_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['messages.Message']", 'unique': 'True', 'primary_key': 'True'})
+        }
+    }
+    
+    complete_apps = ['messages']
