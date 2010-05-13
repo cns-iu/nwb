10b9c516256ab9e96c9c78eb9ae521e903ac1b7d
Russell Duhon <rduhon@indiana.edu>
2010-05-13 15:41:50 +0000

    fix hudson settings

--- trunk/epic/hudson_settings.py
+++ trunk/epic/hudson_settings.py
@@ -2,6 +2,5 @@ from base_settings import *
 import log_settings
 
 
-log_settings.safeSetup()
-
+log_settings.safeSetup('local_logging.conf')
 TEST_RUNNER='xmlrunner.extra.djangotestrunner.run_tests'
