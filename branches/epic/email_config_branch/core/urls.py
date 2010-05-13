082e72f8b55a87238f5df47c0c6deaa02beea555
Russell Duhon <rduhon@indiana.edu>
2010-05-13 16:01:36 +0000

    email host change

---
+++ branches/epic/email_config_branch/core/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('epic.core.views',
+    (r'^$', 'view_profile', {'user_id': None}),
+	(r'^profile/(?P<user_id>\d+)/$', 'view_profile'),
+	(r'^edit_profile/$', 'edit_profile'),
+	(r'^forgot_password/$', 'forgot_password'),
+	(r'^change_password/$', 'change_password'),
+    (r'^deactivated_account/$', 'deactivated_account'),
+)
+urlpatterns += patterns('',
+	(r'^(?P<user_id>\d+)/messages/', include('epic.messages.urls')),
+)
