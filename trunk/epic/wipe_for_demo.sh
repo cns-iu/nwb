29a717e72cb424a37fdf0d537f303b79e2abb91b
Russell Duhon <rduhon@indiana.edu>
2010-05-14 17:20:05 +0000

    change to new fixtures for demo

--- trunk/epic/wipe_for_demo.sh
+++ trunk/epic/wipe_for_demo.sh
@@ -5,7 +5,7 @@ echo "export PYTHONPATH=/home/epic_website:/home/epic_website/epic_code" >> remo
 echo "python2.6 manage.py shell --settings=$EPIC_SETTINGS < wipeout.py" >> remote.sh
 echo "python2.6 manage.py syncdb --all --noinput --settings=$EPIC_SETTINGS " >> remote.sh
 echo "python2.6 manage.py migrate --fake --settings=$EPIC_SETTINGS " >> remote.sh
-echo "python2.6 manage.py loaddata demo_data --settings=$EPIC_SETTINGS " >> remote.sh
+echo "python2.6 manage.py loaddata demo_data_2 --settings=$EPIC_SETTINGS " >> remote.sh
 echo "rm -rf /home/epic_website/epic_data/*" >> remote.sh
 
 scp remote.sh $EPIC_HOST:/tmp/remote.sh
