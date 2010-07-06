bf73c8c62919767e5c9caf47d3f2af47b5d88f47
Patrick Phillips <pataphil@indiana.edu>
2010-07-06 20:22:04 +0000

    * Partially made Java 1.5.

--- trunk/plugins/visualization/edu.iu.nwb.visualization.roundrussell/src/edu/iu/nwb/visualization/roundrussell/RoundRussellAlgorithmFactory.java
+++ trunk/plugins/visualization/edu.iu.nwb.visualization.roundrussell/src/edu/iu/nwb/visualization/roundrussell/RoundRussellAlgorithmFactory.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Dictionary;
 import java.util.LinkedHashMap;
 
@@ -12,6 +15,7 @@ import org.cishell.framework.algorithm.Algorithm;
 import org.cishell.framework.algorithm.AlgorithmFactory;
 import org.cishell.framework.algorithm.ParameterMutator;
 import org.cishell.framework.data.Data;
+import org.cishell.utilities.ArrayListUtilities;
 import org.cishell.utilities.MapUtilities;
 import org.cishell.utilities.mutateParameter.dropdown.DropdownMutator;
 import org.osgi.framework.BundleContext;
@@ -35,22 +39,38 @@ public class RoundRussellAlgorithmFactory implements AlgorithmFactory, Parameter
 	public static final String NO_STRENGTH_IDENTIFIER = "No Strength";
 	public static final String NO_COLOR_IDENTIFIER = "No Node Color";
 	public static final String NO_LEVEL_IDENTIFIER = "No Level";
+	public static final Collection<String> NODE_KEYS_TO_IGNORE =
+		Collections.unmodifiableList(Arrays.asList(NWBFileProperty.ATTRIBUTE_ID));
+	public static final Collection<String> NODE_STRENGTH_KEYS_TO_ADD_TO_FRONT =
+		Collections.unmodifiableList(Arrays.asList(NO_STRENGTH_IDENTIFIER));
+	public static final Collection<String> NODE_C0LOR_KEYS_TO_ADD_TO_FRONT =
+		Collections.unmodifiableList(Arrays.asList(NO_COLOR_IDENTIFIER));
+	public static final Collection<String> NODE_LEVEL_KEYS_TO_ADD_TO_FRONT =
+		Collections.unmodifiableList(Arrays.asList(NO_LEVEL_IDENTIFIER));
+	public static final Collection<String> EGGE_KEYS_TO_IGNORE =
+		Collections.unmodifiableList(Arrays.asList(
+			NWBFileProperty.ATTRIBUTE_SOURCE, NWBFileProperty.ATTRIBUTE_TARGET));
+	public static final Collection<String> EDGE_KEYS_TO_ADD_TO_FRONT =
+		Collections.unmodifiableList(Arrays.asList(NO_EDGE_WEIGHT_IDENTIFIER));
 	
 	private static URL postScriptHeaderPath;
 	private static final String postScriptHeaderFilePath = "postScriptHeader.ps";
 
-	protected void activate(ComponentContext ctxt) {
-    	BundleContext bContext = ctxt.getBundleContext();
+	protected void activate(ComponentContext componentContext) {
+    	BundleContext bundleContext = componentContext.getBundleContext();
     	
-    	this.postScriptHeaderPath = bContext.getBundle().getResource(postScriptHeaderFilePath);
+    	RoundRussellAlgorithmFactory.postScriptHeaderPath =
+    		bundleContext.getBundle().getResource(postScriptHeaderFilePath);
     	PostScriptOperations.setPostScriptHeaderFile(postScriptHeaderPath);
     	
     }
 	
-	public Algorithm createAlgorithm(Data[] data, Dictionary parameters, CIShellContext context) {
+	public Algorithm createAlgorithm(
+			Data[] data, Dictionary<String, Object> parameters, CIShellContext context) {
 		return new RoundRussellAlgorithm(data, parameters, context);
 	}
 	
+	@SuppressWarnings("unchecked")	// Raw Map (metaDataHandler.getNodeSchema())
 	public ObjectClassDefinition mutateParameters(Data[] data, ObjectClassDefinition parameters) {
 		File nwbFile = (File) data[0].getData();
 		GetNWBFileMetadata metaDataHandler = new GetNWBFileMetadata();
@@ -63,27 +83,46 @@ public class RoundRussellAlgorithmFactory implements AlgorithmFactory, Parameter
 		} catch (ParsingException e) {
 			return parameters;
 		}
-		
-		String[] nodeStrengthAttribute =
-			MapUtilities.getValidKeysOfTypesInMap(
-					metaDataHandler.getNodeSchema(),
-					(String[]) NWBFileProperty.NUMERIC_ATTRIBUTE_TYPES.toArray(new String[0]),
-					new String[]{ NWBFileProperty.ATTRIBUTE_ID },
-					new String[]{ NO_STRENGTH_IDENTIFIER });
-		
-		String[] nodeColorAttribute =
-			MapUtilities.getValidKeysOfTypesInMap(
-					metaDataHandler.getNodeSchema(),
-					(String[]) NWBFileProperty.NUMERIC_ATTRIBUTE_TYPES.toArray(new String[0]),
-					new String[]{ NWBFileProperty.ATTRIBUTE_ID },
-					new String[]{ NO_COLOR_IDENTIFIER });
 
-		String[] nodeLevelAttribute =
-			MapUtilities.getValidKeysOfTypesInMap(
-					metaDataHandler.getNodeSchema(),
-					(String[]) NWBFileProperty.ALL_ATTRIBUTE_TYPES.toArray(new String[0]),
-					new String[]{ NWBFileProperty.ATTRIBUTE_ID },
-					new String[]{ NO_LEVEL_IDENTIFIER });
+		Collection<String> nodeStrengthAttribute = MapUtilities.getValidKeysOfTypesInMap(
+			metaDataHandler.getNodeSchema(),
+			NWBFileProperty.NUMERIC_ATTRIBUTE_TYPES,
+			NODE_KEYS_TO_IGNORE);
+		nodeStrengthAttribute = ArrayListUtilities.unionCollections(
+			NODE_STRENGTH_KEYS_TO_ADD_TO_FRONT, nodeStrengthAttribute, null);
+//		String[] nodeStrengthAttribute =
+//			MapUtilities.getValidKeysOfTypesInMap(
+//					metaDataHandler.getNodeSchema(),
+//					(String[]) NWBFileProperty.NUMERIC_ATTRIBUTE_TYPES.toArray(new String[0]),
+//					new String[]{ NWBFileProperty.ATTRIBUTE_ID },
+//					new String[]{ NO_STRENGTH_IDENTIFIER });
+
+		Collection<String> nodeColorAttribute = MapUtilities.getValidKeysOfTypesInMap(
+			metaDataHandler.getNodeSchema(),
+			NWBFileProperty.NUMERIC_ATTRIBUTE_TYPES,
+			NODE_KEYS_TO_IGNORE);
+		nodeColorAttribute = ArrayListUtilities.unionCollections(
+			NODE_C0LOR_KEYS_TO_ADD_TO_FRONT, nodeColorAttribute, null);
+//		String[] nodeColorAttribute =
+//			MapUtilities.getValidKeysOfTypesInMap(
+//					metaDataHandler.getNodeSchema(),
+//					(String[]) NWBFileProperty.NUMERIC_ATTRIBUTE_TYPES.toArray(new String[0]),
+//					new String[]{ NWBFileProperty.ATTRIBUTE_ID },
+//					new String[]{ NO_COLOR_IDENTIFIER });
+
+
+		Collection<String> nodeLevelAttribute = MapUtilities.getValidKeysOfTypesInMap(
+			metaDataHandler.getNodeSchema(),
+			NWBFileProperty.ALL_ATTRIBUTE_TYPES,
+			NODE_KEYS_TO_IGNORE);
+		nodeLevelAttribute = ArrayListUtilities.unionCollections(
+			NODE_LEVEL_KEYS_TO_ADD_TO_FRONT, nodeLevelAttribute, null);
+//		String[] nodeLevelAttribute =
+//			MapUtilities.getValidKeysOfTypesInMap(
+//					metaDataHandler.getNodeSchema(),
+//					(String[]) NWBFileProperty.ALL_ATTRIBUTE_TYPES.toArray(new String[0]),
+//					new String[]{ NWBFileProperty.ATTRIBUTE_ID },
+//					new String[]{ NO_LEVEL_IDENTIFIER });
 		
 		
 		LinkedHashMap edgeSchema;
@@ -92,14 +131,20 @@ public class RoundRussellAlgorithmFactory implements AlgorithmFactory, Parameter
 		} else {
 			edgeSchema = metaDataHandler.getUndirectedEdgeSchema();
 		}
-		
-		String[] edgeAttributes =
-			MapUtilities.getValidKeysOfTypesInMap(
-					edgeSchema,
-					(String[]) NWBFileProperty.NUMERIC_ATTRIBUTE_TYPES.toArray(new String[0]),
-					new String[]{ NWBFileProperty.ATTRIBUTE_SOURCE, 
-								  NWBFileProperty.ATTRIBUTE_TARGET },
-					new String[]{ NO_EDGE_WEIGHT_IDENTIFIER });
+
+		Collection<String> edgeAttributes = MapUtilities.getValidKeysOfTypesInMap(
+			edgeSchema,
+			NWBFileProperty.NUMERIC_ATTRIBUTE_TYPES,
+			EGGE_KEYS_TO_IGNORE);
+		edgeAttributes = ArrayListUtilities.unionCollections(
+			EDGE_KEYS_TO_ADD_TO_FRONT, edgeAttributes, null);
+//		String[] edgeAttributes =
+//			MapUtilities.getValidKeysOfTypesInMap(
+//					edgeSchema,
+//					(String[]) NWBFileProperty.NUMERIC_ATTRIBUTE_TYPES.toArray(new String[0]),
+//					new String[]{ NWBFileProperty.ATTRIBUTE_SOURCE, 
+//								  NWBFileProperty.ATTRIBUTE_TARGET },
+//					new String[]{ NO_EDGE_WEIGHT_IDENTIFIER });
 		
 		DropdownMutator mutator = new DropdownMutator();		
 		
@@ -139,17 +184,15 @@ public class RoundRussellAlgorithmFactory implements AlgorithmFactory, Parameter
 	
 	/*
 	 * To smartly select the appropriate level column name from the drop down box.
-	 * */
-	private String getDefaultLevelOption(String[] nodeLevelAttribute, int levelIndicator) {
-		for (int attributesIndex = 0; 
-				attributesIndex < nodeLevelAttribute.length; 
-				attributesIndex++) {			
-			String lowerCaseNodeLevelAttributeName = 
-				nodeLevelAttribute[attributesIndex].toLowerCase();
+	 */
+	private String getDefaultLevelOption(
+			Collection<String> nodeLevelAttributes, int levelIndicator) {
+		for (String nodeLevelAttribute : nodeLevelAttributes) {
+			String lowerCaseNodeLevelAttributeName =  nodeLevelAttribute.toLowerCase();
 			
-			if (lowerCaseNodeLevelAttributeName.contains("level") 
-					&& lowerCaseNodeLevelAttributeName.contains(String.valueOf(levelIndicator))) {
-				return nodeLevelAttribute[attributesIndex];
+			if (lowerCaseNodeLevelAttributeName.contains("level") &&
+					lowerCaseNodeLevelAttributeName.contains(String.valueOf(levelIndicator))) {
+				return nodeLevelAttribute;
 			}
 	    }
 		
@@ -160,18 +203,14 @@ public class RoundRussellAlgorithmFactory implements AlgorithmFactory, Parameter
 	 * To smartly select the appropriate strength or weight column name from
 	 *  the drop down box.
 	 */
-	private String getMatchedKey(String[] attributeNames,
-									  String suggestedPattern) {		
+	private String getMatchedKey(Collection<String> attributeNames, String suggestedPattern) {		
 		String lowerCaseSuggestedPattern = suggestedPattern.toLowerCase();
-		
-		for (int attributesIndex = 0;
-				attributesIndex < attributeNames.length;
-				attributesIndex++) {
-			String lowerCaseAttributeName =
-				attributeNames[attributesIndex].toLowerCase();
+
+		for (String attributeName : attributeNames) {
+			String lowerCaseAttributeName = attributeName.toLowerCase();
 
 			if (lowerCaseAttributeName.contains(lowerCaseSuggestedPattern)) {
-				return attributeNames[attributesIndex];
+				return attributeName;
 			}
 	    }
 		
