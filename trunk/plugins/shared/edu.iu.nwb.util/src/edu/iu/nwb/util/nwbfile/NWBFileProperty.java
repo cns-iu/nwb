cef1874c04d7b7a87e1a5e968757721ea65be5b8
Patrick Phillips <pataphil@indiana.edu>
2010-07-06 15:06:46 +0000

    * Partially made Java 1.5.

--- trunk/plugins/shared/edu.iu.nwb.util/src/edu/iu/nwb/util/nwbfile/NWBFileProperty.java
+++ trunk/plugins/shared/edu.iu.nwb.util/src/edu/iu/nwb/util/nwbfile/NWBFileProperty.java
@@ -1,6 +1,7 @@
 package edu.iu.nwb.util.nwbfile;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -22,20 +23,11 @@ public class NWBFileProperty {
     public static final String TYPE_FLOAT = "float";  
     public static final String TYPE_REAL = "real";
     
-    public static final List NUMERIC_ATTRIBUTE_TYPES =
-    	Arrays.asList(new String[]{ 
-    		TYPE_INT,
-    		TYPE_FLOAT,
-    		TYPE_REAL
-    	});
+    public static final Collection<String> NUMERIC_ATTRIBUTE_TYPES = Collections.unmodifiableList(
+    	Arrays.asList(new String[] { TYPE_INT, TYPE_FLOAT, TYPE_REAL }));
     
-    public static final List ALL_ATTRIBUTE_TYPES =
-    	Arrays.asList(new String[]{ 
-    		TYPE_INT,
-    		TYPE_STRING,
-    		TYPE_FLOAT,
-    		TYPE_REAL
-    	});
+    public static final Collection<String> ALL_ATTRIBUTE_TYPES = Collections.unmodifiableList(
+    		Arrays.asList(new String[] { TYPE_INT, TYPE_STRING, TYPE_FLOAT, TYPE_REAL }));
      
 //    public static final String HEADER_NODE_ATTRIBUTES = "*NodeAttributes";
 //    public static final String HEADER_EDGE_ATTRIBUTES = "*EdgeAttributes";
