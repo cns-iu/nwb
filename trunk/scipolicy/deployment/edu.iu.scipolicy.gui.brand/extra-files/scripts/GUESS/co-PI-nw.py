2073580590c5766cf198b81360e9df5c8bac4d74
Patrick Phillips <pataphil@indiana.edu>
2010-10-05 16:11:54 +0000

    * Fixed a bug in the co-PI-nw.py GUESS script.

--- trunk/scipolicy/deployment/edu.iu.scipolicy.gui.brand/extra-files/scripts/GUESS/co-PI-nw.py
+++ trunk/scipolicy/deployment/edu.iu.scipolicy.gui.brand/extra-files/scripts/GUESS/co-PI-nw.py
@@ -1,14 +1,16 @@
-n1.indegree
-n2.indegree
 resizeLinear(totalawardmoney, 5, 50)
-colorize(directed,gray,black)
-colorize(totalawardmoney,black,green)
-nodesbytotalawardmoney=g.nodes[:]
+colorize(directed, gray, black)
+colorize(totalawardmoney, black, green)
+nodesByTotalAwardMoney = g.nodes[:]
+
 def bytotalawardmoney(n1,n2):
  	return cmp(n1.totalawardmoney, n2.totalawardmoney)
-nodesbytotalawardmoney.sort(bytotalawardmoney)
-nodesbytotalawardmoney.reverse()
-for i in range(0, 50): 
- 	nodesbytotalawardmoney[i].labelvisible=true
-for i in range(0, 10):
-	print str(nodesbytotalawardmoney[i].label) + ": " + str(nodesbytotalawardmoney[i].totalawardmoney)
+
+nodesByTotalAwardMoney.sort(bytotalawardmoney)
+nodesByTotalAwardMoney.reverse()
+
+for i in range(0, min(nodesByTotalAwardMoney.size(), 50)): 
+ 	nodesByTotalAwardMoney[i].labelvisible=true
+
+for i in range(0, min(nodesByTotalAwardMoney, 10)):
+	print str(nodesByTotalAwardMoney[i].label) + ": " + str(nodesByTotalAwardMoney[i].totalawardmoney)
