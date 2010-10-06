27c416452cf759b42e3f64773740da0b09fc0e0a
Patrick Phillips <pataphil@indiana.edu>
2010-10-06 12:57:36 +0000

    * Added validation for empty column names.

--- trunk/scipolicy/plugins/normal_plugins/edu.iu.sci2.database.star.common/src/edu/iu/sci2/database/star/common/StarDatabaseCSVDataValidationRules.java
+++ trunk/scipolicy/plugins/normal_plugins/edu.iu.sci2.database.star.common/src/edu/iu/sci2/database/star/common/StarDatabaseCSVDataValidationRules.java
@@ -2,13 +2,14 @@ package edu.iu.sci2.database.star.common;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map.Entry;
 import java.util.regex.Pattern;
 
 import org.cishell.utilities.StringUtilities;
 import org.osgi.service.log.LogService;
 
-import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Multimap;
 
 import edu.iu.sci2.database.star.common.utility.csv.validator.CSVDataValidationRules;
@@ -24,6 +25,11 @@ public class StarDatabaseCSVDataValidationRules implements CSVDataValidationRule
 
 	public void validateHeader(String[] header) throws CSVHeaderValidationException {
 		Multimap<String, String> normalizedNamesByNames = getNormalizedNamesByNames(header);
+
+		Collection<Integer> emptyColumnNameIndices =
+			determineColumnHeaderNames(normalizedNamesByNames);
+		logEmptyColumnNames(emptyColumnNameIndices, this.logger);
+
 		Collection<Collection<String>> duplicateNamesSets =
 			getDuplicateNamesSets(normalizedNamesByNames);
 		logDuplicateNamesSets(duplicateNamesSets, this.logger);
@@ -33,13 +39,31 @@ public class StarDatabaseCSVDataValidationRules implements CSVDataValidationRule
 	}
 
 	private static Multimap<String, String> getNormalizedNamesByNames(String[] names) {
-		Multimap<String, String> normalizedNamesByNames = ArrayListMultimap.create();
+		Multimap<String, String> normalizedNamesByNames = LinkedHashMultimap.create();
 
 		for (String name : names) {
 			normalizedNamesByNames.put(normalizeName(name), name);
 		}
 
 		return normalizedNamesByNames;
+	}
+
+	private static Collection<Integer> determineColumnHeaderNames(
+			Multimap<String, String> normalizedNamesByNames) throws CSVHeaderValidationException {
+		// TODO: Refactor this to use Google Collections?
+
+		Collection<Integer> emptyColumnNameIndices = new HashSet<Integer>();
+
+		int columnIndex = 0;
+		for (String columnName : normalizedNamesByNames.values()) {
+			if (StringUtilities.isNull_Empty_OrWhitespace(columnName)) {
+				emptyColumnNameIndices.add(columnIndex);
+			}
+
+			columnIndex++;
+		}
+
+		return emptyColumnNameIndices;
 	}
 
 	private static Collection<Collection<String>> getDuplicateNamesSets(
@@ -57,11 +81,23 @@ public class StarDatabaseCSVDataValidationRules implements CSVDataValidationRule
 		return duplicateNamesSets;
 	}
 
+	// TODO: Think about throwing multiple error messages at a time.
+	public static void logEmptyColumnNames(
+			Collection<Integer> emptyColumnNameIndices, LogService logger)
+			throws CSVHeaderValidationException {
+		if (emptyColumnNameIndices.size() > 0) {
+			String logMessage = buildEmptyColumnNamesLogMessage(emptyColumnNameIndices);
+			logger.log(LogService.LOG_ERROR, logMessage);
+
+			throw new CSVHeaderValidationException(logMessage);
+		}
+	}
+
 	public static void logDuplicateNamesSets(
 			Collection<Collection<String>> duplicateNamesSets, LogService logger)
 			throws CSVHeaderValidationException {
 		if (duplicateNamesSets.size() > 0) {
-			String logMessage = buildLogMessage(duplicateNamesSets);
+			String logMessage = buildDuplicateNamesLogMessage(duplicateNamesSets);
 			logger.log(LogService.LOG_ERROR, logMessage);
 
 			throw new CSVHeaderValidationException(logMessage);
@@ -77,8 +113,21 @@ public class StarDatabaseCSVDataValidationRules implements CSVDataValidationRule
 		return withWhitespaceReplacedByUnderscores;
 	}
 
-	private static String buildLogMessage(Collection<Collection<String>> duplicateNamesSets) {
-		final String logMessageHeader =
+	private static String buildEmptyColumnNamesLogMessage(
+			Collection<Integer> emptyColumnNameIndices) {
+		String format =
+			"The names of the columns in your CSV file must all be specified to be usable in" +
+			" a database.  When normalizing them, one or more name(s) were found that cannot be" +
+			"normalized for use in a database.  Column indices: [ %s ]";
+		String logMessage = String.format(
+			format, StringUtilities.implodeItems(emptyColumnNameIndices, ", "));
+
+		return logMessage;
+	}
+
+	private static String buildDuplicateNamesLogMessage(
+			Collection<Collection<String>> duplicateNamesSets) {
+		String logMessageHeader =
 			"The names of the columns in your CSV file must be normalized to be usable in " +
 			"a database.  When normalizing, one or more set(s) of columns conflicted: ";
 		Collection<String> duplicateNamesLogMessages = new ArrayList<String>();
