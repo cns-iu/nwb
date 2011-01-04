81f41e7eda509c4a7a5e2b3a2dd1d9cb3f8741bb
Patrick Phillips <pataphil@indiana.edu>
2011-01-04 16:54:36 +0000

    * Metadata Reporter now outputs a more conclusive, useful algorithm CSV.

--- trunk/cns/edu.iu.cns.tool.metadata_report/src/edu/iu/cns/tool/metadata_report/MetadataReporterAlgorithm.java
+++ trunk/cns/edu.iu.cns.tool.metadata_report/src/edu/iu/cns/tool/metadata_report/MetadataReporterAlgorithm.java
@@ -4,17 +4,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Dictionary;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import org.cishell.framework.algorithm.Algorithm;
 import org.cishell.framework.algorithm.AlgorithmExecutionException;
@@ -22,14 +14,9 @@ import org.cishell.framework.data.BasicData;
 import org.cishell.framework.data.Data;
 import org.cishell.framework.data.DataProperty;
 import org.cishell.utilities.FileUtilities;
-import org.cishell.utilities.StringUtilities;
-import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
-
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Multimap;
 
 public class MetadataReporterAlgorithm implements Algorithm {
 	public static final String TEXT_FILE_EXTENSION = "txt";
@@ -84,117 +71,55 @@ public class MetadataReporterAlgorithm implements Algorithm {
     }
 
     public Data[] execute() throws AlgorithmExecutionException {
-    	FileWriter reportWriter = null;
-    	FileWriter headersWriter = null;
-    	FileWriter bundleNameWriter = null;
+    	FileWriter metadataWriter = null;
 
     	try {
     		ServiceReference[] serviceReferences =
     			this.bundleContext.getAllServiceReferences(null, null);
-
-    		Multimap<String, String> symbolicNamesToReports =
-    			HashMultimap.<String, String>create();
-
-    		Map<String, List<String>> bundleNamesToProperties =
-    			new HashMap<String, List<String>>();
-    		
-    		Set<Dictionary> bundleHeaders = new HashSet<Dictionary>();
-
-    		for (ServiceReference serviceReference : serviceReferences) {
-    			Bundle bundle = serviceReference.getBundle();
-    			String symbolicName = bundle.getSymbolicName();
-
-    			if (!SERVICE_SYMBOLIC_NAMES_TO_IGNORE.contains(symbolicName)) {
-    				String pluginString = createPluginString(serviceReference);
-    				symbolicNamesToReports.put(symbolicName, pluginString);
-
-    				List<String> properties = new ArrayList<String>();
-    				Object pid = serviceReference.getProperty("service.pid");
-
-    				if (pid != null) {
-    					properties.add(pid.toString());
-    				} else {
-    					properties.add("null");
-    				}
-
-    				Object label = serviceReference.getProperty("label");
-
-    				if (label != null) {
-    					properties.add(label.toString());
-    				} else {
-    					properties.add("null");
-    				}
-
-    				bundleNamesToProperties.put(symbolicName, properties);
-
-    				bundleHeaders.add(bundle.getHeaders());
-    			}
-    		}
-
-    		File reportFile = FileUtilities.createTemporaryFileInDefaultTemporaryDirectory(
-    			"algorithm_metadata_report", TEXT_FILE_EXTENSION);
-    		reportWriter = new FileWriter(reportFile);
     		File bundleNameFile = FileUtilities.createTemporaryFileInDefaultTemporaryDirectory(
     			"pluginNames", CSV_FILE_EXTENSION);
-    		bundleNameWriter = new FileWriter(bundleNameFile);
+    		metadataWriter = new FileWriter(bundleNameFile);
 
-    		bundleNameWriter.append("Plugin Name,Algorithm PID,Label\n");
+    		metadataWriter.append(String.format(
+    			"Menu Label,Algorithm ID,Containing Plug-In,Description%n"));
 
-    		for (Iterator<String> keys = symbolicNamesToReports.keySet().iterator();
-    				keys.hasNext();) {
-    			String key = keys.next();
+    		for (ServiceReference serviceReference : serviceReferences) {
+    			Object menuPathObject = serviceReference.getProperty("label");
 
-    			for (String value : symbolicNamesToReports.get(key)) {
-    				reportWriter.append(value);
+    			if (menuPathObject != null) {
+    				String menuLabel = menuPathObject.toString();
+    				String algorithmPID = serviceReference.getProperty("service.pid").toString();
+    				String containingPluginName = serviceReference.getBundle().getSymbolicName();
+    				Object descriptionObject = serviceReference.getProperty("description");
+    				String description = "N/A";
+
+    				if (descriptionObject != null) {
+    					description = descriptionObject.toString();
+    				}
+
+    				String row = String.format(
+    					"\"%s\",\"%s\",\"%s\",\"%s\"%n",
+    					menuLabel,
+    					algorithmPID,
+    					containingPluginName,
+    					description);
+    				metadataWriter.append(row);
     			}
-
-   				bundleNameWriter.append(
-    				key + "," +
-    				StringUtilities.implodeList(bundleNamesToProperties.get(key), ",") +
-    				"\n");
     		}
 
-    		reportWriter.close();
-    		bundleNameWriter.close();
-
-    		File headersFile = FileUtilities.createTemporaryFileInDefaultTemporaryDirectory(
-    			"bundle_headers", TEXT_FILE_EXTENSION);
-    		headersWriter = new FileWriter(headersFile);
-
-    		for (Dictionary headers : bundleHeaders) {
-    			headersWriter.append(headers.toString() + ":\n");
-
-    			for (Enumeration keys = headers.keys(); keys.hasMoreElements();) {
-    				String key = keys.nextElement().toString();
-    				headersWriter.append("\t" + key + ": \"" + headers.get(key) + "\"\n");
-    			}
-
-    			headersWriter.append("\n");
-    		}
-
-    		headersWriter.close();
-
-    		
-//
-//    		// TODO Sort by symbolic name first.
-//    		for (ServiceReference serviceReference : serviceReferences) {
-//    			String pluginString = createPluginString(serviceReference);
-//    			reportWriter.append(pluginString);
-//    		}
-
-    		return formOutData(reportFile, headersFile, bundleNameFile);
+    		return formOutData(bundleNameFile);
     	} catch (InvalidSyntaxException e) {
     		String exceptionMessage = "No one should ever see this: \"" + e.getMessage() + "\"";
     		throw new AlgorithmExecutionException(exceptionMessage, e);
     	} catch (IOException e) {
-    		String exceptionMessage =
-    			"An error occurred when trying to write to the algorithm metadata report " +
-    			"to a temporary file: \"" + e.getMessage() + "\"";
+	    		String exceptionMessage =
+    				"An error occurred when trying to write to the algorithm metadata report " +
+    				"to a temporary file: \"" + e.getMessage() + "\"";
     		throw new AlgorithmExecutionException(exceptionMessage, e);
     	} finally {
-    		if (reportWriter != null) {
+    		if (metadataWriter != null) {
     			try {
-    				reportWriter.close();
+    				metadataWriter.close();
     			} catch (IOException e) {
     				String exceptionMessage =
     					"An error occurred when trying to close the algorithm metadata report " +
@@ -202,88 +127,221 @@ public class MetadataReporterAlgorithm implements Algorithm {
     				throw new AlgorithmExecutionException(exceptionMessage, e);
     			}
     		}
-
-    		if (headersWriter != null) {
-    			try {
-    				headersWriter.close();
-    			} catch (IOException e) {
-    				String exceptionMessage =
-    					"An error occurred when trying to close the bundle headers file: \"" +
-    					e.getMessage() + "\"";
-    				throw new AlgorithmExecutionException(exceptionMessage, e);
-    			}
-    		}
-
-			if (bundleNameWriter != null) {
-    			try {
-    				bundleNameWriter.close();
-    			} catch (IOException e) {
-    				String exceptionMessage =
-    					"An error occurred when trying to close the plugin names file: \"" +
-    					e.getMessage() + "\"";
-    				throw new AlgorithmExecutionException(exceptionMessage, e);
-    			}
-    		}
     	}
     }
+//    public Data[] execute() throws AlgorithmExecutionException {
+//    	FileWriter reportWriter = null;
+//    	FileWriter headersWriter = null;
+//    	FileWriter bundleNameWriter = null;
+//
+//    	try {
+//    		ServiceReference[] serviceReferences =
+//    			this.bundleContext.getAllServiceReferences(null, null);
+//
+//    		Multimap<String, String> symbolicNamesToReport =
+//    			HashMultimap.<String, String>create();
+//
+//    		Map<String, List<String>> bundleNamesToProperties =
+//    			new HashMap<String, List<String>>();
+//    		
+//    		Set<Dictionary> bundleHeaders = new HashSet<Dictionary>();
+//
+//    		for (ServiceReference serviceReference : serviceReferences) {
+////    			System.err.println("property keys: " + StringUtilities.implodeStringArray(
+////    				serviceReference.getPropertyKeys(), ", "));
+//    			
+//    			Bundle bundle = serviceReference.getBundle();
+//    			String symbolicName = bundle.getSymbolicName();
+//
+//    			if (!SERVICE_SYMBOLIC_NAMES_TO_IGNORE.contains(symbolicName)) {
+//    				String pluginString = createPluginString(serviceReference);
+//    				symbolicNamesToReport.put(symbolicName, pluginString);
+//
+//    				List<String> properties = new ArrayList<String>();
+//    				Object pid = serviceReference.getProperty("service.pid");
+//
+//    				if (pid != null) {
+//    					properties.add(pid.toString());
+//    				} else {
+//    					properties.add("null");
+//    				}
+//
+//    				Object label = serviceReference.getProperty("label");
+//
+//    				if (label != null) {
+//    					properties.add(label.toString());
+//    				} else {
+//    					properties.add("null");
+//    				}
+//
+//    				bundleNamesToProperties.put(symbolicName, properties);
+//
+//    				bundleHeaders.add(bundle.getHeaders());
+//    			}
+//    		}
+//
+//    		File reportFile = FileUtilities.createTemporaryFileInDefaultTemporaryDirectory(
+//    			"algorithm_metadata_report", TEXT_FILE_EXTENSION);
+//    		reportWriter = new FileWriter(reportFile);
+//    		File bundleNameFile = FileUtilities.createTemporaryFileInDefaultTemporaryDirectory(
+//    			"pluginNames", CSV_FILE_EXTENSION);
+//    		bundleNameWriter = new FileWriter(bundleNameFile);
+//
+//    		bundleNameWriter.append("Plugin Name,Algorithm PID,Label\n");
+//
+//    		for (Iterator<String> keys = symbolicNamesToReport.keySet().iterator();
+//    				keys.hasNext();) {
+//    			String key = keys.next();
+//
+//    			for (String value : symbolicNamesToReport.get(key)) {
+//    				reportWriter.append(value);
+//    			}
+//
+//   				bundleNameWriter.append(
+//    				key + "," +
+//    				StringUtilities.implodeList(bundleNamesToProperties.get(key), ",") +
+//    				"\n");
+//    		}
+//
+//    		reportWriter.close();
+//    		bundleNameWriter.close();
+//
+//    		File headersFile = FileUtilities.createTemporaryFileInDefaultTemporaryDirectory(
+//    			"bundle_headers", TEXT_FILE_EXTENSION);
+//    		headersWriter = new FileWriter(headersFile);
+//
+//    		for (Dictionary headers : bundleHeaders) {
+//    			headersWriter.append(headers.toString() + ":\n");
+//
+//    			for (Enumeration keys = headers.keys(); keys.hasMoreElements();) {
+//    				String key = keys.nextElement().toString();
+//    				headersWriter.append("\t" + key + ": \"" + headers.get(key) + "\"\n");
+//    			}
+//
+//    			headersWriter.append("\n");
+//    		}
+//
+//    		headersWriter.close();
+//
+//    		
+////
+////    		// TODO Sort by symbolic name first.
+////    		for (ServiceReference serviceReference : serviceReferences) {
+////    			String pluginString = createPluginString(serviceReference);
+////    			reportWriter.append(pluginString);
+////    		}
+//
+//    		return formOutData(reportFile, headersFile, bundleNameFile);
+//    	} catch (InvalidSyntaxException e) {
+//    		String exceptionMessage = "No one should ever see this: \"" + e.getMessage() + "\"";
+//    		throw new AlgorithmExecutionException(exceptionMessage, e);
+//    	} catch (IOException e) {
+//    		String exceptionMessage =
+//    			"An error occurred when trying to write to the algorithm metadata report " +
+//    			"to a temporary file: \"" + e.getMessage() + "\"";
+//    		throw new AlgorithmExecutionException(exceptionMessage, e);
+//    	} finally {
+//    		if (reportWriter != null) {
+//    			try {
+//    				reportWriter.close();
+//    			} catch (IOException e) {
+//    				String exceptionMessage =
+//    					"An error occurred when trying to close the algorithm metadata report " +
+//    					"file: \"" + e.getMessage() + "\"";
+//    				throw new AlgorithmExecutionException(exceptionMessage, e);
+//    			}
+//    		}
+//
+//    		if (headersWriter != null) {
+//    			try {
+//    				headersWriter.close();
+//    			} catch (IOException e) {
+//    				String exceptionMessage =
+//    					"An error occurred when trying to close the bundle headers file: \"" +
+//    					e.getMessage() + "\"";
+//    				throw new AlgorithmExecutionException(exceptionMessage, e);
+//    			}
+//    		}
+//
+//			if (bundleNameWriter != null) {
+//    			try {
+//    				bundleNameWriter.close();
+//    			} catch (IOException e) {
+//    				String exceptionMessage =
+//    					"An error occurred when trying to close the plugin names file: \"" +
+//    					e.getMessage() + "\"";
+//    				throw new AlgorithmExecutionException(exceptionMessage, e);
+//    			}
+//    		}
+//    	}
+//    }
 
-    private static String createPluginString(ServiceReference serviceReference) {
-		String header = createPluginHeader(serviceReference);
-		String values = createPluginValuesString(serviceReference);
+//    private static String createPluginString(ServiceReference serviceReference) {
+//		String header = createPluginHeader(serviceReference);
+//		String values = createPluginValuesString(serviceReference);
+//
+//		return header + values + "\r\n";
+//    }
 
-		return header + values + "\r\n";
-    }
+//    private static Data[] formOutData(File reportFile, File headersFile, File bundleNameFile) {
+//		Data reportFileData = new BasicData(reportFile, TEXT_MIME_TYPE);
+//		Dictionary<String, Object> reportFileMetadata = reportFileData.getMetadata();
+//		reportFileMetadata.put(DataProperty.LABEL, "Algorithm Metadata Report");
+//		reportFileMetadata.put(DataProperty.TYPE, DataProperty.TEXT_TYPE);
+//
+//		Data headersFileData = new BasicData(headersFile, TEXT_MIME_TYPE);
+//		Dictionary<String, Object> headersFileMetadata = headersFileData.getMetadata();
+//		headersFileMetadata.put(DataProperty.LABEL, "Bundle Headers Report");
+//		headersFileMetadata.put(DataProperty.TYPE, DataProperty.TEXT_TYPE);
+//
+//		Data bundleNameFileData = new BasicData(bundleNameFile, CSV_MIME_TYPE);
+//		Dictionary<String, Object> bundleNameFileMetadata = bundleNameFileData.getMetadata();
+//		bundleNameFileMetadata.put(DataProperty.LABEL, "Bundle Names Report");
+//		bundleNameFileMetadata.put(DataProperty.TYPE, DataProperty.TABLE_TYPE);
+//    	
+//        return new Data[] { reportFileData, headersFileData, bundleNameFileData };
+//    }
 
-    private static Data[] formOutData(File reportFile, File headersFile, File bundleNameFile) {
-		Data reportFileData = new BasicData(reportFile, TEXT_MIME_TYPE);
+    private static Data[] formOutData(File bundleNameFile) {
+		Data reportFileData = new BasicData(bundleNameFile, CSV_MIME_TYPE);
 		Dictionary<String, Object> reportFileMetadata = reportFileData.getMetadata();
 		reportFileMetadata.put(DataProperty.LABEL, "Algorithm Metadata Report");
-		reportFileMetadata.put(DataProperty.TYPE, DataProperty.TEXT_TYPE);
-
-		Data headersFileData = new BasicData(headersFile, TEXT_MIME_TYPE);
-		Dictionary<String, Object> headersFileMetadata = headersFileData.getMetadata();
-		headersFileMetadata.put(DataProperty.LABEL, "Bundle Headers Report");
-		headersFileMetadata.put(DataProperty.TYPE, DataProperty.TEXT_TYPE);
-
-		Data bundleNameFileData = new BasicData(bundleNameFile, CSV_MIME_TYPE);
-		Dictionary<String, Object> bundleNameFileMetadata = bundleNameFileData.getMetadata();
-		bundleNameFileMetadata.put(DataProperty.LABEL, "Bundle Names Report");
-		bundleNameFileMetadata.put(DataProperty.TYPE, DataProperty.TABLE_TYPE);
+		reportFileMetadata.put(DataProperty.TYPE, DataProperty.TABLE_TYPE);
     	
-        return new Data[] { reportFileData, headersFileData, bundleNameFileData };
+        return new Data[] { reportFileData };
     }
 
-    private static String createPluginHeader(ServiceReference serviceReference) {
-    	String pluginHeader = serviceReference.getBundle().getSymbolicName() + ":\r\n";
+//    private static String createPluginHeader(ServiceReference serviceReference) {
+//    	String pluginHeader = serviceReference.getBundle().getSymbolicName() + ":\r\n";
+//
+//    	return pluginHeader;
+//    }
+//
+//    private static String createPluginValuesString(ServiceReference serviceReference) {
+//    	StringBuilder pluginValues = new StringBuilder();
+//    	
+//    	List<String> keys = Arrays.asList(serviceReference.getPropertyKeys());
+//    	Collections.sort(keys);
+//    	
+//    	for (String key : keys) {
+//    		String keyValueString = createPluginValueString(serviceReference, key);
+//    		pluginValues.append(keyValueString);
+//    	}
+//
+//    	return pluginValues.toString();
+//    }
 
-    	return pluginHeader;
-    }
-
-    private static String createPluginValuesString(ServiceReference serviceReference) {
-    	StringBuilder pluginValues = new StringBuilder();
-    	
-    	List<String> keys = Arrays.asList(serviceReference.getPropertyKeys());
-    	Collections.sort(keys);
-    	
-    	for (String key : keys) {
-    		String keyValueString = createPluginValueString(serviceReference, key);
-    		pluginValues.append(keyValueString);
-    	}
-
-    	return pluginValues.toString();
-    }
-
-    private static String createPluginValueString(ServiceReference serviceReference, String key) {
-    	if (!SERVICE_PROPERTY_KEYS_TO_IGNORE.contains(key)) {
-    		Object value = serviceReference.getProperty(key);
-
-    		if (value != null) {
-    			return "\t" + key + ": " + value.toString() + "\r\n";
-    		} else {
-    			return "";
-    		}
-    	} else {
-    		return "";
-    	}
-    }
+//    private static String createPluginValueString(ServiceReference serviceReference, String key) {
+//    	if (!SERVICE_PROPERTY_KEYS_TO_IGNORE.contains(key)) {
+//    		Object value = serviceReference.getProperty(key);
+//
+//    		if (value != null) {
+//    			return "\t" + key + ": " + value.toString() + "\r\n";
+//    		} else {
+//    			return "";
+//    		}
+//    	} else {
+//    		return "";
+//    	}
+//    }
 }
