b251308cf8df182d2beab3fa22e83bd2a0635ac3
Patrick Phillips <pataphil@indiana.edu>
2011-01-19 16:30:30 +0000

    * Now prints the schemas of prefuse.data.Table and (node and edge tables of) prefuse.data.Graph.

--- trunk/cns/edu.iu.cns.inspectdata/src/edu/iu/cns/inspectdata/DataInspectorAlgorithm.java
+++ trunk/cns/edu.iu.cns.inspectdata/src/edu/iu/cns/inspectdata/DataInspectorAlgorithm.java
@@ -1,12 +1,19 @@
 package edu.iu.cns.inspectdata;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Enumeration;
+import java.util.List;
 
 import org.cishell.framework.algorithm.Algorithm;
 import org.cishell.framework.data.Data;
+import org.cishell.utilities.StringUtilities;
 import org.osgi.service.log.LogService;
+
+import prefuse.data.Graph;
+import prefuse.data.Table;
+import prefuse.data.column.Column;
 
 public class DataInspectorAlgorithm implements Algorithm {
     private Data data;
@@ -18,16 +25,29 @@ public class DataInspectorAlgorithm implements Algorithm {
     }
 
     public Data[] execute() {
+    	Object dataContent = this.data.getData();
     	this.logger.log(
     		LogService.LOG_INFO,
-    		String.format("Data item '%s' (%s):", this.data.toString(), this.data.getFormat()));
-    	Object dataContent = this.data.getData();
+    		String.format(
+    			"Data item '%s' (%s)(%s):",
+    			this.data.toString(),
+    			this.data.getFormat(),
+    			dataContent.getClass().getName()));
 
     	if (dataContent instanceof File) {
     		File dataFile = (File) dataContent;
     		this.logger.log(
     			LogService.LOG_INFO,
     			String.format("\tIs a file (absolute path: '%s')", dataFile.getAbsolutePath()));
+    	} else if (dataContent instanceof Table) {
+    		Table dataTable = (Table) dataContent;
+    		printTable(dataTable, "Table");
+    	} else if (dataContent instanceof Graph) {
+    		Graph dataGraph = (Graph) dataContent;
+    		Table nodeTable = dataGraph.getNodeTable();
+    		Table edgeTable = dataGraph.getEdgeTable();
+    		printTable(nodeTable, "Node");
+    		printTable(edgeTable, "Edge");
     	}
 
     	Dictionary<String, Object> metadata = this.data.getMetadata();
@@ -41,5 +61,20 @@ public class DataInspectorAlgorithm implements Algorithm {
     	}
 
         return null;
+    }
+
+    private void printTable(Table table, String tableType) {
+		List<String> schema = new ArrayList<String>();
+
+		for (int ii = 0; ii < table.getSchema().getColumnCount(); ii++) {
+			String name = table.getSchema().getColumnName(ii);
+			String type = table.getColumn(ii).getClass().getName();
+			String schemaEntry = String.format("%s: %s", name, type);
+			schema.add(schemaEntry);
+		}
+
+		String logMessage =
+			String.format("%s Schema: %s", tableType, StringUtilities.implodeItems(schema, ", "));
+		this.logger.log(LogService.LOG_INFO, logMessage);
     }
 }
