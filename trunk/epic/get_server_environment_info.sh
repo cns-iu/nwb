63ed3cc43c5e06d20bd1060589563ab39fa1373a
Chintan Tank <cdtank@indiana.edu>
2011-06-23 18:35:17 +0000

    1. Modified the script for getting info since the last approach was incorrect.

--- trunk/epic/get_server_environment_info.sh
+++ trunk/epic/get_server_environment_info.sh
@@ -2,9 +2,7 @@
 
 echo "cd /home/epic_website/epic_code/epic" > remote.sh
 echo "export PYTHONPATH=/home/epic_website:/home/epic_website/epic_code" >> remote.sh
-echo "python2.6 manage.py shell --settings=$EPIC_SETTINGS" >> remote.sh
-echo "import haystack" >> remote.sh
-echo "print haystack.__path__" >> remote.sh
+echo "python2.6 manage.py shell --settings=$EPIC_SETTINGS < get_environment_info.py" >> remote.sh
 
 scp remote.sh $EPIC_HOST:/tmp/remote.sh
 ssh $EPIC_HOST "chmod +x /tmp/remote.sh"
