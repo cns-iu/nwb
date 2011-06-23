60c659a0f1a35e0be6c56d99d42c1ab30a1192d9
Chintan Tank <cdtank@indiana.edu>
2011-06-23 18:47:16 +0000

    1. More changes to the "get environment info" script.

--- trunk/epic/get_server_environment_info.sh
+++ trunk/epic/get_server_environment_info.sh
@@ -2,6 +2,7 @@
 
 echo "cd /home/epic_website/epic_code/epic" > remote.sh
 echo "export PYTHONPATH=/home/epic_website:/home/epic_website/epic_code" >> remote.sh
+echo "ls -la" >> remote.sh
 echo "python2.6 manage.py shell --settings=$EPIC_SETTINGS < get_environment_info.py" >> remote.sh
 
 scp remote.sh $EPIC_HOST:/tmp/remote.sh
