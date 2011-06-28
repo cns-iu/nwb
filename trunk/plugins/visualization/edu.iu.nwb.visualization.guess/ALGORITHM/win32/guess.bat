60192b4d1389298cd396a0fc811bd82844db4e0a
Joseph Biberstine <jrbibers@indiana.edu>
2011-06-28 18:05:23 +0000

    http://cns-jira.slis.indiana.edu/browse/EPIC-58
    This slightly cleaner packaging is an update to revision 5414.
    
    "EpiCCompartmentColoring.py" now always runs on GUESS load.  When appropriate, it applies compartmental coloring.  In any case, it introduces several interpreter-ready functions for enumerative coloring and animation thereof.  Key functions include animate, showTimestep, listTimestepsInAnimation, colorNodesByState, makeColorSchemeForNodeColumn, makeColorSchemeForAnimation, detectAnimationNames, and readStatesInAnimation.
    
    Reviewed by Micah.

--- trunk/plugins/visualization/edu.iu.nwb.visualization.guess/ALGORITHM/win32/guess.bat
+++ trunk/plugins/visualization/edu.iu.nwb.visualization.guess/ALGORITHM/win32/guess.bat
@@ -13,4 +13,4 @@ echo GUESS log file for this session can be found in %GUESS_HOME%\guesslog.txt
 
 set GUESS_LIB=%GUESS_HOME%\lib
 set GCLASSPATH=guess.jar;%GUESS_LIB%\guess.jar;%GUESS_LIB%\piccolo.jar;%GUESS_LIB%\piccolox.jar;%GUESS_LIB%\jung.jar;%GUESS_LIB%\commons-collections.jar;%GUESS_LIB%\hsqldb.jar;%GUESS_LIB%\freehep-all.jar;%GUESS_LIB%\colt.jar;%GUESS_LIB%\prefuse.jar;%GUESS_LIB%\TGGraphLayout.jar;%GUESS_LIB%\looks.jar;%GUESS_LIB%\mascoptLib.jar;%GUESS_LIB%\jfreechart.jar;%GUESS_LIB%\jide-components.jar;%GUESS_LIB%\jide-common.jar;%GUESS_LIB%\forms.jar;%GUESS_LIB%\jcommon.jar;%GUESS_LIB%\opencsv-2.2.jar
-java -Xms256m -Xmx1000m -classpath "%GCLASSPATH%" -Dsun.java2d.opengl=False "-Dpython.home=%GUESS_HOME%\src" -DgHome="%GUESS_HOME%" -Ddata_label=%8 com.hp.hpl.guess.Guess %1 %2 %3 %4 %5 %6 %7 1> guesslog.txt 2>&1
+java -Xms256m -Xmx1000m -classpath "%GCLASSPATH%" -Dsun.java2d.opengl=False "-Dpython.home=%GUESS_HOME%\src" -DgHome="%GUESS_HOME%" -Ddata_label=%1 com.hp.hpl.guess.Guess %2 %3 %4 %5 %6 %7 %8 %9 1> guesslog.txt 2>&1
