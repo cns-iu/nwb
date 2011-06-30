0da05a0f82c22ad1ff11dfbf82366aa4684b538e
Chintan Tank <cdtank@indiana.edu>
2011-06-30 20:02:35 +0000

    1. Modifying the dev wipe_demo sh for importing categories.

--- trunk/epic/wipe_for_demo.sh
+++ trunk/epic/wipe_for_demo.sh
@@ -6,6 +6,7 @@ echo "python2.6 manage.py shell --settings=$EPIC_SETTINGS < wipeout.py" >> remot
 echo "python2.6 manage.py syncdb --all --noinput --settings=$EPIC_SETTINGS " >> remote.sh
 echo "python2.6 manage.py migrate --fake --settings=$EPIC_SETTINGS " >> remote.sh
 echo "python2.6 manage.py loaddata demo_data_2 --settings=$EPIC_SETTINGS " >> remote.sh
+echo "python2.6 manage.py loaddata preset_categories.json --settings=$EPIC_SETTINGS " >> remote.sh
 echo "echo 'y' | python2.6 manage.py rebuild_index --settings=$EPIC_SETTINGS " >> remote.sh
 echo "chmod -R 777 /tmp/whoosh" >> remote.sh
 echo "rm -rf /home/epic_website/epic_data/*" >> remote.sh
