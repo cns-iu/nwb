12e5cc589bba30c74cd968a4653d9fe883e132b2
Joseph Biberstine <jrbibers@indiana.edu>
2011-07-01 22:10:24 +0000

    Shouldn't these "name" arguments be given with underscores, as elsewhere?  Testing.

--- trunk/epic/categories/urls.py
+++ trunk/epic/categories/urls.py
@@ -6,17 +6,17 @@ urlpatterns = patterns('epic.categories.views',
     url(
         r'^(?P<category_id>[\w-]+)/$',
         'view_items_for_category',
-        name='view-items-for-category'),
+        name='view_items_for_category'),
     url(
         r'^(?P<category_id>[\w-]+)/datarequests/$',
         'view_datarequests_for_category',
-        name='view-datarequests-for-category'),
+        name='view_datarequests_for_category'),
     url(
         r'^(?P<category_id>[\w-]+)/datasets/$',
         'view_datasets_for_category',
-        name='view-datasets-for-category'),
+        name='view_datasets_for_category'),
     url(
         r'^(?P<category_id>[\w-]+)/projects/$',
         'view_projects_for_category',
-        name='view-projects-for-category'),
+        name='view_projects_for_category'),
 )
