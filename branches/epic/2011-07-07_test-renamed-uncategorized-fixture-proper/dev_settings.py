90cbd9a81b722a0c4ea2bd075496715000e4ee03
Joseph Biberstine <jrbibers@indiana.edu>
2011-07-07 18:45:07 +0000

    Testing whether the "Deploy to staging server" job is failing due to multiple "Uncategorized" categories.

---
+++ branches/epic/2011-07-07_test-renamed-uncategorized-fixture-proper/dev_settings.py
@@ -0,0 +1,13 @@
+from base_settings import *
+import log_settings
+
+
+from deploy_settings import *
+
+
+GOOGLE_KEY = \
+    'ABQIAAAAGfs37W0paJHSAkG_6nFTJRR4-7fY0mUWhLoroRlYL9BhgFA-nBSB1J6vu5W15LpVZgge0zFYJ18CEg'
+
+DEBUG = True
+
+log_settings.safeSetup('/home/epic_website/epic_code/epic/dev_stage_logging.conf')
