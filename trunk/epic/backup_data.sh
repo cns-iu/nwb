995bfa98368f4b952b696d08bbe796f443c111e4
Joseph Biberstine <jrbibers@indiana.edu>
2011-07-12 20:28:47 +0000

    Previously backup_data.sh overwrote the existing backup with each new backup.  Now we keep all old backups.  Each is identified by the date and time when the script ran.
    Reviewed by Chintan.

--- trunk/epic/backup_data.sh
+++ trunk/epic/backup_data.sh
@@ -1,2 +1,9 @@
-pg_dump -h cns-dbp -U epic_dumper --schema=public -Fc epic_web > epic_database.archive
-rsync -avz apache@epic:/home/epic_website/epic_data epic_data_backup
+time=$(date "+%F-%H-%M-%S")
+
+# Dump production database to an archive file
+pg_dump -h cns-dbp -U epic_dumper --schema=public -Fc epic_web > epic_database.$time.archive
+
+# Dump production data to an archive directory
+destination_path=epic_data_backup/epic_data_$time
+mkdir -p $destination_path
+rsync -avz apache@epic:/home/epic_website/epic_data/ $destination_path
