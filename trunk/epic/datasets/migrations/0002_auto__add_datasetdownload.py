8591233373f06a8935198d04e791541b37ebaca2
Joseph Biberstine <jrbibers@indiana.edu>
2011-07-13 23:32:44 +0000

    Fixed migration dependency typo.

--- trunk/epic/datasets/migrations/0002_auto__add_datasetdownload.py
+++ trunk/epic/datasets/migrations/0002_auto__add_datasetdownload.py
@@ -6,9 +6,9 @@ from django.db import models
 
 class Migration(SchemaMigration):
     
-    depends_on = {
+    depends_on = (
         ('projects', '0002_auto__add_projectdownload')
-    }
+    )
     
     def forwards(self, orm):
         
