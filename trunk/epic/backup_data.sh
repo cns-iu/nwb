ea3a235467e2e017367b0d78ac869df2669cf421
Joseph Biberstine <jrbibers@indiana.edu>
2011-07-13 23:11:19 +0000

    epic_data was not being sent to $BACKUP_PATH

--- trunk/epic/backup_data.sh
+++ trunk/epic/backup_data.sh
@@ -7,6 +7,6 @@ DATABASE_ARCHIVE_FILE_PATH=$BACKUP_PATH/epic_database.$CURRENT_DATETIME.archive
 pg_dump -h cns-dbp -U epic_dumper --schema=public -Fc epic_web > $DATABASE_ARCHIVE_FILE_PATH
 
 # Dump production data to an archive directory
-DATA_ARCHIVE_PATH=epic_data_backup/epic_data_$CURRENT_DATETIME
+DATA_ARCHIVE_PATH=$BACKUP_PATH/epic_data_$CURRENT_DATETIME
 mkdir -p $DATA_ARCHIVE_PATH
 rsync -avz apache@epic:/home/epic_website/epic_data/ $DATA_ARCHIVE_PATH
