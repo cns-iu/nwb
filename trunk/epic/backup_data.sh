bc10577534e12b533579c6f52e53618d67ead91a
Joseph Biberstine <jrbibers@indiana.edu>
2011-07-13 22:56:23 +0000

    backup_data.sh now creates a new database dump file and a new epic_data folder, both datetime stamped, on each run.

--- trunk/epic/backup_data.sh
+++ trunk/epic/backup_data.sh
@@ -1,2 +1,10 @@
-pg_dump -h cns-dbp -U epic_dumper --schema=public -Fc epic_web > epic_database.archive
-rsync -avz apache@epic:/home/epic_website/epic_data epic_data_backup
+CURRENT_DATETIME=$(date "+%F-%H-%M-%S")
+
+# Dump production database to an archive file
+DATABASE_ARCHIVE_FILE_PATH=$BACKUP_PATH/epic_database.$CURRENT_DATETIME.archive
+pg_dump -h cns-dbp -U epic_dumper --schema=public -Fc epic_web > $DATABASE_ARCHIVE_FILE_PATH
+
+# Dump production data to an archive directory
+DATA_ARCHIVE_PATH=epic_data_backup/epic_data_$CURRENT_DATETIME
+mkdir -p $DATA_ARCHIVE_PATH
+rsync -avz apache@epic:/home/epic_website/epic_data/ $DATA_ARCHIVE_PATH
