8dd946f19f77e8933d6d0a4fc1f442ed581a2300
Joseph Biberstine <jrbibers@indiana.edu>
2011-07-13 23:37:08 +0000

    Fixed another migration dependency typo.

---
+++ tags/epic/2011-07-13_fixed-another-migration-dependency-typo/backup_data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CURRENT_DATETIME=$(date "+%F-%H-%M-%S")
+
+# Dump production database to an archive file
+DATABASE_ARCHIVE_FILE_PATH=$BACKUP_PATH/epic_database.$CURRENT_DATETIME.archive
+pg_dump -h cns-dbp -U epic_dumper --schema=public -Fc epic_web > $DATABASE_ARCHIVE_FILE_PATH
+
+# Dump production data to an archive directory
+DATA_ARCHIVE_PATH=$BACKUP_PATH/epic_data_$CURRENT_DATETIME
+mkdir -p $DATA_ARCHIVE_PATH
+rsync -avz apache@epic:/home/epic_website/epic_data/ $DATA_ARCHIVE_PATH
