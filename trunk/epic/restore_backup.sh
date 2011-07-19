b1adb596289c680650984b17cbafeda3ed2d6345
Joseph Biberstine <jrbibers@indiana.edu>
2011-07-19 21:29:13 +0000

    Testing restore_backup.sh: dialing up the verbosity.

--- trunk/epic/restore_backup.sh
+++ trunk/epic/restore_backup.sh
@@ -33,40 +33,47 @@ execute_on_host() {
 
 # Unpack the database archive (already put on the target host), wipe the current database, and load the archive
 restore_database_backup() {
-	echo "# Unpack the database archive from the backup"                                                    > restore_database.sh
-	echo "DATABASE_ARCHIVE=epic_database_$BACKUP_DATETIME.archive"                                         >> restore_database.sh
-	echo "tar -xzf /tmp/$BACKUP_FILENAME \$DATABASE_ARCHIVE"                                               >> restore_database.sh
-	
-	echo "# Wipe current database using django.db commands"                                                >> restore_database.sh
-	echo "cd /home/epic_website/epic_code/epic"                                                            >> restore_database.sh
-	echo "export PYTHONPATH=/home/epic_website:/home/epic_website/epic_code"                               >> restore_database.sh
-	# WARNING: Notice that wipeout.py will be whichever revision is currently deployed to EPIC_HOST
+	local SCRIPT=restore_database.sh
+
+	# Notice that wipeout.py will be whichever revision is currently deployed to EPIC_HOST
 	# TODO Put warnings in wipeout.py itself, too?
-	echo "python2.6 manage.py shell --settings=$EPIC_SETTINGS < wipeout.py"                                >> restore_database.sh
+	echo "echo Unpacking the database archive from the backup.."                                            > $SCRIPT
+	echo "DATABASE_ARCHIVE=epic_database_$BACKUP_DATETIME.archive"                                         >> $SCRIPT
+	echo "tar -xzf /tmp/$BACKUP_FILENAME \$DATABASE_ARCHIVE"                                               >> $SCRIPT
+	echo "echo   Done."                                                                                    >> $SCRIPT	
+	echo "echo Wiping current database using django.db commands.."                                         >> $SCRIPT
+	echo "cd /home/epic_website/epic_code/epic"                                                            >> $SCRIPT
+	echo "export PYTHONPATH=/home/epic_website:/home/epic_website/epic_code"                               >> $SCRIPT
+	echo "python2.6 manage.py shell --settings=$EPIC_SETTINGS < wipeout.py"                                >> $SCRIPT
+	echo "echo   Done."                                                                                    >> $SCRIPT	
+	echo "echo Loading database backup.."                                                                  >> $SCRIPT
+	echo "cd"                                                                                              >> $SCRIPT
+	echo "pg_restore --no-acl --no-owner -h $DATABASE_HOST -U epic_appuser -d epic_web \$DATABASE_ARCHIVE" >> $SCRIPT
+	echo "echo   Done."                                                                                    >> $SCRIPT
 	
-	echo "# Load database backup"                                                                          >> restore_database.sh
-	echo "cd"                                                                                              >> restore_database.sh
-	echo "pg_restore --no-acl --no-owner -h $DATABASE_HOST -U epic_appuser -d epic_web \$DATABASE_ARCHIVE" >> restore_database.sh
-	
-	execute_on_host restore_database.sh apache $EPIC_HOST
+	execute_on_host $SCRIPT apache $EPIC_HOST
 }
 
 # Unpack the data folder archive (already put on the target host), wipe the current data folder, and load the archive
 restore_data_folder_backup() {
-	echo "# Unpack data folder archive from the backup"              > restore_data_folder.sh
-	echo "DATA_FOLDER_NAME=epic_data_$BACKUP_DATETIME"              >> restore_data_folder.sh
-	echo "DATA_FOLDER_ARCHIVE=\$DATA_FOLDER_NAME.tgz"               >> restore_data_folder.sh
-	echo "tar -xzf /tmp/$BACKUP_FILENAME \$DATA_FOLDER_ARCHIVE"     >> restore_data_folder.sh
+	local SCRIPT=restore_data_folder.sh
 
-	echo "# Wipe current data folder"                               >> restore_data_folder.sh
-	echo "rm -rf /home/epic_website/epic_data/*"                    >> restore_data_folder.sh
+	echo "echo Unpacking data folder archive from the backup.."      > $SCRIPT
+	echo "DATA_FOLDER_NAME=epic_data_$BACKUP_DATETIME"              >> $SCRIPT
+	echo "DATA_FOLDER_ARCHIVE=\$DATA_FOLDER_NAME.tgz"               >> $SCRIPT
+	echo "tar -xzf /tmp/$BACKUP_FILENAME \$DATA_FOLDER_ARCHIVE"     >> $SCRIPT
+	echo "echo   Done."                                             >> $SCRIPT
+	echo "echo Wiping current data folder.."                        >> $SCRIPT
+	echo "rm -rf /home/epic_website/epic_data/*"                    >> $SCRIPT	
+	echo "echo   Done."                                             >> $SCRIPT
+	echo "echo Loading data folder backup.."                        >> $SCRIPT
+	echo "tar -xzf \$DATA_FOLDER_ARCHIVE"                           >> $SCRIPT
+	echo "cp -R \$DATA_FOLDER_NAME/* /home/epic_website/epic_data/" >> $SCRIPT
+	echo "echo   Done."                                             >> $SCRIPT
 	
-	echo "# Load data folder backup"                                >> restore_data_folder.sh
-	echo "tar -xzf \$DATA_FOLDER_ARCHIVE"                           >> restore_data_folder.sh
-	echo "cp -R \$DATA_FOLDER_NAME/* /home/epic_website/epic_data/" >> restore_data_folder.sh
-	
-	execute_on_host restore_data_folder.sh apache $EPIC_HOST
+	execute_on_host $SCRIPT apache $EPIC_HOST
 }
+
 
 if [ ! -e "$BACKUP_FILE_PATH" ]; then
 	echo "Restore stopped: backup file '$BACKUP_FILE_PATH' does not exist."
