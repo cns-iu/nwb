d97dea954af77c14f06d165cd97cf5703adc67ec
Joseph Biberstine <jrbibers@indiana.edu>
2011-07-21 21:23:02 +0000

    wipe.sh: should syncdb and migrate --fake after the db wipe, as wipe_for_demo.sh does.

--- trunk/epic/wipe.sh
+++ trunk/epic/wipe.sh
@@ -13,14 +13,16 @@ else
 	exit
 fi
 
-echo "cd /home/epic_website/epic_code/epic"                               > remote.sh
-echo "export PYTHONPATH=/home/epic_website:/home/epic_website/epic_code" >> remote.sh
-echo "echo Wiping database.."                                            >> remote.sh
-echo "python2.6 manage.py shell --settings=$EPIC_SETTINGS < wipeout.py"  >> remote.sh
-echo "echo Done."                                                        >> remote.sh
-echo "echo Wiping dataset files.."                                       >> remote.sh
-echo "rm -rf /home/epic_website/epic_data/*"                             >> remote.sh
-echo "echo Done."                                                        >> remote.sh
+echo "cd /home/epic_website/epic_code/epic"                                   > remote.sh
+echo "export PYTHONPATH=/home/epic_website:/home/epic_website/epic_code"     >> remote.sh
+echo "echo Wiping database.."                                                >> remote.sh
+echo "python2.6 manage.py shell --settings=$EPIC_SETTINGS < wipeout.py"      >> remote.sh
+echo "python2.6 manage.py syncdb --all --noinput --settings=$EPIC_SETTINGS " >> remote.sh
+echo "python2.6 manage.py migrate --fake --settings=$EPIC_SETTINGS "         >> remote.sh
+echo "echo Done."                                                            >> remote.sh
+echo "echo Wiping dataset files.."                                           >> remote.sh
+echo "rm -rf /home/epic_website/epic_data/*"                                 >> remote.sh
+echo "echo Done."                                                            >> remote.sh
 
 scp remote.sh apache@$EPIC_HOST:/tmp/remote.sh
 ssh apache@$EPIC_HOST "chmod +x /tmp/remote.sh"
