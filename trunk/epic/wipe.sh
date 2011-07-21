a32f15ecff7ba380d4a8a2afc2576c5e71fdd029
Joseph Biberstine <jrbibers@indiana.edu>
2011-07-21 22:29:58 +0000

    Trying to tell South that the first core migration depends on the first categories migration.

--- trunk/epic/wipe.sh
+++ trunk/epic/wipe.sh
@@ -17,7 +17,7 @@ echo "cd /home/epic_website/epic_code/epic"                               > remo
 echo "export PYTHONPATH=/home/epic_website:/home/epic_website/epic_code" >> remote.sh
 echo "echo Wiping database.."                                            >> remote.sh
 echo "python2.6 manage.py shell --settings=$EPIC_SETTINGS < wipeout.py"  >> remote.sh
-echo "python2.6 manage.py syncdb --noinput --settings=$EPIC_SETTINGS "   >> remote.sh
+echo "python2.6 manage.py syncdb --noinput --settings=$EPIC_SETTINGS"    >> remote.sh
 echo "python2.6 manage.py migrate --settings=$EPIC_SETTINGS "            >> remote.sh
 echo "echo Done."                                                        >> remote.sh
 echo "echo Wiping dataset files.."                                       >> remote.sh
