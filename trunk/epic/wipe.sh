ef41bd43ac39381b883250da228b1266793a321b
Joseph Biberstine <jrbibers@indiana.edu>
2011-07-21 22:36:55 +0000

    That didn't seem to work.  Trying instead to migrate apps individually.

--- trunk/epic/wipe.sh
+++ trunk/epic/wipe.sh
@@ -18,7 +18,17 @@ echo "export PYTHONPATH=/home/epic_website:/home/epic_website/epic_code" >> remo
 echo "echo Wiping database.."                                            >> remote.sh
 echo "python2.6 manage.py shell --settings=$EPIC_SETTINGS < wipeout.py"  >> remote.sh
 echo "python2.6 manage.py syncdb --noinput --settings=$EPIC_SETTINGS"    >> remote.sh
-echo "python2.6 manage.py migrate --settings=$EPIC_SETTINGS "            >> remote.sh
+echo "python2.6 manage.py migrate epic.core --settings=$EPIC_SETTINGS " >> remote.sh
+echo "python2.6 manage.py migrate epic.categories --settings=$EPIC_SETTINGS " >> remote.sh
+echo "python2.6 manage.py migrate epic.datarequests --settings=$EPIC_SETTINGS " >> remote.sh
+echo "python2.6 manage.py migrate epic.projects --settings=$EPIC_SETTINGS " >> remote.sh
+echo "python2.6 manage.py migrate epic.datasets --settings=$EPIC_SETTINGS " >> remote.sh
+echo "python2.6 manage.py migrate epic.comments --settings=$EPIC_SETTINGS " >> remote.sh
+echo "python2.6 manage.py migrate epic.djangoratings --settings=$EPIC_SETTINGS " >> remote.sh
+echo "python2.6 manage.py migrate epic.tags --settings=$EPIC_SETTINGS " >> remote.sh
+echo "python2.6 manage.py migrate epic.messages --settings=$EPIC_SETTINGS " >> remote.sh
+echo "python2.6 manage.py migrate epic.geoloc --settings=$EPIC_SETTINGS " >> remote.sh
+echo "python2.6 manage.py migrate epic.search --settings=$EPIC_SETTINGS " >> remote.sh
 echo "echo Done."                                                        >> remote.sh
 echo "echo Wiping dataset files.."                                       >> remote.sh
 echo "rm -rf /home/epic_website/epic_data/*"                             >> remote.sh
