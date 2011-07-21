5c5beaca02b319ad9e1c07dad51ed3974f41a948
Joseph Biberstine <jrbibers@indiana.edu>
2011-07-21 21:32:40 +0000

    wipe.sh: even better, how about real migrations?

--- trunk/epic/wipe.sh
+++ trunk/epic/wipe.sh
@@ -18,7 +18,7 @@ echo "export PYTHONPATH=/home/epic_website:/home/epic_website/epic_code"     >> 
 echo "echo Wiping database.."                                                >> remote.sh
 echo "python2.6 manage.py shell --settings=$EPIC_SETTINGS < wipeout.py"      >> remote.sh
 echo "python2.6 manage.py syncdb --all --noinput --settings=$EPIC_SETTINGS " >> remote.sh
-echo "python2.6 manage.py migrate --fake --settings=$EPIC_SETTINGS "         >> remote.sh
+echo "python2.6 manage.py migrate --settings=$EPIC_SETTINGS "                >> remote.sh
 echo "echo Done."                                                            >> remote.sh
 echo "echo Wiping dataset files.."                                           >> remote.sh
 echo "rm -rf /home/epic_website/epic_data/*"                                 >> remote.sh
