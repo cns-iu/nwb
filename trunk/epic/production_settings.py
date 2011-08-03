42c9fb5e349973572b80e995763113cb3b2e0fb5
Joseph Biberstine <jrbibers@indiana.edu>
2011-08-03 17:23:08 +0000

    Changing DEBUG to False in the production settings.

--- trunk/epic/production_settings.py
+++ trunk/epic/production_settings.py
@@ -8,7 +8,7 @@ from deploy_settings import *
 GOOGLE_KEY = \
     'ABQIAAAAGfs37W0paJHSAkG_6nFTJRRGxhoRU2jcEM7N9J-RUWDgHfTUZhQJ_PPrSXn376xsMJMdkDQtKnWf_Q'
 
-DEBUG = True
+DEBUG = False
 
 log_settings.safeSetup('/home/epic_website/epic_code/epic/production_logging.conf')
 
