fe263f69e1429139f5c808f1a621c2833ab58bf0
Chintan Tank <cdtank@indiana.edu>
2011-08-19 00:39:03 +0000

    1. not a proper branch folder.

--- branches/epic_tool/stencil_upgrade/src/edu/iu/epic/visualization/linegraph/ActiveAlgorithmHook.java
+++
@@ -1,6 +0,0 @@
-package edu.iu.epic.visualization.linegraph;
-
-public interface ActiveAlgorithmHook {
-	void nowActive(LineGraphAlgorithm algorithm);
-	void nowInactive(LineGraphAlgorithm algorithm);
-}
--- branches/epic_tool/stencil_upgrade/src/edu/iu/epic/visualization/linegraph/utilities/DoubleStartException.java
+++
@@ -1,9 +0,0 @@
-package edu.iu.epic.visualization.linegraph.utilities;
-
-public class DoubleStartException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public DoubleStartException(String message) {
-		super(message);
-	}
-}
