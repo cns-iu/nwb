4b09355123b9584fa4604f5d51d5289434747ff6
David Coe <dmcoe@indiana.edu>
2012-01-11 19:18:12 +0000

    Formerly called 'horizontallinegraph'

---
+++ trunk/sci2/plugins/normal_plugins/edu.iu.sci2.visualization.temporalbargraph/src/edu/iu/sci2/visualization/temporalbargraph/PostScriptCreationException.java
@@ -0,0 +1,21 @@
+package edu.iu.sci2.visualization.temporalbargraph;
+
+public class PostScriptCreationException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public PostScriptCreationException() {
+		super();
+	}
+
+	public PostScriptCreationException(String arg0) {
+		super(arg0);
+	}
+
+	public PostScriptCreationException(Throwable arg0) {
+		super(arg0);
+	}
+
+	public PostScriptCreationException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+}
