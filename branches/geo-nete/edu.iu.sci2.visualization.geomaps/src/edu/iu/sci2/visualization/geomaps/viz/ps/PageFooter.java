eb1a31eb6eb1e8d4731f01abdd05057fb0a16be8
Joseph Biberstine <jrbibers@indiana.edu>
2012-03-12 22:08:25 +0000

    Changing footer to match mockup

--- branches/geo-nete/edu.iu.sci2.visualization.geomaps/src/edu/iu/sci2/visualization/geomaps/viz/ps/PageFooter.java
+++ branches/geo-nete/edu.iu.sci2.visualization.geomaps/src/edu/iu/sci2/visualization/geomaps/viz/ps/PageFooter.java
@@ -3,59 +3,29 @@ package edu.iu.sci2.visualization.geomaps.viz.ps;
 import java.awt.Font;
 import java.awt.geom.Point2D;
 
-import org.antlr.stringtemplate.StringTemplate;
-
-import edu.iu.sci2.visualization.geomaps.GeoMapsAlgorithm;
 import edu.iu.sci2.visualization.geomaps.viz.PageLayout;
 
 public class PageFooter implements PostScriptable {
-	public static final String INDENT = "    ";
-	public static final String ATTRIBUTION_PREFIX =	"Created with ";
-	
-	public static final String CNS_URL = "http://cns.iu.edu";
-	public static final String CNS =
-		"Cyberinfrastructure for Network Science Center";
-	
+	public static final String TEXT =
+			"NIH's Reporter Web site (projectreporter.nih.gov), NETE & CNS (cns.iu.edu)";
 	public static final Font FONT = GeoMapViewPS.CONTENT_FONT.deriveFont(6.0f);
 	public static final double FONT_BRIGHTNESS = 0.35;
 	
-	public static final Point2D.Double LOWER_LEFT =
-			new Point2D.Double(PageLayout.pageMargin(), PageLayout.pageMargin());
+	public static final Point2D.Double LOWER_CENTER =
+			new Point2D.Double(
+					PageLayout.pageMargin() + PageLayout.pageWidth() / 2.0,
+					PageLayout.pageMargin() - FONT.getSize());
 
-	private static final String SEP = " | ";
-	
-	private boolean hasPrintedDefinitions = false;
-	
 	public String toPostScript() {
 		StringBuilder builder = new StringBuilder();
-		
-		if (!hasPrintedDefinitions) {
-			StringTemplate definitionsTemplate =
-				GeoMapsAlgorithm.TEMPLATE_GROUP.getInstanceOf("showToolNameDefinitions");
-			
-			builder.append(definitionsTemplate.toString());
-			
-			this.hasPrintedDefinitions = true;
-		}
-		
+
 		builder.append("gsave" + "\n");
 
 		builder.append(PSUtility.findscalesetfont(FONT) + "\n");
 		builder.append(PSUtility.setgray(FONT_BRIGHTNESS) + "\n");
-		builder.append(INDENT + LOWER_LEFT.x + " " + LOWER_LEFT.y + " moveto" + "\n");
+
+		builder.append(String.format("%f %f (%s) centerShow\n", LOWER_CENTER.x, LOWER_CENTER.y, TEXT));
 		
-		builder.append(INDENT + "(" + ATTRIBUTION_PREFIX + ") show" + "\n");
-		
-		builder.append(INDENT + "currentpoint" + " ");
-		builder.append("{/" + FONT.getName() + " findfont}" + " ");
-		builder.append(FONT_BRIGHTNESS + " ");
-		builder.append(FONT.getSize() + " ");
-		builder.append("showToolName moveto" + "\n");
-		
-		builder.append(INDENT + "(" + SEP + CNS + " " + ")" + " show" + "\n");
-		builder.append(INDENT + "(" + "\\(" + ")" + " show" + "\n");
-		builder.append(INDENT + "(" + CNS_URL + ")" + " underlinedShow" + "\n");
-		builder.append(INDENT + "(" + "\\)" + ")" + " show" + "\n");
 		builder.append("grestore" + "\n");
 		
 		return builder.toString();
