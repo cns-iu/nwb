b43d2e409e1489e46427f4924450957ef57885b3
Joseph Biberstine <jrbibers@indiana.edu>
2012-03-23 19:25:32 +0000

    Unused import.

--- branches/geo-nete/edu.iu.sci2.visualization.geomaps/src/edu/iu/sci2/visualization/geomaps/data/GeoDataset.java
+++ branches/geo-nete/edu.iu.sci2.visualization.geomaps/src/edu/iu/sci2/visualization/geomaps/data/GeoDataset.java
@@ -15,7 +15,6 @@ import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 import com.google.common.collect.Collections2;
 import com.google.common.collect.ImmutableCollection;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Maps;
