32ff94c56b1b50842434240be50c4b6f922a554d
Thomas Smith <thgsmith@indiana.edu>
2012-04-13 15:18:44 +0000

    Test all of the pure-sql database extraction algorithms!  Yay!
    Changed to use a SQL file to insert the data, rather than depending on the ISI loader.

--- trunk/sci2/plugins/unittests/edu.iu.sci2.database.isi.extract.tests/src/edu/iu/sci2/database/isi/extract/tests/DatabaseExtractTest.java
+++ trunk/sci2/plugins/unittests/edu.iu.sci2.database.isi.extract.tests/src/edu/iu/sci2/database/isi/extract/tests/DatabaseExtractTest.java
@@ -1,7 +1,13 @@
 package edu.iu.sci2.database.isi.extract.tests;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Hashtable;
 
 import org.cishell.framework.CIShellContext;
@@ -11,49 +17,96 @@ import org.cishell.framework.algorithm.AlgorithmExecutionException;
 import org.cishell.framework.algorithm.AlgorithmFactory;
 import org.cishell.framework.data.BasicData;
 import org.cishell.framework.data.Data;
+import org.cishell.service.database.Database;
+import org.cishell.service.database.DatabaseService;
 import org.cishell.utilities.AlgorithmUtilities;
 import org.cishell.utilities.FileUtilities;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
 
-
+@RunWith(Parameterized.class)
 public class DatabaseExtractTest {
 	private static Data databaseData;
 	private static BundleContext bundleContext;
 	private static CIShellContext ciContext;
 	
-	@BeforeClass
 	public static void makeDB() throws Exception {
 		bundleContext = Activator.getContext();
 		ciContext = new LocalCIShellContext(bundleContext);
-		String file =
+		File sqlFile =
 				FileUtilities.safeLoadFileFromClasspath(DatabaseExtractTest.class, 
-						"/edu/iu/sci2/database/scopus/load/testdata/BrainCancer.scopus").toString();
+						"/edu/iu/sci2/database/isi/extract/tests/FivePapersISIDb.sql");
+		String dbCreationSQL = FileUtilities.readEntireTextFile(sqlFile);
 		
-		AlgorithmFactory isiLoaderFactory = AlgorithmUtilities.getAlgorithmFactoryByPID(
-				"edu.iu.sci2.database.isi.load.ISIDatabaseLoaderAlgorithm", bundleContext);
-
-		Data inFile = new BasicData(file, "file:text/csv");
+		DatabaseService databaseProvider = (DatabaseService) ciContext.getService(DatabaseService.class.getName());
+		Database database = databaseProvider.createNewDatabase();
+		Connection c = database.getConnection();
+		Statement s = null;
+		String currentStatement = "";
+		try {
+			s = c.createStatement();
+			// we're lucky that the data doesn't contain semicolons :-/
+			for (String statement : Splitter.on(";").trimResults().omitEmptyStrings().split(dbCreationSQL)) {
+				currentStatement = statement;
+				s.execute(statement);
+			}
+		} catch (SQLException e) {
+			throw new Exception("Exception while running " + currentStatement, e);
+		} finally {
+			if (s != null) s.close();
+			c.close();
+		}
 		
-		Algorithm algo = isiLoaderFactory.createAlgorithm(new Data[] {inFile}, new Hashtable<String, Object>(), ciContext);
-		Data[] results = algo.execute();
-		databaseData = results[0];
-//		Database db = (Database) results[0].getData();
-//		connection = db.getConnection();
+		databaseData = new BasicData(database, "db:isi");
+	}
+	
+	// The first release of JUnit after April 9, 2012 will support this:
+	// @Parameters(name="DatabaseExtractTest({0})")
+	// See https://github.com/KentBeck/junit/commit/3a5c9f2731462e36dd1c173ea8840d7b9b34b0ab
+	@Parameters
+	public static Collection<Object[]> data() throws Exception {
+		makeDB();
+		ServiceReference<?>[] refs = bundleContext.getServiceReferences(AlgorithmFactory.class.getName(),
+				"(service.pid=*isi.extract.*)");
+		ArrayList<Object[]> listOfObjArrays = Lists.newArrayList();
+		for (ServiceReference<?> r : refs) {
+			String thisPid = (String) r.getProperty("service.pid");
+			// this is the only one that takes parameters, so skip it for now
+			if (thisPid.contains("weighted_document")) continue;
+			listOfObjArrays.add(new Object[] { thisPid });
+		}
+		return listOfObjArrays;
+	}
+	
+	
+	private String pid;
+	public DatabaseExtractTest(String pid) {
+		this.pid = pid;
 	}
 	
 	@Test
 	public void test() throws Exception {
-		
+		System.out.println(this.toString());
 		AlgorithmFactory fact = AlgorithmUtilities.getAlgorithmFactoryByPID(
-				"edu.iu.sci2.database.isi.extract.network.cocitation.document.core", bundleContext);
+				pid, bundleContext);
 		
 		Algorithm algo = fact.createAlgorithm(new Data[] {databaseData}, new Hashtable<String, Object>(), ciContext);
-		Data[] results = algo.execute();
-		
-		fail("Not yet implemented");
+		try {
+			Data[] results = algo.execute();
+			assertTrue(pid + " didn't generate any Data results", results.length > 0);
+		} catch (AlgorithmExecutionException e) {
+			throw new AssertionError("Error when running " + pid, e);
+		}
 	}
 
+	public String toString() {
+		return String.format("Test of %s", pid);
+	}
 }
