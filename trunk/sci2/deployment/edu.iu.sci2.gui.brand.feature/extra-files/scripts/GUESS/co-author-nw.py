ef7d8e482ff5651aa87de726849e07f5a7d25212
Chin Hua Kong <kongch@indiana.edu>
2012-06-14 16:56:32 +0000

    Fix issue caused by network parameter changes. See 6323 checked in by David. This is the check in for Sci2 deployment

--- trunk/sci2/deployment/edu.iu.sci2.gui.brand.feature/extra-files/scripts/GUESS/co-author-nw.py
+++ trunk/sci2/deployment/edu.iu.sci2.gui.brand.feature/extra-files/scripts/GUESS/co-author-nw.py
@@ -1,13 +1,19 @@
-resizeLinear(numberofworks,1,50)
-colorize(numberofworks,gray,black)
+resizeLinear(number_of_authored_works, 1, 50)
+colorize(number_of_authored_works, gray, black)
 for n in g.nodes:
-	n.strokecolor = n.color 	# border color same as its inside color
-resizeLinear(numberofcoauthoredworks, .25, 8) 
-colorize(numberofcoauthoredworks, "127,193,65,255", black) 
-nodesbynumworks = g.nodes[:] 		# make a copy of the list of all nodes
-def bynumworks(n1, n2): 		# define a function for comparing nodes
-	return cmp(n1.numberofworks, n2.numberofworks) 
-nodesbynumworks.sort(bynumworks) 	# sort list 
-nodesbynumworks.reverse() 		# reverse sorting, list starts with highest #
-for i in range(0, 50): 		# make labels of most productive authors visible 
-	nodesbynumworks[i].labelvisible = true 
+	# border color same as its inside color
+	n.strokecolor = n.color
+resizeLinear(number_of_coauthored_works, .25, 8)
+colorize(number_of_coauthored_works, "127,193,65,255", black)
+# make a copy of the list of all nodes
+nodesbynumworks = g.nodes[:]
+# define a function for comparing nodes	
+def bynumworks(n1, n2):
+	return cmp(n1.number_of_authored_works, n2.number_of_authored_works)
+nodesbynumworks.sort(bynumworks)
+# reverse sorting, list starts with highest number 	 
+nodesbynumworks.reverse()
+# make labels of most productive authors visible	
+for i in range(0, 50):
+	nodesbynumworks[i].labelvisible = true
+
