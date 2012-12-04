ffa3c7a08299746db6fc158dbefa98d5e3abd695
Chin Hua Kong <kongch@indiana.edu>
2012-12-04 02:58:03 +0000

    Initial Check in for Twitter Reader

---
+++ trunk/sci2/plugins/normal_plugins/Twitter Reader/src/edu/iu/sci2/reader/twitter/TwitterReader.java
@@ -0,0 +1,115 @@
+package edu.iu.sci2.reader.twitter;
+
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Iterator;
+import java.util.List;
+
+import org.cishell.framework.CIShellContext;
+import org.cishell.framework.algorithm.Algorithm;
+import org.cishell.framework.algorithm.AlgorithmExecutionException;
+import org.cishell.framework.data.BasicData;
+import org.cishell.framework.data.Data;
+import org.cishell.framework.data.DataProperty;
+import org.osgi.service.log.LogService;
+
+import prefuse.data.Table;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Tweet;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+public class TwitterReader implements Algorithm {
+	private LogService logger;
+    private Data[] data;
+	private String userIDColumn;
+	private String tag;
+    
+    public TwitterReader(Data[] data,
+    				  Dictionary<String, Object> parameters,
+    				  CIShellContext ciShellContext) {
+    	this.logger = (LogService) ciShellContext.getService(LogService.class.getName());
+        this.data = data;
+        this.userIDColumn = parameters.get("uid").toString();
+        this.tag = parameters.get("tag").toString();
+    }
+
+    public Data[] execute() throws AlgorithmExecutionException {
+    	List<String> userIDs = extractUserIDsFromTable();
+    	Table resultTable;
+		try {
+			resultTable = searchTwit(userIDs);
+		} catch (TwitterException e) {
+			throw new AlgorithmExecutionException(
+					"Twitter service or network is unavailable. Try again later", e);
+		}
+	    
+        return generateOutputData(resultTable);
+    }
+    
+    private List<String> extractUserIDsFromTable() {
+    	List<String> userIDs = new ArrayList<String>();
+    	Table inputTable = (Table) data[0].getData();
+        int userIDColumnIndex = inputTable.getColumnNumber(userIDColumn);
+         
+        Iterator<?> rowsIterator = inputTable.iterator();
+        while (rowsIterator.hasNext()) {
+            int currentRowNumber = Integer.parseInt(rowsIterator.next().toString());
+            String userID = inputTable.get(currentRowNumber, userIDColumnIndex).toString();
+            userIDs.add(userID);
+        }
+        
+        return userIDs;
+    }
+    
+    private Table searchTwit(List<String> userIDs) throws TwitterException {
+		//String uIDs = "katycns,xliu12,scott_bot";   //,20456447@N03
+		//i.e. "(from:\"katycns\" OR from:\"xliu12\" OR from:\"scott_bot\") #ivmooc"
+
+		String QueryString = "(";
+		for (String uID: userIDs) {
+			QueryString = QueryString + "from:\"" + uID + "\" OR ";
+		}
+		
+		QueryString = QueryString.substring(0, QueryString.length() - 4) + ") #" + tag;
+		System.out.println(QueryString);
+		
+	    Twitter twitter = new TwitterFactory().getInstance();
+	    Query query = new Query(QueryString);
+	    QueryResult result = twitter.search(query);
+	    
+	    return covertResultIntoTable(result.getTweets());
+    }
+    
+    private Table covertResultIntoTable(List<Tweet> tweets) {
+    	Table table = new Table();
+        table.addColumn("useID", String.class);
+        table.addColumn("tweet", String.class);
+        for (Tweet tweet : tweets) {
+        	int rowNumber = table.addRow();
+            table.set(rowNumber, "useID", tweet.getFromUser());
+            table.set(rowNumber, "tweet", tweet.getText());
+ 	    }
+        this.logger.log(LogService.LOG_INFO, 
+        		String.format("There are %d tweets downloaded.", tweets.size()));
+        
+        return table;
+    }
+    
+    private Data[] generateOutputData(Table table) {
+        
+        /*
+         * After getting the output in table format make it available to the user.
+         */
+        Data output = new BasicData(table, Table.class.getName());
+        Dictionary<String, Object> metadata = output.getMetadata();
+        metadata.put(DataProperty.LABEL, "TwitResult");
+        metadata.put(DataProperty.PARENT, this.data[0]);
+        metadata.put(DataProperty.TYPE, DataProperty.TABLE_TYPE);
+         
+        return new Data[] { output };
+    }
+}
---
+++ trunk/sci2/plugins/normal_plugins/Twitter Reader/src/edu/iu/sci2/reader/twitter/TwitterReaderFactory.java
@@ -0,0 +1,59 @@
+package edu.iu.sci2.reader.twitter;
+
+import java.util.Dictionary;
+
+import org.cishell.framework.CIShellContext;
+import org.cishell.framework.algorithm.Algorithm;
+import org.cishell.framework.algorithm.AlgorithmFactory;
+import org.cishell.framework.algorithm.ParameterMutator;
+import org.cishell.framework.data.Data;
+import org.cishell.reference.service.metatype.BasicAttributeDefinition;
+import org.cishell.reference.service.metatype.BasicObjectClassDefinition;
+import org.cishell.utilities.TableUtilities;
+import org.osgi.service.metatype.AttributeDefinition;
+import org.osgi.service.metatype.ObjectClassDefinition;
+
+import prefuse.data.Table;
+
+public class TwitterReaderFactory implements AlgorithmFactory, ParameterMutator {
+    public Algorithm createAlgorithm(Data[] data,
+    								 Dictionary<String, Object> parameters,
+    								 CIShellContext ciShellContext) {
+        return new TwitterReader(data, parameters, ciShellContext);
+    }
+
+    @Override
+	public ObjectClassDefinition mutateParameters(Data[] data,
+			ObjectClassDefinition parameters) {
+		Table table = (Table) data[0].getData();
+        
+        BasicObjectClassDefinition newParameters = new BasicObjectClassDefinition(
+            parameters.getID(), parameters.getName(), parameters.getDescription(), null);
+        AttributeDefinition[] oldAttributeDefinitions = parameters.getAttributeDefinitions(
+            ObjectClassDefinition.ALL);
+        String[] columnTitles = 
+            TableUtilities.getAllColumnNames(table.getSchema()).toArray(new String[0]);
+        for (AttributeDefinition oldAttributeDefinition : oldAttributeDefinitions) {
+            /*
+             * Append column titles into drop down boxes and assign to name parameter.
+             */
+            if (oldAttributeDefinition.getID().equals("uid")) {
+                newParameters.addAttributeDefinition(
+                        ObjectClassDefinition.REQUIRED,
+                        new BasicAttributeDefinition(
+                                oldAttributeDefinition.getID(), 
+                                oldAttributeDefinition.getName(), 
+                                oldAttributeDefinition.getDescription(), 
+                                oldAttributeDefinition.getType(), 
+                                columnTitles, 
+                                columnTitles));
+            } else {
+                /* None edit field */
+                newParameters.addAttributeDefinition(
+                    ObjectClassDefinition.REQUIRED, oldAttributeDefinition);
+            }
+        }
+        return newParameters;
+	}
+    
+}
