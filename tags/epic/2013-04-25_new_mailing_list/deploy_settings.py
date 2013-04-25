14d1af67440069b1a3ecbb61b78ba5fa235b58dc
Chin Hua Kong <kongch@indiana.edu>
2013-04-25 13:09:27 +0000

    Update with new mailing list iulist.indiana.edu

---
+++ tags/epic/2013-04-25_new_mailing_list/deploy_settings.py
@@ -0,0 +1,25 @@
+from base_settings import *
+import log_settings
+
+from database_credentials import *
+
+
+DATABASE_ENGINE = 'postgresql_psycopg2'
+DATABASE_NAME = 'epic_web'
+DATABASE_PORT = '5432'
+DATABASE_OPTIONS = {'sslmode': 'require'}
+
+MEDIA_ROOT = '/home/epic_website/epic_data/'
+
+#csrfProtect()
+
+
+
+TEMPLATE_DIRS = (
+	'/home/epic_website/epic_code/epic/templates',
+)
+
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = '25'
+
+HAYSTACK_WHOOSH_PATH = '/tmp/whoosh'
