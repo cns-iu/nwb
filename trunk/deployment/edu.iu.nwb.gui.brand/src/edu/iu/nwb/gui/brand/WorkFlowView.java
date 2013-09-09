24be19d886bbaa97b17d25e485f4c9fce7464ebf
Adam Simpson <adhsimps@indiana.edu>
2013-09-09 17:17:32 +0000

    Supporting Workflow Manager integration  - Added new Workflow Manager icons  - Added Workflow Manager to the .pom's and .xml's

---
+++ trunk/deployment/edu.iu.nwb.gui.brand/src/edu/iu/nwb/gui/brand/WorkFlowView.java
@@ -0,0 +1,11 @@
+package edu.iu.nwb.gui.brand;
+
+import org.cishell.reference.gui.workflow.views.WorkflowView;
+
+public class WorkFlowView extends WorkflowView {
+	public WorkFlowView() {
+		super("edu.iu.nwb.gui.brand");
+		
+//		Activator.getDefault().earlyStartup();
+	}
+}
---
+++ trunk/sci2/deployment/edu.iu.sci2.gui.brand/src/edu/iu/sci2/gui/brand/WorkFlowView.java
@@ -0,0 +1,11 @@
+package edu.iu.sci2.gui.brand;
+
+import org.cishell.reference.gui.workflow.views.WorkflowView;
+
+public class WorkFlowView extends WorkflowView {
+	public WorkFlowView() {
+		super("edu.iu.sci2.gui.brand");
+		
+//		Activator.getDefault().earlyStartup();
+	}
+}
