2e9f538b3364a359f64baef618fefc6bb4e7c473
Chin Hua Kong <kongch@indiana.edu>
2013-12-11 20:28:52 +0000

    Fix MacOS fail to pass input file issue. See the source code command for more detail.

--- trunk/plugins/visualization/gephi/src/edu/iu/nwb/visualization/network/gephi/GephiAlgorithm.java
+++ trunk/plugins/visualization/gephi/src/edu/iu/nwb/visualization/network/gephi/GephiAlgorithm.java
@@ -25,6 +25,9 @@ public class GephiAlgorithm implements Algorithm {
 	/* An extension for GraphML that Gephi recognizes. */
 	public static final String GRAPHML_EXT = ".graphml";
 	
+	private static final String MAC_EXECUTABLE_PATH = "/Applications/gephi.app/Contents/MacOS/gephi";
+	private static String OS = System.getProperty("os.name").toLowerCase();
+	
 	private final Data[] data;
 	private Program gephi;
 	private final LogService logger;
@@ -49,6 +52,7 @@ public class GephiAlgorithm implements Algorithm {
 					+ "with Gephi for this algorithm to work.\n";
 			throw new AlgorithmCreationFailedException(message);
 		}
+		
 	}
 
 	public Data[] execute() throws AlgorithmExecutionException {
@@ -56,7 +60,6 @@ public class GephiAlgorithm implements Algorithm {
 		List<File> files = fixFileExtForGephi();
 
 		for (File file : files) {
-						
 			/*
 			 *  BUG Launching this way has an issue where if CIShell has a console
 			 *  open, Gephi will reuse this console.  There does not appear to
@@ -65,11 +68,47 @@ public class GephiAlgorithm implements Algorithm {
 			 *  Because it reuses this console, if you close CIShell console, it will
 			 *  close Gephi without warning.  
 			 */
-			gephi.execute(file.getPath());
+			if (isMacOS()) {
+				launchGephiOnMac(gephi, file);
+			} else {
+				executeProgramWithFile(gephi, file);;
+			}
 
 		}
 
 		return new Data[0];
+	}
+	
+	private static boolean isMacOS() {
+		return (OS.indexOf("mac") >= 0);
+	}
+	
+	/**
+	 * Gephi can't take input file from Program.execute(). This is only
+	 * happenned on gephi. So we try to use the static path to launch gephi
+	 * for now. If it failed, it will launch gephi with Program.execute()
+	 * 
+	 * Another issue is when gephi load the input file, the input file
+	 * is deleted from the Mac temporary folder. Either it is Mac OS 
+	 * default behavior or Gephi. However, this will cause the Gephi
+	 * fail to load the file. To avoid this, the permission need to be changed.
+	 */
+	private static void launchGephiOnMac(Program program, File file) {
+		try {
+			Runtime.getRuntime().exec("chmod 555 " + file.getAbsolutePath());
+			Runtime.getRuntime().exec(MAC_EXECUTABLE_PATH + " " + file.getAbsolutePath());
+		} catch (Exception e) {
+			executeProgramWithFile(program, file);
+		}
+	}
+	
+	private static void executeProgramWithFile(final Program program,
+			   final File file) {
+		Display.getDefault().asyncExec(new Runnable() {
+			public void run() {
+				program.execute(file.getAbsolutePath());
+			}
+		});
 	}
 
 	private List<File> fixFileExtForGephi() throws AlgorithmExecutionException {
